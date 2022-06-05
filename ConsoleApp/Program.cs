3c5dd5e17376ffd76104058bb34eec81127e1725
victoriasmirnova <victoriasmirnova4@gmail.com>
2022-06-05 15:51:50 +0300

    change message

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Vika!");
+            Console.WriteLine("Hello, Vika!");
         }
     }
 }
