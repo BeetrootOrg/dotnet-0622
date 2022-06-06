a8493f79a08b3165d52fd93adcff04ccbdd5cb70
Killjaen <killjaen@gmail.com>
2022-06-06 22:07:59 +0300

    +Пробіли

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,25 +7,25 @@ float sixthvalue = 2.2f;
 double seventhvalue = 3.7f;
 decimal eighthvalue = 12.8M;
 
-Console.WriteLine("First part of homework");
-Console.WriteLine();                                                        // просто для пробілу при виводі
-Console.WriteLine("int + short: " + thirdvalue + secondvalue);
-Console.WriteLine();
-Console.WriteLine("long + int: " + (fourthvalue - thirdvalue));
-Console.WriteLine();
-Console.WriteLine("decimal * byte: " + eighthvalue * firstvalue);
+    Console.WriteLine("First part of homework");
+    Console.WriteLine();                                                        // просто для пробілу при виводі
+    Console.WriteLine("int + short: " + thirdvalue + secondvalue);
+    Console.WriteLine();
+    Console.WriteLine("long + int: " + (fourthvalue - thirdvalue));
+    Console.WriteLine();
+    Console.WriteLine("decimal * byte: " + eighthvalue * firstvalue);
 
-Console.WriteLine();
+    Console.WriteLine();
 
 int x = 14;
 int y = 73;
 
-Console.WriteLine("Second part of homework");
-Console.WriteLine();
-Console.WriteLine("Math.Max(x,y): " + Math.Max(x, y));
-Console.WriteLine();
-Console.WriteLine("2*Pi*x: " + 2 * Math.PI * x);
-Console.WriteLine();
-Console.WriteLine("abs(x)*sin(x): " + Math.Abs(x) * Math.Sin(x));
-Console.WriteLine();
-Console.WriteLine("-6*x^3+5*x^2-10*x+15: " + (-6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15));
+    Console.WriteLine("Second part of homework");
+    Console.WriteLine();
+    Console.WriteLine("Math.Max(x,y): " + Math.Max(x, y));
+    Console.WriteLine();
+    Console.WriteLine("2*Pi*x: " + 2 * Math.PI * x);
+    Console.WriteLine();
+    Console.WriteLine("abs(x)*sin(x): " + Math.Abs(x) * Math.Sin(x));
+    Console.WriteLine();
+    Console.WriteLine("-6*x^3+5*x^2-10*x+15: " + (-6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15));
