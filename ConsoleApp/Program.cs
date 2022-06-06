79b373220163e3c85d0296f3c0d03fab9f565fdb
Andriy.Gulpa <andriy.gulpa@gmail.com>
2022-06-06 19:52:57 +0300

    integer type

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,3 +5,20 @@ int b = 6;
 int sum = a + b;
 
 Console.WriteLine(sum);
+Console.WriteLine(a + b + 1);
+Console.WriteLine(a * b);
+
+int c = 9;
+int d = 4;
+System.Console.WriteLine("divide");
+System.Console.WriteLine(c / d);
+System.Console.WriteLine(d / c);
+System.Console.WriteLine("remaine");
+System.Console.WriteLine(c % d);
+System.Console.WriteLine(d % c);
+// coment fast Ctrl+/
+
+byte b1 = 5;
+short s1 = 6;
+long l1 = 129030192;
+ulong u1 = 129030192;
