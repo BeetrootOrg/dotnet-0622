c27883de81472f5beb098d7925290203eae64aef
iVanyaVas <vanya.iaschenko@gmail.com>
2022-06-06 22:05:47 +0300

    Task 2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,3 +17,11 @@ System.Console.WriteLine(dd1 - b1);
 System.Console.WriteLine(i1 + l1);
 System.Console.WriteLine(b1 + s1 * l1 - dd1);
 
+System.Console.WriteLine("Task #2");
+var x = 3;
+var y = 2;
+System.Console.WriteLine(-6 * Math.Pow(x,3) + 5 * Math.Pow(x,2) - 10 * x + 15);
+System.Console.WriteLine(Math.Abs(x) * Math.Sin(x));
+System.Console.WriteLine( 2 * Math.PI * x);
+System.Console.WriteLine(Math.Max(x,y));
+
