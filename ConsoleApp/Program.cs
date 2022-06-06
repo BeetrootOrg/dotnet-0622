da264f3593d72b2785201b97f126914bdd6d5e0e
yurner000 <sysoev.yu@gmail.com>
2022-06-06 19:53:40 +0300

    add number types

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,3 +7,20 @@ int sum = a + b;
 Console.WriteLine(sum);
 Console.WriteLine(a + b);
 Console.WriteLine(a * b);
+
+int c = 9;
+int d = 4;
+System.Console.WriteLine("Devide");
+System.Console.WriteLine(c / d);
+System.Console.WriteLine(d / c);
+System.Console.WriteLine("Remaining");
+System.Console.WriteLine(c % d);
+System.Console.WriteLine(d % c);
+// error below if you try to uncoment
+// System.Console.WriteLine(100 / 0);
+
+byte b1 = 5;
+short s1 = 6;
+long l1 = 438493;
+ulong l2 = 3433434;
+uint i2 = 6;
