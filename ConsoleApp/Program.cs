a43cdce38853b263cbcfe8c4309e5cbc2df7a984
iVanyaVas <vanya.iaschenko@gmail.com>
2022-06-06 19:57:00 +0300

    value types

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a =5;
+int b = 6;
+int sum = a + b;
+
+Console.WriteLine(a * b);
+Console.WriteLine(a + b);
+
+int c = 9;
+int d = 4;
+
+System.Console.WriteLine(c / d);
+System.Console.WriteLine(d / c);
+
+int i1 = 7;
+byte b1 = 5;
+short s1 = 6;
+long l1 = 129021492;
+long l2 = 544124;
+uint i2 = 6;
+
+
