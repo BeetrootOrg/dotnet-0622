7a58f867b3ffe83adccbbc6927f6ebf60805c409
iVanyaVas <vanya.iaschenko@gmail.com>
2022-06-06 22:04:35 +0300

    Task 1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,19 @@
 ﻿
+byte b1 = 255;
+short s1 = 10;
+int i1 = 12;
+long l1 = 20;
+float f1 = 20.123f;
+double d1 = 44.52d;
+decimal dd1 = 251.553m;
+
+
+System.Console.WriteLine("Task #1");
+System.Console.WriteLine(b1 + s1);
+System.Console.WriteLine(b1 + dd1);
+System.Console.WriteLine(f1 * d1);
+System.Console.WriteLine(b1 - f1);
+System.Console.WriteLine(dd1 - b1);
+System.Console.WriteLine(i1 + l1);
+System.Console.WriteLine(b1 + s1 * l1 - dd1);
+
