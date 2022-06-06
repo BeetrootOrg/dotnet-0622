0b42fe76b1cdd03f64c7a5dd83f650027796743c
d.misik <misik.dmitriy@gmail.com>
2022-06-06 20:13:37 +0300

    logical operation

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -24,3 +24,32 @@ short s1 = 6;
 long l1 = 129030192;
 ulong l2 = 129030192;
 uint i2 = 6;
+
+System.Console.WriteLine(i1);
+System.Console.WriteLine(b1);
+System.Console.WriteLine(s1);
+System.Console.WriteLine(l1);
+System.Console.WriteLine(l2);
+System.Console.WriteLine(i2);
+
+// logical operation
+bool first = true;
+bool second = false;
+bool third = true;
+bool fourth = false;
+
+System.Console.WriteLine("Logical NOT");
+System.Console.WriteLine(!first);
+System.Console.WriteLine(!second);
+
+// if at least one operand false - AND equals to false
+System.Console.WriteLine("Logical AND");
+System.Console.WriteLine(first && second);
+System.Console.WriteLine(first && third);
+System.Console.WriteLine(first && fourth);
+
+// if at least one operand true - OR equals to true
+System.Console.WriteLine("Logical OR");
+System.Console.WriteLine(second || first);
+System.Console.WriteLine(second || third);
+System.Console.WriteLine(second || fourth);
