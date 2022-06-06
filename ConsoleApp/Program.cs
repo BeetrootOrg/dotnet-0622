2ad5cc231787ea2b50889db40f6ba873685afce1
iVanyaVas <vanya.iaschenko@gmail.com>
2022-06-06 22:07:59 +0300

    missing Task 2 glorious return

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,6 +17,14 @@ System.Console.WriteLine(dd1 - b1);
 System.Console.WriteLine(i1 + l1);
 System.Console.WriteLine(b1 + s1 * l1 - dd1);
 
+System.Console.WriteLine("Task #2");
+var x = 3;
+var y = 2;
+System.Console.WriteLine(-6 * Math.Pow(x,3) + 5 * Math.Pow(x,2) - 10 * x + 15);
+System.Console.WriteLine(Math.Abs(x) * Math.Sin(x));
+System.Console.WriteLine( 2 * Math.PI * x);
+System.Console.WriteLine(Math.Max(x,y));
+
 
 System.Console.WriteLine("Task Extra");
 DateTime date = DateTime.Today;
