b00199cf6967948e61181bdad9aba96c0abee233
d.misik <misik.dmitriy@gmail.com>
2022-06-06 19:49:07 +0300

    divide

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,3 +4,16 @@ int b = 6;
 int sum = a + b;
 
 Console.WriteLine(sum);
+Console.WriteLine(a + b);
+Console.WriteLine(a * b);
+
+int c = 9;
+int d = 4;
+System.Console.WriteLine("Divide");
+System.Console.WriteLine(c / d);
+System.Console.WriteLine(d / c);
+System.Console.WriteLine("Remaining");
+System.Console.WriteLine(c % d);
+System.Console.WriteLine(d % c);
+// error below if you try to uncoment
+// System.Console.WriteLine(100 / 0);
