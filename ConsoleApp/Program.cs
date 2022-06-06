1042a1e911c212c69a98bf8e835a93400671d85c
iVanyaVas <vanya.iaschenko@gmail.com>
2022-06-06 22:07:00 +0300

    Task Extra

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,11 +17,14 @@ System.Console.WriteLine(dd1 - b1);
 System.Console.WriteLine(i1 + l1);
 System.Console.WriteLine(b1 + s1 * l1 - dd1);
 
-System.Console.WriteLine("Task #2");
-var x = 3;
-var y = 2;
-System.Console.WriteLine(-6 * Math.Pow(x,3) + 5 * Math.Pow(x,2) - 10 * x + 15);
-System.Console.WriteLine(Math.Abs(x) * Math.Sin(x));
-System.Console.WriteLine( 2 * Math.PI * x);
-System.Console.WriteLine(Math.Max(x,y));
 
+System.Console.WriteLine("Task Extra");
+DateTime date = DateTime.Today;
+DateTime pastYear = new DateTime(2022,01,01);
+DateTime followingYear = new DateTime(2023,01,01);
+TimeSpan days;
+
+days = date.Subtract(pastYear);
+System.Console.WriteLine($"{days.Days} days paseed from New Year");
+days = followingYear.Subtract(date);
+System.Console.WriteLine($"{days.Days} days left to New Year");
