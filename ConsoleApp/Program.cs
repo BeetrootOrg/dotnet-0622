bc0afcab923b5c80ae4947c2c436723acc8020e4
d.misik <misik.dmitriy@gmail.com>
2022-06-06 19:41:45 +0300

    first operation

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// camel case name convention
+int a = 5;
+int b = 6;
+int sum = a + b;
+
+Console.WriteLine(sum);
