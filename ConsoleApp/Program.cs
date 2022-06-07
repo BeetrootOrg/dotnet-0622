6d8015d17b09c658f03e975a8c17014ac5cc0e54
Tikhon Lavrov <Tykhon.Lavrov@koroka.solutions>
2022-06-07 18:10:32 +0300

    Defined Variables

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Defining variables
+byte myByte = 1;
+short myShort = 10;
+int myInt = 101;
+long myLong = 1000;
+bool myBool = true;
+bool myFalseBool = false;
+float myFloat = 0.1f;
+double myDouble = 0.01;
+decimal myDecimal = 0.001m;
