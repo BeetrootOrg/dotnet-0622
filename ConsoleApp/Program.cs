8baa6b94c360d342ea3977d8faa19aea1f1b3bc4
Telelymyauskas <ntelelym@gmail.com>
2022-06-07 18:11:44 +0300

    Initial

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Numeric Variables
+byte a = 8;
+short c = -56;
+int e = 451318;
+long g = 1598798;
+
+// Arithmetic operations 
+System.Console.WriteLine(a + c);
+System.Console.WriteLine(e - c);
+System.Console.WriteLine(e * a);
+System.Console.WriteLine(g / a);
+// Remaining 
+System.Console.WriteLine(g % a);
+System.Console.WriteLine(a % g);
