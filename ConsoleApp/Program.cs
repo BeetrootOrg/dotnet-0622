ef4edab3dfbcf773cf8f24d951be93b43151ed83
Anarik <106695465+Anariks@users.noreply.github.com>
2022-06-07 20:33:31 +0300

    sum numbers

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x;
+int y;
+
+// Entering x
+Console.WriteLine("Hello, let's do some loops, please enter whole number x:");
+
+var console = Console.ReadLine();
+
+if (int.TryParse(console, out x))
+{
+    Console.WriteLine("x="+ x);
+} 
+else 
+Console.WriteLine("x is not an integer number");
+
+// Entering y
+Console.WriteLine("Now enter whole number y:");
+
+console = Console.ReadLine();
+
+if (int.TryParse(console, out y))
+{
+    Console.WriteLine("y="+ y);
+} 
+else 
+Console.WriteLine("y is not an integer number");
+
+int length;
+length = (x > y) ? x - y : y - x; 
+//Console.WriteLine($"Gonna be {length+1} iterations");
+
+int sum = 0;
+
+for (int i = 0; i <= length; i++)
+{
+   if (length != 0)
+    sum += (x > y) ? y++ : x++; 
+   else
+    sum = x + y;
+}
+
+Console.WriteLine($"The sum is {sum}");
+
+
+
+
+
