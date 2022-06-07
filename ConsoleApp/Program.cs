3a02fcc00ed7247dd7fc91f662473bbcac2f9c45
Tikhon Lavrov <Tykhon.Lavrov@koroka.solutions>
2022-06-07 18:11:20 +0300

    Performed Math and Logical Operations

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,3 +8,11 @@ bool myFalseBool = false;
 float myFloat = 0.1f;
 double myDouble = 0.01;
 decimal myDecimal = 0.001m;
+
+// Math and logical operations to defined variables
+System.Console.WriteLine(myByte + myLong);
+System.Console.WriteLine(myShort - myFloat);
+System.Console.WriteLine(myInt * myDecimal);
+System.Console.WriteLine(myBool || myFalseBool);
+System.Console.WriteLine(myDouble / myInt);
+System.Console.WriteLine(myInt % myShort);
