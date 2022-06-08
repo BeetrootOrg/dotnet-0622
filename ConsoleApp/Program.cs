0448365d12eb834433a133b1e11482b946d172d7
Anarik <106695465+Anariks@users.noreply.github.com>
2022-06-09 01:20:33 +0300

    while loop added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -31,17 +31,23 @@ Console.WriteLine("y is not an integer number, run again and put right value");
 return;
 }
 
-int length;
-length = (x > y) ? x - y : y - x; 
-//Console.WriteLine($"Gonna be {length+1} iterations");
 
-int sum = 0;
+// While loop
+int sum = Math.Min(x , y);
+while (x != y)
+{
+    sum += (x > y) ? ++y : ++x;
+}
+
+// //for loop also working
+/* int length;
+length = (x > y) ? x - y : y - x; 
 
 for (int i = 0; i <= length; i++)
 {
     sum += (x > y) ? y++ : x++; 
 
-}
+ } */
 
 Console.WriteLine($"The sum is {sum}");
 
