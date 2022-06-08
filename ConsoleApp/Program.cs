745478bf1650f2be73a039ffdb098a43ebb00954
AndryM <103665742+AndryMinoMax@users.noreply.github.com>
2022-06-08 12:56:44 +0300

    222

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int b = 5 ;
+int a = 5 ;
+int sum = (b+a);
+Console.WriteLine(a+b);
+Console.WriteLine(sum);
+
+
+int c = 9;
+int d = 4;
+Console.WriteLine("Devide");
+Console.WriteLine(d / c);
+Console.WriteLine(c / d);
+Console.WriteLine("Remaining");
+Console.WriteLine(c % d);
+Console.WriteLine(d % c);
+
