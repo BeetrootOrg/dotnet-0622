8cac5f83ead7776156a8697701ffb887c317121f
Telelymyauskas <ntelelym@gmail.com>
2022-06-08 19:24:14 +0300

    conditions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,48 +1,25 @@
-﻿// Integral Numeric types
-byte a = 8;
-short b = 56;
-int c = 451318;
-long d = 1598798;
+﻿var a = 42;
 
-System.Console.WriteLine("Arithmetic operations with integral numeric types");
-System.Console.WriteLine(a + c);
-System.Console.WriteLine(b - c);
-System.Console.WriteLine(d * a);
-System.Console.WriteLine(c * a);
+if (a == 42) {
+    System.Console.WriteLine("A is 42");
+}
+else if (a == 43) {
+    System.Console.WriteLine("A is 43");
+}
+else if (a == 44) {
+    System.Console.WriteLine("A is 44");
+}
+else 
+{
+    System.Console.WriteLine("A is unknown");
+}
 
-// floating-point numeric types
-float e = 2.102f; 
-double f = 3.516;
-decimal j = 31.47482m;
-
-System.Console.WriteLine("Arithmetic operations with floating-point numeric types");
-System.Console.WriteLine(e + f);
-System.Console.WriteLine(e - f);
-System.Console.WriteLine (j * (decimal)f);
-
-bool first = true;
-bool second = false;
-
-System.Console.WriteLine("Logical operations");
-System.Console.WriteLine(first && second);
-
-
-System.Console.WriteLine("several math functions");
-
-var x = 13;
-var y = 7;
-
-System.Console.WriteLine(-6 * Math.Pow(x,3) + 5 * Math.Pow(x,2) - 10 * x + 15);
-System.Console.WriteLine(Math.Abs(x) * Math.Sin(x));
-System.Console.WriteLine(2 * Math.PI * x);
-System.Console.WriteLine(Math.Max(x, y));
-
-// Extra DateTime
-System.Console.WriteLine("Date Time");
-DateTime thisDay = DateTime.Today; 
-DateTime lastNewYear = new DateTime(2022, 1, 1);
-DateTime nextNewYear = new DateTime(2023, 1, 1);
-int afterLastNewYear = (thisDay - lastNewYear).Days;
-System.Console.WriteLine($"{afterLastNewYear} days passed from New Year");
-int beforeNextNewYear = (nextNewYear - thisDay).Days;
-System.Console.WriteLine($"{beforeNextNewYear} days left to New Year");
+int b = 10; 
+if (a > 40 && b > 5)
+{
+    System.Console.WriteLine("a > 40 && b > 5 = TRUE");
+}
+else 
+{
+    System.Console.WriteLine("a > 40 && b > 5 = TRUE");
+}
