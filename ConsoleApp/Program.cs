fdd5abcc7594a9379925674a81d05cbe8e6145a3
d.misik <misik.dmitriy@gmail.com>
2022-06-08 19:21:23 +0300

    conditions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,48 @@
-﻿System.Console.WriteLine(2 + 2);
+﻿var a = 45;
+
+if (a == 42)
+{
+    System.Console.WriteLine("A is 42");
+}
+else if (a == 43)
+{
+    System.Console.WriteLine("A is 43");
+}
+else if (a == 44)
+{
+    System.Console.WriteLine("A is 44");
+}
+else
+{
+    System.Console.WriteLine("A is unknown");
+}
+
+if (a == 42)
+{
+    System.Console.WriteLine("A is 42");
+}
+else if (a == 43)
+{
+    System.Console.WriteLine("A is 43");
+}
+else if (a == 44)
+{
+    System.Console.WriteLine("A is 44");
+}
+else
+{
+    System.Console.WriteLine("A is unknown");
+}
+
+if (a == 42)
+{
+    System.Console.WriteLine("A is 42");
+}
+else if (a == 43)
+{
+    System.Console.WriteLine("A is 43");
+}
+else if (a == 44)
+{
+    System.Console.WriteLine("A is 44");
+}
