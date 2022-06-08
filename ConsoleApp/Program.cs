d0da179deec90b991f29cb3429f98f5a75033ed9
ztgarry <as.tkachuk@gmail.com>
2022-06-08 11:43:41 +0300

    createHomework02Branche2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* HOME TASK
+Define several variables in your program (byte, short, int, long, bool, float, double, decimal) and write to console the result of addition, subtraction, multiplication 
+of several of them.
+Write to console result of several math functions:
+
+-6*x^3+5*x^2-10*x+15
+abs(x)*sin(x)
+2*pi*x
+max(x, y)
+
+*/
+
+
+
+
