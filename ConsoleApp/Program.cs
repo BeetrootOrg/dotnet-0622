f1bf9b054c03f02edc5d5fa2e70412b84c0d2e97
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-06-08 19:26:16 +0300

    conditions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,27 +1,49 @@
-﻿byte b1 = 5;
-short s1 = 6;
-int i1 = 7;
-long l1 = 129030192;
-bool b2 = true;
-float f1 = 123.01f;
-double d1 = 123.01d;
-decimal d2 = 400.75m;
+﻿var a = 45;
 
-Console.WriteLine((l1-s1)+(i1*b1));
+if (a == 42)
+{
+    System.Console.WriteLine("A is 42");
+}
+else if (a == 43)
+{
+    System.Console.WriteLine("A is 43");
+}
+else if (a == 44)
+{
+    System.Console.WriteLine("A is 44");
+}
+else
+{
+    System.Console.WriteLine("A is unknown");
+}
 
-int x = 15;
-int y = 10;
-Console.WriteLine(-6 * Math.Pow(x,3) + 5 * Math.Pow(x,2) - 10 * x + 15);
-Console.WriteLine(Math.Abs(x) * Math.Sin(x));
-Console.WriteLine(2 * Math.PI * x);
-Console.WriteLine(Math.Max(x, y));
+if (a == 42)
+{
+    System.Console.WriteLine("A is 42");
+}
+else if (a == 43)
+{
+    System.Console.WriteLine("A is 43");
+}
+else if (a == 44)
+{
+    System.Console.WriteLine("A is 44");
+}
+else
+{
+    System.Console.WriteLine("A is unknown");
+}
 
-DateTime now = DateTime.Now;
-DateTime NewYear = new DateTime(DateTime.Now.Year,12,31);
-
-int x1 = NewYear.DayOfYear - now.DayOfYear;
-int y1 = now.DayOfYear;
-
-Console.WriteLine(x1+" days left to New Year");
-Console.WriteLine(y1+" days passed from New Year");
+if (a == 42)
+{
+    System.Console.WriteLine("A is 42");
+}
+else if (a == 43)
+{
+    System.Console.WriteLine("A is 43");
+}
+else if (a == 44)
+{
+    System.Console.WriteLine("A is 44");
+}
 
