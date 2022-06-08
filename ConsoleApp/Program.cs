f337911e0d755bdc773994c21f360df3b56cbd86
iVanyaVas <vanya.iaschenko@gmail.com>
2022-06-08 19:37:20 +0300

    switch

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,3 +17,79 @@ else
 {
     System.Console.WriteLine("a is unknown");
 }
+
+int b = 10;
+
+if (a > 40 && b > 5)
+{
+    System.Console.WriteLine("a > 40 && b > 5");
+}
+else{
+    System.Console.WriteLine("a > 40 && b > 5 is False");
+}
+
+if (a > 40 || b > 5)
+{
+    System.Console.WriteLine("a > 40 || b > 5");
+}
+else 
+{
+    System.Console.WriteLine("a > 40 || b > 5 is False");
+}
+
+switch (a)
+{
+    case 42:
+        System.Console.WriteLine("a is 42");
+        break;
+
+    case 43:
+        System.Console.WriteLine("a is 43");
+        break;
+
+    case 44:
+        System.Console.WriteLine("a is 44");
+        break;
+
+    case 45:
+        System.Console.WriteLine("a is 45");
+        break;
+    default:
+    System.Console.WriteLine("a is unknown");
+    break;
+}
+
+//if method
+int c;
+if(a == 45) c = 10;
+else c = 5;
+
+System.Console.WriteLine(c);
+
+//terenar option
+c = a == 45 ? 10 : 5;
+c = a == 45 ? 10 : (a == 50 ? 15 : 5); // two cases
+System.Console.WriteLine(c);
+
+// third option - switch expression  (при присваивании значения)
+c = a switch
+{
+    45 => 10,
+    50 => 15,
+    _ => 5
+};
+
+//4rd option - switch operator
+
+switch(a)
+{
+    case 45:
+    c = 10;
+    break;
+
+    default:
+    c = 5;
+    break;
+}
+
+c = a > 40 && b >= 5 ? 15 : 10; // 
