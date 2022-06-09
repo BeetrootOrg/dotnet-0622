8c524dd4f880327e7b878e784653ce4970f651aa
Telelymyauskas <ntelelym@gmail.com>
2022-06-09 16:12:18 +0300

    minor fixes 2.0

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,50 +1,56 @@
-﻿// With Extra task
-System.Console.WriteLine("This is a program to calculate the sum between two numbers");
-int x, y;
-int sum = 0;
-System.Console.WriteLine("Please declare values for X: ");
-string variableX = Console.ReadLine();
-if (int.TryParse(variableX, out x))
+﻿internal class Program
 {
-    System.Console.WriteLine($"X = {x}");
-    System.Console.WriteLine("Please declare values for Y: ");
-    string variableY = Console.ReadLine();
-
-    if (int.TryParse(variableY, out y))
+    private static void Main(string[] args)
     {
-        System.Console.WriteLine($"Y = {y}");
-        System.Console.WriteLine("Press Enter to proceed");
-        System.Console.ReadKey();
-        System.Console.WriteLine();
-        System.Console.WriteLine($"x = {x} y = {y}");
-        if (x < y)
+        // With Extra task
+        Console.WriteLine("This is a program to calculate the sum between two numbers");
+        int x, y;
+        int sum = 0;
+        Console.WriteLine("Please declare values for X: ");
+        string variableX = Console.ReadLine();
+        if (int.TryParse(variableX, out x))
         {
-            for (int i = x; i <= y; ++i)
+            Console.WriteLine($"X = {x}");
+            Console.WriteLine("Please declare values for Y: ");
+            string variableY = Console.ReadLine();
+
+            if (int.TryParse(variableY, out y))
             {
-                sum += i;
+                Console.WriteLine($"Y = {y}");
+                Console.WriteLine("Press any key to proceed");
+                Console.ReadKey();
+                Console.WriteLine();
+                Console.WriteLine($"x = {x} y = {y}");
+                if (x < y)
+                {
+                    for (int i = x; i <= y; ++i)
+                    {
+                        sum += i;
+                    }
+                    Console.WriteLine("first condition"); // to check, which condition works 
+                }
+                else if (x > y) // x = 5, y = 2
+                {
+                    for (int i = y; i <= x; ++i)
+                    {
+                        sum += i;
+                    }
+                    Console.WriteLine("second condition"); // to check, which condition works 
+                }
+                else
+                {
+                    sum = x;
+                }
+                Console.WriteLine($"The sum of numbers from X to Y is {sum}");
             }
-            System.Console.WriteLine("first condition"); // to check, which condition works 
-        }
-        else if (x > y) // x = 5, y = 2
-        {
-            for (int i = y; i <= x; ++i)
+            else
             {
-                sum += i;
+                Console.WriteLine("Invalid input");
             }
-            System.Console.WriteLine("second condition"); // to check, which condition works 
         }
         else
         {
-            sum = x;
+            Console.WriteLine("Invalid input");
         }
-        System.Console.WriteLine($"The sum of numbers from X to Y is {sum}");
     }
-    else
-    {
-        System.Console.WriteLine("Invalid input");
-    }
-}
-else
-{
-    System.Console.WriteLine("Invalid input");
 }
