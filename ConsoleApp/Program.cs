e7b164bcea94c1ad95281982d68fdfbfc99c743c
Oles Borys <oles.borys@gmail.com>
2022-06-09 23:37:12 +0200

    Homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,25 +1,54 @@
-﻿byte byte1 = 5;
-short short1 = 6;
-int int1 = 15;
-long long1 = 115;
-bool bool1 = true;
-float float1 = 15.5f;
-double double1 = 15.55;
-decimal decimal1 = 16.254m;
+﻿Console.WriteLine("Please type in your first number: ");
+string xString = Console.ReadLine();
+int xNubmer;
+bool xSuccess = int.TryParse(xString, out xNubmer);
 
-Console.WriteLine($"Here is the list of declared variables : {byte1} , {short1} , {int1} , {long1} , {bool1} , {float1} , {double1} , {decimal1}.");
+if (xSuccess == true)
+{
+    Console.WriteLine($"Your number is {xNubmer}");
+}
+else 
+{
+    Console.WriteLine("Invalid input, exiting the program");
+    Environment.Exit(0);
+}
 
-double x = 15;
-double result1 = (-6 * Math.Pow(x, 3)) + (5 * Math.Pow(x, 2) - 10 * x + 15);
-double result2 = Math.Abs(x) * Math.Sin(x);
-double result3 = 2 * Math.PI * x;
-double result4 = Math.Max(x , 28);
+Console.WriteLine("Please type in your second number: ");
+var yString = Console.ReadLine();
+int yNubmer;
+bool ySuccess = int.TryParse(yString, out yNubmer);
 
-Console.WriteLine($"Here is the list of results : {result1} , {result2} , {result3} , {result4}.");
+if (ySuccess == true)
+{
+    Console.WriteLine($"Your number is {yNubmer}");
+}
+else 
+{
+    Console.WriteLine("Invalid input, exiting the program");
+    Environment.Exit(0);
+}
 
-DateTime date = new DateTime(DateTime.Now.Year, 12, 31);
-int DaysLeft = date.Subtract(DateTime.Now).Days;
-Console.WriteLine($"{DaysLeft + 1} days left to New Year.");
-DateTime currentDateTime = DateTime.Now;
-Console.WriteLine($"{currentDateTime.DayOfYear} days passed from New Year.");
+int sum = 0;
+if (xNubmer < yNubmer && xNubmer != yNubmer)
+{
+    for (int i = xNubmer; i <= yNubmer; i++)
+    {
+        sum += i;
+    }
 
+    Console.WriteLine(sum);
+}   
+else if (xNubmer > yNubmer && xNubmer != yNubmer)
+{
+    for (int i = yNubmer; i <= xNubmer; i++)
+    {
+        sum += i;
+    }
+
+    Console.WriteLine(sum);
+}
+else
+{
+    sum = xNubmer;
+    Console.WriteLine($"You have entered same numbers {sum}!");
+}
