d3de06e3880fc99e91a4d54c76d2c0acfa3f25db
Danylo Strus <danylo.strus@gmail.com>
2022-06-09 11:02:39 +0300

    equality check, method of finding the sum and error output when entering letters

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,8 +1,33 @@
 ﻿
-System.Console.WriteLine("Input first num:");
+System.Console.Write("Input first num(only numbers):");
 var num1 = Console.ReadLine();
-System.Console.WriteLine("Input second num:");
+System.Console.Write("Input second num(only numbers):");
 var num2 = Console.ReadLine();
 
+if(num1 == num2) System.Console.WriteLine($"Numbers are equal {num1}");
+
+else if (int.TryParse(num1, out int pNumber1) && int.TryParse(num2, out int pNumber2))
+{
+    GetSum(pNumber1,pNumber2);
+}
+
+else
+{
+    System.Console.WriteLine("You're stupid!");
+    System.Console.WriteLine("Press any button to exit the program...");
+    Console.ReadKey();
+    Environment.Exit(0);
+}
+
+void GetSum(int a, int b)
+{
+    System.Console.WriteLine($"max number: {Math.Max(a,b)}");
+    int sum = 0;
+    for (int i = Math.Min(a,b); i <= Math.Max(a,b); ++i)
+    {
+        sum += i;
+    }
+    System.Console.WriteLine($"Sum from {a} to {b} = {sum}");
+}
 
 
