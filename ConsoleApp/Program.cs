df28dd2f9ad2039390daa80a448d0b5de5e08797
gordeevtrade <48132424+gordeevtrade@users.noreply.github.com>
2022-06-10 19:35:56 +0300

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,33 +1,39 @@
-﻿
-try
-{
-    System.Console.WriteLine("Введите число ");
-    int x = int.Parse(Console.ReadLine());
-    System.Console.WriteLine("Введите второе число ");
-    int y = int.Parse(Console.ReadLine());
+ System.Console.WriteLine("Введите число ");
+                int x = int.Parse(Console.ReadLine());
+                System.Console.WriteLine("Введите второе число ");
+                int y = int.Parse(Console.ReadLine());
 
-  int sum = 0;
-  int operand1 = x;
-  int   operand2 = y;
+                   int sum = 0;
+                   int operand1 = x;
+                   int operand2 = y;
 
-    if (operand1 == operand2)
-    {
-        sum = operand1;
-    }
-    else if (y < x)
-    {
-        operand1 = y;
-        operand2 = x;
-    }
-        for (int i = operand1; i <= operand2; i++)
-        {
-            sum += i;
-        }
+                while (true)
+                {
+                    if (operand1 == operand2)
 
-    Console.WriteLine("Cумма чисел = {0}", sum);
+                    {
+                        sum = operand1;
+                        Console.WriteLine("Числа равны {0}", sum);
+                        break;
+                    }
 
-}
-catch (Exception)
-{
-    Console.WriteLine("Invalid input");
-}
+                    if (y < x)
+                    {
+                        operand1 = y;
+                        operand2 = x;
+                    }
+
+                    for (int i = operand1; i <= operand2; i++)
+                    {
+                        sum += i;
+                    }
+
+                    Console.WriteLine("Cумма чисел = {0}", sum);
+                    break;
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input");
+            }
