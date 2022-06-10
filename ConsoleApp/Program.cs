ff57468eca6bf5bae0e4db9d99e6585c7ce0d889
artem <tradegordeev@gmail.com>
2022-06-10 17:12:36 +0300

    add homework 03

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+try
+{
+    System.Console.WriteLine("Введите число ");
+    int x = int.Parse(Console.ReadLine());
+    System.Console.WriteLine("Введите второе число ");
+    int y = int.Parse(Console.ReadLine());
+
+  int sum = 0;
+  int operand1 = x;
+  int   operand2 = y;
+
+    if (operand1 == operand2)
+    {
+        sum = operand1;
+    }
+    else if (y < x)
+    {
+        operand1 = y;
+        operand2 = x;
+    }
+        for (int i = operand1; i <= operand2; i++)
+        {
+            sum += i;
+        }
+
+    Console.WriteLine("Cумма чисел = {0}", sum);
+
+}
+catch (Exception)
+{
+    Console.WriteLine("Invalid input");
+}
