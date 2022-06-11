76b96d0bd1bae3c6094a9b860f4e8a54370cdbb0
d.misik <misik.dmitriy@gmail.com>
2022-06-11 10:32:23 +0300

    method with parameter

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,4 +3,12 @@
     System.Console.WriteLine("Hello, world");
 }
 
+static void HelloToSomebody(string somebody)
+{
+    System.Console.WriteLine($"Hello, {somebody}");
+}
+
 HelloWorld();
+
+HelloToSomebody("Dima");
+HelloToSomebody("Class");
