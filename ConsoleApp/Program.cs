f8eb22d233d4a8584c2fb6138ee7a3268299f051
victoriasmirnova <victoriasmirnova4@gmail.com>
2022-06-12 00:10:23 +0300

    csharp - 02 - homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,11 +6,42 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, 5 times!");
-            Console.WriteLine("Hello, 5 times!");
-            Console.WriteLine("Hello, 5 times!");
-            Console.WriteLine("Hello, 5 times!");
-            Console.WriteLine("Hello, 5 times!");
+     // Define several variables
+
+      byte a = 126;
+      short b = - 126;
+      int c = 12;
+      long d = 1234;
+      bool bool1 = true;
+      bool bool2 = false;
+      float e = 7.52f;
+      double f = 6.1d;
+      decimal g = 12.22m;
+
+    //   addition, subtraction, multiplication of several of them
+
+System.Console.WriteLine(d - a);
+System.Console.WriteLine(a + b);
+System.Console.WriteLine(d / c);
+System.Console.WriteLine(f * c);
+System.Console.WriteLine( bool1 || bool2);
+
+    //  result of several math functions
+
+
+        int x = 5;
+System.Console.WriteLine(-6*x^3+5*x^2-10*x+15);
+
+        x = 100;
+System.Console.WriteLine(Math.Abs(x)*Math.Sin(x));
+
+        x = -11;
+System.Console.WriteLine(2*Math.PI*x);
+
+        x = 7;
+        int y = 8;
+         System.Console.WriteLine (Math.Max(x, y));
+  
         }
     }
 }
