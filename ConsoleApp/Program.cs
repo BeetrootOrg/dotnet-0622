2f78dd70849a9da1d6cbbc5b2be27a77abe7fd4b
Danylo Strus <danylo.strus@gmail.com>
2022-06-11 21:46:32 +0300

    create proj

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.Write("Enter first number:");
+        var num1 = Console.ReadLine();
+        System.Console.Write("Enter first number:");
+        var num2 = Console.ReadLine();
+        
+
+    }
+}
