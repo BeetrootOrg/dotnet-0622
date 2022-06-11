70df55bc517953750e746a3d784b7cb7148e460f
d.misik <misik.dmitriy@gmail.com>
2022-06-11 10:34:44 +0300

    square

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,12 +3,20 @@
     System.Console.WriteLine("Hello, world");
 }
 
-static void HelloToSomebody(string somebody)
+static void HelloToSomebody(string somebody) => System.Console.WriteLine($"Hello, {somebody}");
+
+static int Square(int x)
 {
-    System.Console.WriteLine($"Hello, {somebody}");
+    return x * x;
 }
 
 HelloWorld();
 
 HelloToSomebody("Dima");
 HelloToSomebody("Class");
+
+var square3 = Square(3);
+System.Console.WriteLine(square3);
+
+// the same as above
+System.Console.WriteLine(Square(3));
