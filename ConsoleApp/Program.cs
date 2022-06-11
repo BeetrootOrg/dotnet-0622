244babca91d9fb8a6e25f20194b8500971d415ed
d.misik <misik.dmitriy@gmail.com>
2022-06-11 10:52:05 +0300

    try out

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -20,6 +20,25 @@ static void Add5(ref int initial)
     initial += 3;
 }
 
+static bool TryParseInt(string str, out int result) => int.TryParse(str, out result);
+static bool TryDivideBy3(int number, out int result)
+{
+    if (number % 3 == 0)
+    {
+        result = number / 3;
+        return true;
+    }
+
+    result = 0;
+    return false;
+}
+
+static bool TryDivideBy4(int number, out int result)
+{
+    result = number / 4;
+    return number % 4 == 0;
+}
+
 HelloWorld();
 
 HelloToSomebody("Dima");
@@ -39,3 +58,19 @@ System.Console.WriteLine(initial);
 System.Console.WriteLine("ADD 5");
 Add5(ref initial);
 System.Console.WriteLine(initial);
+
+// parsing
+var success = TryParseInt("42", out var result);
+System.Console.WriteLine($"Success: {success}. Result: {result}");
+
+success = TryDivideBy3(6, out initial);
+System.Console.WriteLine($"Success: {success}. Result: {initial}");
+
+success = TryDivideBy3(7, out initial);
+System.Console.WriteLine($"Success: {success}. Result: {initial}");
+
+success = TryDivideBy4(8, out initial);
+System.Console.WriteLine($"Success: {success}. Result: {initial}");
+
+success = TryDivideBy4(9, out initial);
+System.Console.WriteLine($"Success: {success}. Result: {initial}");
