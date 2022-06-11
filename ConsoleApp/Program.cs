f0164fe5f9fb3c8585908d9dd72994f1d56e1af0
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-06-11 12:07:48 +0300

    metods, ref out/ recursion

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,168 +1,191 @@
-﻿var a = 45;
-
-if (a == 42)
+﻿internal class Program
 {
-    System.Console.WriteLine("A is 42");
+    static void HelloWorld()
+    {
+        Console.WriteLine("Hello, world");
+    }
+
+    static void HelloToSomebody(string somebody) => Console.WriteLine($"Hello, {somebody}");
+
+    static int Square(int x)
+    {
+        return x * x;
+    }
+
+    static void Add3(int initial)
+    {
+        initial += 3;
+    }
+
+    static void Add5(ref int initial)
+    {
+        initial += 3;
+    }
+
+    static bool TryParseInt(string str, out int result) => int.TryParse(str, out result);
+    static bool TryDivideBy3(int number, out int result)
+    {
+        if (number % 3 == 0)
+        {
+            result = number / 3;
+            return true;
+        }
+
+        result = 0;
+        return false;
+    }
+
+    static bool TryDivideBy4(int number, out int result)
+    {
+        result = number / 4;
+        return number % 4 == 0;
+    }
+
+    static void TryRef(ref int value)
+    {
+        if (value > 10)
+        {
+            value += 20;
+        }
+    }
+
+    // Compilation error
+    // static void TryOut(out int value)
+    // {
+    //     if (value > 10)
+    //     {
+    //         value += 20;
+    //     }
+    // }
+
+    static int SumOfTwo(int a = 1, int b = 2) => a + b;
+
+    static double Multiply() => 42;
+    static double Multiply(double a) => a * 42;
+    static double Multiply(double a, double b, double c) => a * b * c;
+    static int Multiply(int a, int b, int c) => a * b * c;
+    static int Multiply(int a, int b) => a * b;
+    static int Multiply(int a) => a * 42;
+
+    static long Fibonacci(long n)
+    {
+        if (n <= 0)
+        {
+            return -1;
+        }
+
+        long result = 1, prev = 1, counter = n - 1;
+
+        while (--counter > 0)
+        {
+            var temp = result;
+            result += prev;
+            prev = temp;
+        }
+
+        return result;
+    }
+
+    static long FibonacciRecursive(long n)
+    {
+        if (n <= 0) return -1;
+        if (n == 1 || n == 2)
+        {
+            System.Console.WriteLine($"F({n}) = 1");
+            return 1;
+        }
+
+        System.Console.WriteLine($"Count F({n}) = F({n - 1}) + F({n - 2})");
+        var result = FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+        System.Console.WriteLine($"Result F({n}) = {result}");
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        HelloWorld();
+
+        HelloToSomebody("Dima");
+        HelloToSomebody("Class");
+
+        var square3 = Square(3);
+        Console.WriteLine(square3);
+
+        // the same as above
+        Console.WriteLine(Square(3));
+
+        var initial = 5;
+        Console.WriteLine("ADD 3");
+        Add3(initial);
+        Console.WriteLine(initial);
+
+        Console.WriteLine("ADD 5");
+        Add5(ref initial);
+        Console.WriteLine(initial);
+
+        // parsing
+        var success = TryParseInt("42", out var result);
+        Console.WriteLine($"Success: {success}. Result: {result}");
+
+        success = TryDivideBy3(6, out initial);
+        Console.WriteLine($"Success: {success}. Result: {initial}");
+
+        success = TryDivideBy3(7, out initial);
+        Console.WriteLine($"Success: {success}. Result: {initial}");
+
+        success = TryDivideBy4(8, out initial);
+        Console.WriteLine($"Success: {success}. Result: {initial}");
+
+        success = TryDivideBy4(9, out initial);
+        Console.WriteLine($"Success: {success}. Result: {initial}");
+
+        TryRef(ref initial);
+        Console.WriteLine(initial);
+        initial = 30;
+        TryRef(ref initial);
+        Console.WriteLine(initial);
+
+        // Compilation error
+        // int newValue;
+        // TryRef(ref newValue);
+
+        Console.WriteLine(SumOfTwo(5, 6));
+        Console.WriteLine(SumOfTwo());
+        Console.WriteLine(SumOfTwo(5));
+        Console.WriteLine(SumOfTwo(b: 6));
+
+        Console.WriteLine(Multiply());
+        Console.WriteLine(Multiply(5));
+        Console.WriteLine(Multiply(5, 6, 2));
+        Console.WriteLine(Multiply(5.5, 6, 2));
+
+        System.Console.WriteLine("Fibonacci");
+        System.Console.WriteLine(Fibonacci(1));
+        System.Console.WriteLine(Fibonacci(2));
+        System.Console.WriteLine(Fibonacci(3));
+        System.Console.WriteLine(Fibonacci(8));
+        System.Console.WriteLine(Fibonacci(0));
+        System.Console.WriteLine(Fibonacci(-2));
+
+        System.Console.WriteLine(Fibonacci(1000));
+
+        System.Console.WriteLine("Fibonacci Reccursive");
+        System.Console.WriteLine("FOR 1");
+        System.Console.WriteLine(FibonacciRecursive(1));
+
+        System.Console.WriteLine("FOR 2");
+        System.Console.WriteLine(FibonacciRecursive(2));
+
+        System.Console.WriteLine("FOR 3");
+        System.Console.WriteLine(FibonacciRecursive(3));
+
+        System.Console.WriteLine("FOR 8");
+        System.Console.WriteLine(FibonacciRecursive(8));
+
+        System.Console.WriteLine(FibonacciRecursive(0));
+        System.Console.WriteLine(FibonacciRecursive(-2));
+
+        // System.Console.WriteLine(FibonacciRecursive(1000));
+    }
 }
-else if (a == 43)
-{
-    System.Console.WriteLine("A is 43");
-}
-else if (a == 44)
-{
-    System.Console.WriteLine("A is 44");
-}
-else
-{
-    System.Console.WriteLine("A is unknown");
-}
-
-if (a == 42)
-{
-    System.Console.WriteLine("A is 42");
-}
-else if (a == 43)
-{
-    System.Console.WriteLine("A is 43");
-}
-else if (a == 44)
-{
-    System.Console.WriteLine("A is 44");
-}
-else
-{
-    System.Console.WriteLine("A is unknown");
-}
-
-if (a == 42)
-{
-    System.Console.WriteLine("A is 42");
-}
-else if (a == 43)
-{
-    System.Console.WriteLine("A is 43");
-}
-else if (a == 44)
-{
-    System.Console.WriteLine("A is 44");
-}
-
-switch (a)
-{
-    case 42:
-        System.Console.WriteLine("A is 42");
-        break;
-    case 43:
-        System.Console.WriteLine("A is 43");
-        break;
-    case 44:
-        System.Console.WriteLine("A is 44");
-        break;
-    default:
-        System.Console.WriteLine("A is unknown");
-        break;
-}
-
-// assign variable
-// 1st option
-int c;
-if (a == 45) c = 10;
-else c = 5;
-
-System.Console.WriteLine(c);
-
-// 2nd option - ternary
-c = a == 45 ? 10 : (a == 50 ? 15 : 5);
-System.Console.WriteLine(c);
-
-// 3rd option - switch expression
-c = a switch
-{
-    45 => 10,s
-    50 => 15,
-    _ => 5
-};
-
-// 4rd option - switch operator
-switch (a)
-{
-    case 45:
-        c = 10;
-        break;
-    case 50:
-        c = 15;
-        break;
-    default:
-        c = 5;
-        break;
-}
-
-System.Console.WriteLine("Loop FOR");
-for (int i = 0; i < 10; ++i)
-{
-    System.Console.WriteLine(i);
-}
-
-var sum = 0;
-for (int i = 0; i < 10; ++i)
-{
-    sum += i;
-}
-System.Console.WriteLine($"Sum from 0 to 9 equals to {sum}");
-
-// infinite loop below
-// for (;;)
-// {
-
-// }
-
-for (; sum < 50; sum += 3)
-{
-}
-
-System.Console.WriteLine(sum);
-
-// for (; ; ++sum)
-// {
-//     System.Console.WriteLine(sum);
-// }
-
-sum = int.MaxValue;
-System.Console.WriteLine(sum + 1);
-System.Console.WriteLine(sum - 1);
-
-System.Console.WriteLine("Loop WHILE");
-int j = 0;
-sum = 0;
-while (j < 10)
-{
-    sum += j;
-    ++j;
-}
-
-System.Console.WriteLine($"Sum is {sum}");
-
-while (sum < 0)
-{
-    System.Console.WriteLine("I'm inside");
-}
-
-System.Console.WriteLine("Loop DO-WHILE");
-j = 0;
-sum = 0;
-do
-{
-    sum += j;
-    ++j;
-} while (j < 10);
-System.Console.WriteLine($"Sum is {sum}");
-
-do
-{
-    System.Console.WriteLine("I'm inside");
-} while (sum < 0);
-
-double d = 0.1 + 0.1 + 0.1;
-float f = 0.1f * 0.1f * 0.1f;
-System.Console.WriteLine(d);
-System.Console.WriteLine(f);
