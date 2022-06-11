19435306209a45228541cfb6b72488be700bb117
d.misik <misik.dmitriy@gmail.com>
2022-06-11 10:41:32 +0300

    ref

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,16 @@ static int Square(int x)
     return x * x;
 }
 
+static void Add3(int initial)
+{
+    initial += 3;
+}
+
+static void Add5(ref int initial)
+{
+    initial += 3;
+}
+
 HelloWorld();
 
 HelloToSomebody("Dima");
@@ -20,3 +30,12 @@ System.Console.WriteLine(square3);
 
 // the same as above
 System.Console.WriteLine(Square(3));
+
+var initial = 5;
+System.Console.WriteLine("ADD 3");
+Add3(initial);
+System.Console.WriteLine(initial);
+
+System.Console.WriteLine("ADD 5");
+Add5(ref initial);
+System.Console.WriteLine(initial);
