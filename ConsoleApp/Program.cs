37451af0af5f908fdec22f82f0d91d395007350c
iVanyaVas <vanya.iaschenko@gmail.com>
2022-06-11 12:08:32 +0300

    methods

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,206 +1,158 @@
-﻿var a = 45;
-
-// if statments
-if (a == 42)
+﻿internal class Program
 {
-    System.Console.WriteLine("a is 42");
-}
-else if (a == 43)
-{
-    System.Console.WriteLine("a is 43");
-}
-else if (a == 44)
-{
-System.Console.WriteLine("a is 44");
-}
-else 
-{
-    System.Console.WriteLine("a is unknown");
-}
+            static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
 
-int b = 10;
+        static void HelloToSomebody(string somebody)
+        {
+            Console.WriteLine($"Hello {somebody}");
+        }
 
-if (a > 40 && b > 5)
-{
-    System.Console.WriteLine("a > 40 && b > 5");
-}
-else{
-    System.Console.WriteLine("a > 40 && b > 5 is False");
-}
+        static int Square(int x)
+        {
+            return x * x;
+        }
+        static void Add3(int initial)
+        {
+            initial += 3;
+        }
 
-if (a > 40 || b > 5)
-{
-    System.Console.WriteLine("a > 40 || b > 5");
-}
-else 
-{
-    System.Console.WriteLine("a > 40 || b > 5 is False");
-}
+        static void Add5(ref int initial)
+        {
+            initial += 5;
+        }
 
-switch (a)
-{
-    case 42:
-        System.Console.WriteLine("a is 42");
-        break;
+        static bool TryParseInt(string str, out int result) => int.TryParse(str, out result);
+        static bool TryDivideBy3(int number, out int result)
+        {
+            if (number % 3 == 0)
+            {
+                result = number / 3;
+                return true;
+            }
+            result = 0;
+            return false;
+        }
 
-    case 43:
-        System.Console.WriteLine("a is 43");
-        break;
+        static bool TryDivideBy4(int number, out int result)
+        {
+            result = number / 4;
+            return number % 4 == 0;
+        }
+        static void TryRef(ref int value)
+        {
+            if (value > 10)
+            {
+                value += 20;
+            }
+        }
 
-    case 44:
-        System.Console.WriteLine("a is 44");
-        break;
+        static int Sum(int a = 1, int b = 0) => a + b;
+        static double Multiply() => 42;
+        static double Multiply(double a) => a * 42;
+        static double Multiply(double a, double b, double c) => a * b * c;
+        static int Multiply(int a, int b, int c)=> a * b * c;
 
-    case 45:
-        System.Console.WriteLine("a is 45");
-        break;
-    default:
-    System.Console.WriteLine("a is unknown");
-    break;
-}
+        static int Fibonacci(int n)
+        {
+              if(n<=0)
+              {
+                return -1;
+              }
+            var result = 0;
 
-//if method
-int c;
-if(a == 45) c = 10;
-else c = 5;
+            var prev = 1;
+            result = 1;
+            var counter = n - 1;
 
-System.Console.WriteLine(c);
-
-//terenar option
-c = a == 45 ? 10 : 5;
-c = a == 45 ? 10 : (a == 50 ? 15 : 5); // two cases
-System.Console.WriteLine(c);
-
-// third option - switch expression  (при присваивании значения)
-c = a switch
-{
-    45 => 10,
-    50 => 15,
-    _ => 5
-};
-
-//4rd option - switch operator
-
-switch(a)
-{
-    case 45:
-    c = 10;
-    break;
-
-    default:
-    c = 5;
-    break;
-}
-
-c = a > 40 && b >= 5 ? 15 : 10; // any dificult
-
-System.Console.WriteLine("Loop For");
-for (int i = 0; i < 10; ++i) //pre increment compiles faster than increment
-{ 
-    System.Console.WriteLine(i);
-}
-
-var sum = 0;
-for(int  i = 0 ; i < 10; ++i)
-{
-    sum += i;
-}
-System.Console.WriteLine($"Sum from 0 to 9 equals = {sum}");
-
-System.Console.WriteLine("Loop While");
-int j = 0;
-sum = 0;
-while (j < 10)
-{
- sum += j;
- ++j;
-}
-System.Console.WriteLine($"Sum is {sum}");
-
-System.Console.WriteLine("Loop Do-While");
-j = 0;
-sum = 0;
-do 
-{
-    sum += j;
-    ++j;
-} while(j < 10);
-System.Console.WriteLine($"Sum is {sum}");
-
-sum = 0;
-for (int counter = 0; counter < 100; counter += 2)
-{
-    sum += counter;
-}
-System.Console.WriteLine(sum);
-
-//2nd - option
-sum = 0;
-
-for (int counter = 0; counter < 100; counter += 2)
-{
-    if(counter % 2 != 0)
+            while (--counter > 0)
+            {
+                var temp = result;
+                result += prev;
+                prev = temp;
+            }
+            Console.WriteLine($"{n}-th Fibonacci number is {result}");
+            return result;
+        }
+                
+     
+     static int FibbonachiRecursive(int n)
+     {
+        if(n <= 0) return -1;
+        if (n == 1 || n == 2 ) return 1;
+        return FibbonachiRecursive(n-1) + FibbonachiRecursive(n-2);
+     }
+    private static void Main(string[] args)
     {
-        continue;
+        
+
+        HelloWorld();
+        HelloToSomebody("Ivan");
+
+        var square3 = Square(3);
+        Console.WriteLine(square3);
+        Console.WriteLine(Square(3));
+
+        var initial = 5;
+        Add3(initial);
+        Console.WriteLine(initial);
+
+        Add5(ref initial);
+        Console.WriteLine(initial);
+
+        var success = int.TryParse("42", out var result);
+        Console.WriteLine($"Success: {success}. Result:{result}");
+
+        success = TryDivideBy3(6, out initial);
+        Console.WriteLine($"Success: {success}. Result:{initial}");
+        success = TryDivideBy3(7, out initial);
+        Console.WriteLine($"Success: {success}. Result:{initial}");
+
+        success = TryDivideBy4(8, out initial);
+        Console.WriteLine($"Success: {success}. Result:{initial}");
+        success = TryDivideBy4(9, out initial);
+        Console.WriteLine($"Success: {success}. Result:{initial}");
+
+        TryRef(ref initial);
+        initial = 30;
+        TryRef(ref initial);
+
+        Console.WriteLine(Sum(5 + 6));
+        Console.WriteLine(Sum());
+        Console.WriteLine(Sum(5));
+        Console.WriteLine(Sum(b: 6));
+
+        System.Console.WriteLine(Multiply());
+        System.Console.WriteLine(Multiply(5));
+        System.Console.WriteLine(Multiply(5, 6, 2));
+        System.Console.WriteLine(Multiply(5.5, 6.5, 7.5));
+
+                System.Console.WriteLine("Fibonacci");
+        System.Console.WriteLine(Fibonacci(1));
+        System.Console.WriteLine(Fibonacci(2));
+        System.Console.WriteLine(Fibonacci(3));
+        System.Console.WriteLine(Fibonacci(8));
+        System.Console.WriteLine(Fibonacci(0));
+        System.Console.WriteLine(Fibonacci(-2));
+
+        System.Console.WriteLine(Fibonacci(1000));
+
+        System.Console.WriteLine("Fibonacci Reccursive");
+        System.Console.WriteLine("FOR 1");
+        System.Console.WriteLine(FibbonachiRecursive(1));
+
+        System.Console.WriteLine("FOR 2");
+        System.Console.WriteLine(FibbonachiRecursive(2));
+
+        System.Console.WriteLine("FOR 3");
+        System.Console.WriteLine(FibbonachiRecursive(3));
+
+        System.Console.WriteLine("FOR 8");
+        System.Console.WriteLine(FibbonachiRecursive(8));
+
+        System.Console.WriteLine(FibbonachiRecursive(0));
+        System.Console.WriteLine(FibbonachiRecursive(-2));
     }
-    sum += counter;
 }
-System.Console.WriteLine(sum);
-
-//3rd option
-sum = 0;
-j = 0;
-while(j < 100)
-{
-    sum += j;
-    j+=2;
-}
-System.Console.WriteLine(sum);
-
-//4rd option
-sum = 0;
-j = 0;
-/*while(j < 100)
-{
-    if(j%2 != 0)
-    {
-        continue;
-    }
-    sum += j;
-    ++j;
-}
-System.Console.WriteLine(sum);*/
-
-//5rd option
-//sum = (0+100) / 2* 49;
-//System.Console.WriteLine(sum);
-
-System.Console.WriteLine("Input something: ");
-var s = Console.ReadLine();
-System.Console.WriteLine($"Input : {s}");
-
-System.Console.WriteLine("Enter number: ");
-var numberstr = Console.ReadLine();
-System.Console.WriteLine($"Your number = {numberstr +42}");
-
-var number = int.Parse(numberstr);
-System.Console.WriteLine($"Your number = {number + 42}");
-
-System.Console.WriteLine("enter number (please)");
-numberstr = Console.ReadLine();
-if(int.TryParse(numberstr, out number))
-{
-    System.Console.WriteLine($"You`ve entered number {number}");
-}
-else
-{
-    System.Console.WriteLine("Yo`re stupid");
-}
-
-bool result;
-do
-{
-    System.Console.WriteLine("enter number (please)");
-    numberstr = Console.ReadLine();
-    result = int.TryParse(numberstr, out number);
-}while (!result);
