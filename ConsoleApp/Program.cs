3825aa832aef21884942ea504158f113c246adbc
gordeevtrade <48132424+gordeevtrade@users.noreply.github.com>
2022-06-11 12:40:49 +0300

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,5 @@
+
+try {
  System.Console.WriteLine("Введите число ");
                 int x = int.Parse(Console.ReadLine());
                 System.Console.WriteLine("Введите второе число ");
@@ -7,32 +9,17 @@
                    int operand1 = x;
                    int operand2 = y;
 
-                while (true)
-                {
-                    if (operand1 == operand2)
-
-                    {
-                        sum = operand1;
-                        Console.WriteLine("Числа равны {0}", sum);
-                        break;
-                    }
-
                     if (y < x)
                     {
-                        operand1 = y;
-                        operand2 = x;
+                    operand1 = y;
+                    operand2 = x;
                     }
-
                     for (int i = operand1; i <= operand2; i++)
                     {
                         sum += i;
                     }
-
-                    Console.WriteLine("Cумма чисел = {0}", sum);
-                    break;
-                }
-
-            }
+                    Console.WriteLine("Cумма чисел = {0}", sum);                
+}
             catch (Exception)
             {
                 Console.WriteLine("Invalid input");
