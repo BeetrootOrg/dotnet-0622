f2fd36f7ca95ba5cf89f041351cccae003b39a69
AndryM <103665742+AndryMinoMax@users.noreply.github.com>
2022-06-12 11:17:52 +0300

    second commit

--- nomework/Program.cs
+++ nomework/Program.cs
@@ -1,6 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-1st/commit
+
+//1st/commit
+
 Console.WriteLine("Who ur daddy?");
 Console.ReadLine();
+
+//2st commit
+
+   int a = 132;
+   int b = 323;
+int c = 132 + 323;
+  Console.WriteLine(c);
 
