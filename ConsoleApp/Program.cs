ed88356a325d448f8302b9aed80d64b2a34ff250
Andriy.Gulpa <andriy.gulpa@gmail.com>
2022-06-12 16:08:24 +0300

    update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,33 @@
-﻿System.Console.WriteLine("Enter first value");
-var numberx = Console.ReadLine();
-x = int.Parse(numberx);
-if (int.TryParse(numberx, out x))
+﻿//Enter X value
 
-var y = 10;
+int x;
+ System.Console.WriteLine($"Enter X please");
+var numberx = Console.ReadLine();
+if (int.TryParse(numberx, out x))
+    {
+        System.Console.WriteLine($"you X number {x}");
+    }
+    else
+    {
+        System.Console.WriteLine("Wrong, only number");
+       return;   
+    }
+   
+//Enter Y value
+
+ int y;
+System.Console.WriteLine($"Enter Y please");
+var numbery = Console.ReadLine();
+if (int.TryParse(numbery, out y))
+    {
+        System.Console.WriteLine($"you Y number {y}");
+    }
+    else
+    {
+        System.Console.WriteLine("Wrong, only number");
+        return; 
+    }
+
 var sum = 0;
 
 if (x < y)
