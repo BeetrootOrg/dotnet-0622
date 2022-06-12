0210e6424ce94858832bf4d017867cd6d3030197
Andriy.Gulpa <andriy.gulpa@gmail.com>
2022-06-12 23:30:45 +0300

    start

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,49 +1,13 @@
-﻿//Enter X value
-
-int x;
- System.Console.WriteLine($"Enter X please");
-var numberx = Console.ReadLine();
-if (int.TryParse(numberx, out x))
-    {
-        System.Console.WriteLine($"you X number {x}");
-    }
-    else
-    {
-        System.Console.WriteLine("Wrong, only number");
-       return;   
-    }
-   
-//Enter Y value
-
- int y;
-System.Console.WriteLine($"Enter Y please");
-var numbery = Console.ReadLine();
-if (int.TryParse(numbery, out y))
-    {
-        System.Console.WriteLine($"you Y number {y}");
-    }
-    else
-    {
-        System.Console.WriteLine("Wrong, only number");
-        return; 
-    }
-
-var sum = 0;
-
-if (x < y)
+﻿static  void Helloworld()
 {
-    for (int i = x; i <= y; i++)
-    {
-        sum += i;
-    }
-    System.Console.WriteLine($"sum = {sum}");
+    System.Console.WriteLine("Hello World"); 
 }
-else if (x > y) 
+
+static void HEloToSomeBody(string somebady)
 {
-    for (int i = y; i <= x; i++)
-    {
-       sum += i;
-    } 
-   System.Console.WriteLine($"sum = {sum}");
+    System.Console.WriteLine($"Hello {somebady}"); 
 }
- 
+Helloworld();
+HEloToSomeBody(Console.ReadLine());
+HEloToSomeBody("Dima");
+HEloToSomeBody("CLASS");
