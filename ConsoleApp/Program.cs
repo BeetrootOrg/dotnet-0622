f0ef43b2db9afadb17e463001db5f9760f4d7c8f
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-06-12 21:00:36 +0300

    overload/out/recursion

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,14 +10,81 @@
         int minValue;
         return minValue = Math.Min(firstParam, secondParam);
     }
+    
+    public static int MaxValue(int firstParam, int secondParam, int thirdParam)
+    {
+        int maxValue;
+        return maxValue = Math.Max(Math.Max(firstParam, secondParam), thirdParam);
+    }
+    public static int MaxValue(int firstParam, int secondParam, int thirdParam, int fourthParam)
+    {
+        int maxValue;
+        return maxValue = Math.Max(Math.Max(Math.Max(firstParam, secondParam), thirdParam), fourthParam);
+    }
+    public static int MinValue(int firstParam, int secondParam, int thirdParam)
+    {
+        int minValue;
+        return minValue = Math.Min(Math.Min(firstParam, secondParam), thirdParam);
+    }
+    public static int MinValue(int firstParam, int secondParam, int thirdParam, int fourthParam)
+    {
+        int minValue;
+        return minValue = Math.Min(Math.Min(Math.Min(firstParam, secondParam), thirdParam), fourthParam);
+    }
+    public static bool TrySumIfOdd(int firstParam, int secondParam, out int sumResult)
+    {
+        if ((firstParam + secondParam) % 2 != 0)
+        {
+            sumResult = firstParam + secondParam;
+            return true;
+        }
+        else
+        {
+            sumResult = firstParam + secondParam;
+            return false;
+        }
+    }
 
-    static void Main(string[] args)
+    public static string Repeat(string str, int n)
+    { 
+        int i = 0;
+        if (i >= n) 
+        {
+            return "";
+        } 
+        i++;
+        return str + Repeat (str, n-1);
+
+    }
+
+    public static void Main(string[] args)
     {
    
         int maxValue = MaxValue(6, 4);
-        System.Console.WriteLine(maxValue);
+        Console.WriteLine($"Max value fo two parameters: {maxValue}");
+                
         int minValue = MinValue(6, 4);
-        System.Console.WriteLine(minValue);
+        Console.WriteLine($"Min value fo two parameters: {minValue}");
+                
+        int maxValue1 = MaxValue(1, 2, 3);
+        Console.WriteLine($"Max value fo three parameters: {maxValue1}");
 
+        int minValue1 = MinValue(6, 4, 1);
+        Console.WriteLine($"Min value fo three parameters: {minValue1}");
+
+        int maxValue2 = MaxValue(1, 5, 3, 4);
+        Console.WriteLine($"Max value fo four parameters: {maxValue2}");
+
+        int minValue2 = MinValue(6, 4, 8, 7);
+        Console.WriteLine($"Min value fo four parameters: {minValue2}");
+
+        int sumResult;
+        TrySumIfOdd(2, 3, out sumResult);
+        Console.WriteLine($"sum of 2 numbers : {sumResult}");
+
+        //recursion        
+        Console.WriteLine(Repeat("bla", 3));
+
+         
     }
 }
