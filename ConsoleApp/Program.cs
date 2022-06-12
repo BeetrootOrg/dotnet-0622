4f2d1ad4b3d39c75f44a071939db6a139b718d57
artem <tradegordeev@gmail.com>
2022-06-13 01:10:09 +0300

    add hometask

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,33 +1,121 @@
 ﻿
-try
-{
-    System.Console.WriteLine("Введите число ");
-    int x = int.Parse(Console.ReadLine());
-    System.Console.WriteLine("Введите второе число ");
-    int y = int.Parse(Console.ReadLine());
+namespace Array {
 
-  int sum = 0;
-  int operand1 = x;
-  int   operand2 = y;
+class Program {
 
-    if (operand1 == operand2)
-    {
-        sum = operand1;
-    }
-    else if (y < x)
-    {
-        operand1 = y;
-        operand2 = x;
-    }
-        for (int i = operand1; i <= operand2; i++)
-        {
-            sum += i;
-        }
+static void Repeat(String s , int number) {
+if (number!=0) {
+System.Console.WriteLine(s);
+number--;
+Repeat(s,number);
+}
+}
 
-    Console.WriteLine("Cумма чисел = {0}", sum);
+static int MaxValue(int i, int x){
+int max = 0;
+if (i > x){
+ max = i;
+} else 
+max = x;
+return max;
+}
+
+ static int  MaxValue(int b, int c,int a){
+int max = 0;
+
+if (b > c & b > a){
+     max = b;
+}
+else if (c > b & c >a){
+     max = c;
+}
+else {
+     max = a;
+}
+return max;
 
 }
-catch (Exception)
-{
-    Console.WriteLine("Invalid input");
+ 
+ static int  MaxValue(int b, int c,int a,int t){
+     int max = 0;
+if ( (b > c & b > a & b >t)  ){
+     max = b;
 }
+else if ( (c > b & c > a & c >t)  ){
+     max = c;
+}
+else if ( (a > b & a > c & a >t)  ){
+     max = a;
+}
+else max = t;
+
+return max;
+
+}
+
+static int MinValue (int a,int b,int c){
+int min = 0;
+
+if (a < b & a < c ){
+     min  = a;
+}
+else if (b < a & b < c  ){
+     min  = b;
+}
+else if (c < a & c < b){
+     min  = c;
+}
+return min;
+
+}
+
+ static int MinValue (int a,int b,int c,int t){
+int min = 0;
+
+if (a < b & a < c & a<t){
+     min  = a;
+}
+else if (b < a & b < c & b < t ){
+     min  = b;
+}
+
+else if (c < a & c < b & c < t ){
+     min  = c;
+}
+
+else {
+     min = t;
+}
+return min;
+}
+
+static bool TrySumifOdd (int a, int b){
+int sum = 0;
+sum = a + b;
+
+if ( (sum%2)!=0 ){
+     return true;
+}
+
+return false;
+}
+
+
+     public static void Main (){
+
+          int max = MaxValue(6,1,2,3);
+          int min = MinValue(20,50,10);
+          bool odd = TrySumifOdd(2,3);
+
+          Repeat("str",3);
+
+          System.Console.WriteLine(odd);
+
+          System.Console.WriteLine(min);
+
+
+       
+               }
+     }
+}
+
