68217c039a3709c03edb3b539314202f6935df86
Oleksandr <sanelka77@yahoo.com>
2022-06-12 23:12:23 +0300

    result of operator

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,8 +1,18 @@
-﻿byte micro;
-short mini;
-int normal;
-long elongate;
-bool simple;
-float fract;
-double dualfract;
-decimal money;
+﻿byte micro = 3;
+short mini = 2;
+int normal = 10;
+long elongate = 123;
+bool simple = false;
+float fract = 0.8621235f;
+double dualfract = 681.546813218d;
+decimal money = 977.23546816812m;
+//addition
+System.Console.WriteLine(mini + micro);
+//subtraction
+System.Console.WriteLine(elongate - normal);
+//multiplication
+System.Console.WriteLine(dualfract * fract);
+//negation
+System.Console.WriteLine(!simple);
+//division
+System.Console.WriteLine(money / elongate);
