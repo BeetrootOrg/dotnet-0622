5adc615f09d35df2bb9cb965b2a5b73fdc31f1b6
Oleksandr <sanelka77@yahoo.com>
2022-06-12 23:21:03 +0300

    result of math functions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,10 @@ bool simple = false;
 float fract = 0.8621235f;
 double dualfract = 681.546813218d;
 decimal money = 977.23546816812m;
+
+int x = 37;
+int y = 64;
+
 //addition
 System.Console.WriteLine(mini + micro);
 //subtraction
@@ -16,3 +20,8 @@ System.Console.WriteLine(dualfract * fract);
 System.Console.WriteLine(!simple);
 //division
 System.Console.WriteLine(money / elongate);
+//Output to console result of several math functions:
+System.Console.WriteLine(-6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15);
+System.Console.WriteLine(Math.Abs(x) * Math.Sin(x));
+System.Console.WriteLine(2 * Math.PI * x);
+System.Console.WriteLine(Math.Max(x, y));
