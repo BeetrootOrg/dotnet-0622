f990ff425662226395bbeda020486de6d9bf1ed9
d.misik <misik.dmitriy@gmail.com>
2022-06-13 20:11:52 +0300

    chars

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -58,3 +58,8 @@ System.Console.WriteLine($"(5 + 6) * (4 + 5) = {Multiply(Sum(5, 6), Sum(4, 5))}"
 
 char c = 'c';
 System.Console.WriteLine((int)c);
+
+System.Console.WriteLine('c' + 'a');
+System.Console.WriteLine($"{c + 116}");
+System.Console.WriteLine("something" + c);
+System.Console.WriteLine(c + "something");
