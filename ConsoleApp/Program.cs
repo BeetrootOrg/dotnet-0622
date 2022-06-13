e7cdb068099d12615eb4816fbaff7abe32c3cc2d
d.misik <misik.dmitriy@gmail.com>
2022-06-13 19:46:23 +0300

    strings

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -33,3 +33,11 @@ int Multiply(int a) => a >= 10 ? a * 10 : a * 20;
 System.Console.WriteLine(MultiplyWorst(20));
 System.Console.WriteLine(MultiplyWorse(20));
 System.Console.WriteLine(Multiply(20));
+
+string str = "hello";
+string concatenated = "hello" + ", " + "Dima";
+concatenated = $"{str}, Dima";
+
+string example = "example    f";
+System.Console.WriteLine(example.TrimEnd('f'));
+System.Console.WriteLine(example.Substring(0, example.Length - 1));
