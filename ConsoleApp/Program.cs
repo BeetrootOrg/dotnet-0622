75894aa2c94da0aa9b96abece56e50c52569fd68
d.misik <misik.dmitriy@gmail.com>
2022-06-13 20:47:33 +0300

    factorial fix

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,12 @@ void WriteLine(int n)
     System.Console.Write(n);
 }
 
-int Factorial(int n) => n == 0 ? 1 : n * Factorial(n - 1);
+int Factorial(int n)
+{
+    if (n < 0) return -1;
+    if (n == 0) return 1;
+    return n * Factorial(n - 1);
+}
 // 2. create method int Factorial(int n) that will count n! = n * (n - 1) * (n - 2) * ... * 2 * 1, e.g. 5! = 5 * 4 * 3 * 2 * 1 = 120;
 
 WriteLine(9);
