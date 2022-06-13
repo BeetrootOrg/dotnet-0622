1c6d189ff4f5016578c5974ce30ede22bab8159f
d.misik <misik.dmitriy@gmail.com>
2022-06-13 20:23:27 +0300

    random

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,65 +1,26 @@
-﻿int MultiplyWorst(int a)
+﻿var random = new Random((int)DateTime.Now.Ticks);
+System.Console.WriteLine(random.Next());
+System.Console.WriteLine(random.Next(100));
+System.Console.WriteLine(random.Next(50, 150));
+
+System.Console.WriteLine(random.NextDouble());
+// from 30 to 100: [30; 100)
+System.Console.WriteLine(random.NextDouble() * 70 + 30); // random * (length of values) + minValue
+
+// from 0 to 100: [0; 100)
+System.Console.WriteLine(random.NextDouble() * 100);
+
+const double probability = 1;
+while (true)
 {
-    if (a >= 10)
+    var chance = random.NextDouble() * 100;
+    if (chance < probability)
     {
-        return a * 10;
+        System.Console.WriteLine("You're killed");
+        break;
     }
-    else if (a < 10)
+    else
     {
-        return a * 20;
+        System.Console.WriteLine("You're alive");
     }
-
-    return a;
 }
-
-int MultiplyWorse(int a)
-{
-    if (a >= 10)
-    {
-        return a * 10;
-    }
-
-    if (a < 10)
-    {
-        return a * 20;
-    }
-
-    return a;
-}
-
-
-int MultiplyBest(int a) => a >= 10 ? a * 10 : a * 20;
-int Multiply(int a, int b) => a * b;
-
-int Sum(int a, int b) => a + b;
-
-System.Console.WriteLine(MultiplyWorst(20));
-System.Console.WriteLine(MultiplyWorse(20));
-System.Console.WriteLine(MultiplyBest(20));
-
-string str = "hello";
-string concatenated = "hello" + ", " + "Dima";
-concatenated = $"{str}, Dima";
-
-string example = "example    f";
-System.Console.WriteLine(example.TrimEnd('f'));
-System.Console.WriteLine(example.Substring(0, example.Length - 1));
-
-System.Console.WriteLine(string.Format("{0}, {1}. {0}, {2}", "Hello", "Dima", "Class"));
-
-// (5 + 6) * (4 + 5)
-var result1 = Sum(5, 6);
-var result2 = Sum(4, 5);
-var result = Multiply(result1, result2);
-System.Console.WriteLine($"(5 + 6) * (4 + 5) = {result}");
-
-// the same as above
-System.Console.WriteLine($"(5 + 6) * (4 + 5) = {Multiply(Sum(5, 6), Sum(4, 5))}");
-
-char c = 'c';
-System.Console.WriteLine((int)c);
-
-System.Console.WriteLine('c' + 'a');
-System.Console.WriteLine($"{c + 116}");
-System.Console.WriteLine("something" + c);
-System.Console.WriteLine(c + "something");
