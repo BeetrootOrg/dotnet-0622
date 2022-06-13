f333913009e103c44489992fa2c43641417ffcc2
d.misik <misik.dmitriy@gmail.com>
2022-06-13 20:38:37 +0300

    writeline & factorial

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,31 +1,14 @@
-﻿var random = new Random((int)DateTime.Now.Ticks);
-System.Console.WriteLine(random.Next());
-System.Console.WriteLine(random.Next(100));
-System.Console.WriteLine(random.Next(50, 150));
-
-System.Console.WriteLine(random.NextDouble());
-// from 30 to 100: [30; 100)
-System.Console.WriteLine(random.NextDouble() * 70 + 30); // random * (length of values) + minValue
-
-// from 0 to 100: [0; 100)
-System.Console.WriteLine(random.NextDouble() * 100);
-
-const double probability = 100 / 6f;
-while (true)
+﻿// 1. create method void WriteLine(int n) and write to console numbers from 1 to n using recurssion
+void WriteLine(int n)
 {
-    var chance = random.NextDouble() * 100;
-    if (chance < probability)
-    {
-        System.Console.WriteLine("You're killed");
-        break;
-    }
-    else
-    {
-        System.Console.WriteLine("You're alive");
-    }
-
-    Console.ReadKey();
+    if (n <= 0) return;
+    WriteLine(n - 1);
+    System.Console.Write(n);
 }
 
-// 1. create method void WriteLine(int n) and write to console numbers from 1 to n using recurssion
+int Factorial(int n) => n == 0 ? 1 : n * Factorial(n - 1);
 // 2. create method int Factorial(int n) that will count n! = n * (n - 1) * (n - 2) * ... * 2 * 1, e.g. 5! = 5 * 4 * 3 * 2 * 1 = 120;
+
+WriteLine(9);
+System.Console.WriteLine();
+System.Console.WriteLine(Factorial(5));
