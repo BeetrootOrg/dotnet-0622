e2b382ba627c089e2114a233bcdb1668b68bb345
Danylo Strus <danylo.strus@gmail.com>
2022-06-13 20:40:15 +0300

    1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,16 @@
-﻿var num = Console.ReadLine();
+﻿System.Console.Write("Enter your text: ");
+var text = Console.ReadLine();
+System.Console.Write("Enter count: ");
+var num = Console.ReadLine();
 int.Parse(num);
 
 static void WR(int a)
 {
     
 }
+
+static string Repeat(string text, int count)
+    {
+        if (count <= 0) return "";
+        return text + "\n" + Repeat(text, count - 1);
+    }
