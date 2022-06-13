0179b00c60c6c5085da067cf4def220336a07217
d.misik <misik.dmitriy@gmail.com>
2022-06-13 20:47:46 +0300

    replace comment

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,13 +6,13 @@ void WriteLine(int n)
     System.Console.Write(n);
 }
 
+// 2. create method int Factorial(int n) that will count n! = n * (n - 1) * (n - 2) * ... * 2 * 1, e.g. 5! = 5 * 4 * 3 * 2 * 1 = 120;
 int Factorial(int n)
 {
     if (n < 0) return -1;
     if (n == 0) return 1;
     return n * Factorial(n - 1);
 }
-// 2. create method int Factorial(int n) that will count n! = n * (n - 1) * (n - 2) * ... * 2 * 1, e.g. 5! = 5 * 4 * 3 * 2 * 1 = 120;
 
 WriteLine(9);
 System.Console.WriteLine();
