b716108cd13826068032039e3389658d23537fa3
Tikhon Lavrov <Tykhon.Lavrov@koroka.solutions>
2022-06-13 20:31:03 +0300

    Classwork

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,53 +1,14 @@
-﻿internal class Program //Just feels more comfortable with Program and Main)
+﻿const int N = 10;
+var result = 0;
+int f1 = 1, f2 = 1;
+for (int i = f1; i <= N; ++i)
 {
-    static int CountSumBetweenIntegers(int fromNumber, int toNumber)
+    if (i == N && i != 1)
     {
-        int sum = 0;
-
-        for (int i = fromNumber; i <= toNumber; ++i)
-        {
-            sum += i;
-        }
-
-        return sum;
+        break;
     }
-    private static void Main(string[] args)
-    {
-        // Homework with Extra (User Input)
-        try
-        {
-            int sum = 0;
-            string finalOutput = "";
-
-            Console.WriteLine("Enter your X:");
-            string xStr = Console.ReadLine();
-            int x = int.Parse(xStr);
-
-            Console.WriteLine("Enter your Y:");
-            string yStr = Console.ReadLine();
-            int y = int.Parse(yStr);
-
-            Console.WriteLine($"X = {x}; Y = {y}");
-
-            if (x < y)
-            {
-                sum = CountSumBetweenIntegers(x, y);
-            }
-            else if (x > y)
-            {
-                sum = CountSumBetweenIntegers(y, x);
-            }
-            else
-            {
-                sum = x;
-            }
-
-            finalOutput = x != y ? $"Sum of numbers between {x} and {y} is {sum}" : $"Both X and Y are equal to {x}";
-            Console.WriteLine(finalOutput);
-        }
-        catch (FormatException)
-        {
-            Console.WriteLine("Invalid Input");
-        }
-    }
+    result += f1;
+    f1 = f2;
+    f2 = result;
 }
+System.Console.WriteLine(result);
