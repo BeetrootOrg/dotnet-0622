aec43c9429e950befce168acdbf83177fbf2a3c1
Anarik <106695465+Anariks@users.noreply.github.com>
2022-06-14 14:44:40 +0300

    selection and bubble

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,132 +1,61 @@
-﻿internal class Program 
+﻿internal class Program
 {
-    static int GetMaxValue(int a, int b)
-    {
-        return (a > b) ? a : b;
-    }
-
-        static int GetMinValue(int a, int b)
-    {
-        return (a > b) ? b : a;
-    }
-
-    static int GetMaxValue(int a, int b, int c)
-    {
-        int maxValue;
-        maxValue = GetMaxValue (a, b);
-        maxValue = (maxValue > c) ? GetMaxValue (a, b) : c;
-    
-        return maxValue;
-    }
-
-    static int GetMinValue(int a, int b, int c)
-    {
-        int minValue;
-        minValue = GetMinValue (a, b);
-        minValue = (minValue < c) ? GetMinValue (a, b) : c;
-
-        return minValue;
-    }
-
-    static int GetMaxValue(int a, int b, int c, int d)
-    {
-        int maxValue;
-        maxValue = GetMaxValue (a, b, c);
-        maxValue = (maxValue > d) ? maxValue : d;
-
-        return maxValue;
-    }
-
-   static int GetMinValue(int a, int b, int c, int d)
-    {
-        int minValue;
-        minValue = GetMinValue (a, b, c);
-        minValue = (minValue < d) ? minValue : d;
-
-        return minValue;
-    }
-    // TrySumIfOdd
-
-    static bool TrySumIfOdd(int x, int y, out int sum)
-    {
-        sum = Math.Min(x , y);
-
-        while (x != y)
+    static int[] Selection(int[] array)
         {
-            sum += (x > y) ? ++y : ++x;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+
+            }
+            return array;
         }
 
-        return sum % 2 != 0;
-        
-    }
+        static int[] Bubble(int[] array)
+        {
+            bool sorted;
+            int j = 0;
+            do {
+                sorted = false;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        sorted = true;
+                    }
+                    
+                }
+                j++;
+            }
+            while (j < array.Length && sorted);    
+            return array;
 
-    static bool Get2Values (out int x, out int y)
+        }
+
+        
+
+
+    
+    static void Main(string[] args)
     {
-        x = 0;
-        y = 0;
-        
-        // Entering x
-        Console.WriteLine("Hello, let's do some loops, please enter whole number x:");
+        int[] array = new int[] { 2, -3, 5, 4, 6, -7, 8 };
+        //Selection(array);
+        Bubble(array);
 
-        var console = Console.ReadLine();
-
-        if (!int.TryParse(console, out x))
-        { 
-            Console.WriteLine("x is not an integer number, run again and put right value");
-            return false;
-        }   
-
-        // Entering y
-        Console.WriteLine("Now enter whole number y:");
-
-        console = Console.ReadLine();
-
-        if (!int.TryParse(console, out y))
-        { 
-            Console.WriteLine("y is not an integer number, run again and put right value");
-            return false;
+        foreach (int item in array)
+        {
+            Console.WriteLine(item);
         }
-    return true;
     }
-
-    static string Repeat(string x, int n)
-        {   
-            if (n <= 0) return "";    
-            return x + "\n"+ Repeat(x, n-1);
-        }
-
-
-
-        static void Main(string[] args)
-        {   
-            int result;
-            result = GetMaxValue(49, -10);
-            Console.WriteLine("Maximum value is: " + result);
-            result = GetMinValue(42, 59);
-            Console.WriteLine("Minimum value is: " + result);
-
-            //Let's overload
-            Console.WriteLine($"Overloaded results:");
-
-            result = GetMaxValue(40, 51, 40);
-            Console.WriteLine("Maximum value is: " + result);
-            result = GetMinValue(40, 51, 3);
-            Console.WriteLine("Minimum value is: " + result);
-            result = GetMaxValue(40, 51, c: 40, 55);
-            Console.WriteLine("Maximum value is: " + result);
-            result = GetMinValue(40, 51, 3, -3);
-            Console.WriteLine("Minimum value is: " + result);
-
-            bool isInputOk;
-            isInputOk = (Get2Values(out int x,out int y));
-            Console.WriteLine($"x={x}, y={y}");
-
-            if (isInputOk) Console.WriteLine($"The TrySumIsOdd is: {TrySumIfOdd(x , y, out int sum)} because the sum is {sum}");
-            
-            string someText = "Some text that we gonna repeat";
-            int n = 3;
-            Console.WriteLine($"Hello, gonna do Repeat method and show you \"{someText}\" {n} times \n");
-            Console.WriteLine(Repeat(someText, n));
-        }
-
+        
 }
