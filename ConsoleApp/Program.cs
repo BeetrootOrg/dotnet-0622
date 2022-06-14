fdce63f78a056d57830337b0ffc81ca3ec3e6c25
Andriy.Gulpa <andriy.gulpa@gmail.com>
2022-06-14 20:25:01 +0300

    update

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,13 +1,46 @@
-﻿static  void Helloworld()
+﻿
+ 
+
+System.Console.WriteLine("Input first value: ");
+var nx = Console.ReadLine();
+var x = int.Parse(nx);
+
+System.Console.WriteLine("second value: ");
+var xy = Console.ReadLine();
+var y = int.Parse(xy);
+
+System.Console.WriteLine("third value: ");
+var xc = Console.ReadLine();
+var c = int.Parse(xc);
+
+/*System.Console.WriteLine("forth value: ");
+var xd = Console.ReadLine();
+var yd = int.Parse(xd);*/
+
+
+//Max Value from 2 number
+
+static int MaxValue(int a, int b)
 {
-    System.Console.WriteLine("Hello World"); 
+    return (a > b) ? a : b;
 }
 
-static void HEloToSomeBody(string somebady)
+System.Console.WriteLine($"Max value from two number {MaxValue(x, y)}");
+
+//Min value from 2 number
+
+static int MinValuetwo(int a, int b)
 {
-    System.Console.WriteLine($"Hello {somebady}"); 
+    return (a < b) ? a : b;
 }
-Helloworld();
-HEloToSomeBody(Console.ReadLine());
-HEloToSomeBody("Dima");
-HEloToSomeBody("CLASS");
+
+System.Console.WriteLine($"Min value from two number {MinValuetwo(x, y)}");
+
+
+static int MaxValuethre(int a, int b, int c)
+{
+    if (MaxValue > c) return (c);
+    return(MaxValue);
+}
+
+System.Console.WriteLine($"Max value from two number {MaxValuethre(x, y)}");
