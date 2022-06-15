e92221a553018456ab32776e967ff91f6bf0e3e2
Danylo Strus <danylo.strus@gmail.com>
2022-06-15 19:02:37 +0300

    1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,13 +1,1 @@
-﻿System.Console.Write("Enter your text: ");
-var text = Console.ReadLine();
-System.Console.Write("Enter count: ");
-int num = Convert.ToInt32(Console.ReadLine());
-WR(num,text);
-
-static void WR(int num, string text)
-{
-    if (num >= 0)  System.Console.WriteLine("");
-    System.Console.WriteLine(text);
-    WR(num -1,text);
-}
-
+﻿
