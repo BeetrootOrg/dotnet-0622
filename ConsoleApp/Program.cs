f602de15ef7406b21dda86bae96b643ff05dcf75
ztgarry <as.tkachuk@gmail.com>
2022-06-15 16:18:49 +0300

    d

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,35 +1,29 @@
-﻿/*
-Define and call with different parameters next methods:
-
-    Method that will return max value among all the parameters
-    … min value …
-    Method TrySumIfOdd that accepts 2 parameters and returns true if sum of numbers between inputs is odd, otherwise false, sum return as out parameter
-    Overload first two methods with 3 and 4 parameters
-
-*/
+﻿
+int numb1, numb2;
 
 
 
-int number1 = 7;
-int number2 = 19;
-
-//медод получил 2 числа сравнит их и скажет какое больше
-static int MM1(int number1, int number2)
-
+static int MaxMath(int numb1, int numb2)
 {
-if (number1 > 0)
+int result;
+if ( numb1 > numb2 )
 {
-   return number1;
-
+   result = numb1;
+   
 }
+
+else
+{
+result = numb2;
+}
+
+return result;
 
 }
 
 
-MM1(number1, number2);
+ numb1 = int.Parse(Console.ReadLine());
+ numb2 = int.Parse(Console.ReadLine());
+ //MaxMath(numb1, numb2);
 
-
-
-
-//System.Console.WriteLine(MethodMax(number1));
-
+System.Console.WriteLine(MaxMath(numb1, numb2));
