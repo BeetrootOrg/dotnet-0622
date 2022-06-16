33a1deb399604c02ae303d678b40598401154552
ztgarry <as.tkachuk@gmail.com>
2022-06-16 13:47:50 +0300

    1-2-3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -23,7 +23,6 @@ else
 result = numb2;
 }
 return result;
-
 }
 
 
@@ -46,8 +45,6 @@ return result;
  int numb1, numb2;
  numb1 = int.Parse(Console.ReadLine());
  numb2 = int.Parse(Console.ReadLine());
-// MaxMath(numb1, numb2);
-
 System.Console.WriteLine(MaxMath(numb1, numb2)); 
 System.Console.WriteLine(MinMath(numb1, numb2)); 
 
@@ -67,57 +64,37 @@ if (param1 > param2)
 {
     startValue = param2;
     endValue = param1;
-    System.Console.WriteLine("param2 < param1");
 }
 else if (param1 < param2)
 {
     startValue = param1;
     endValue = param2;
-    System.Console.WriteLine("param1 < param2");
-
 }
 else
 {
     System.Console.WriteLine("both params are equal");
-
 }
 
 int countTimes = endValue - startValue;
-
 for (int i = 0; i <= countTimes; ++i)
 {
     sum = sum + startValue;
     startValue = startValue + 1; 
 }
-//System.Console.WriteLine(sum);
+
 
  if (sum % 2 == 0)
         {
-            //result = number / 3;
-            System.Console.WriteLine("повернулось четное false");
-            System.Console.WriteLine(sum / 2);
+            System.Console.WriteLine("результат false");
             return false;
-
         }
-
-        //result = 0;
-        System.Console.WriteLine("повернулось нечетное true");
-        System.Console.WriteLine(sum / 2);
-
+        System.Console.WriteLine("результат true");
         return true;
-        
 }
 
-
-
-
-
-
-
-
 int sumBetweenTwo = 0;
-
 int param1 = 10;
 int param2 = 12;
 int countTimes = 0; // количество итераций цикла, которое посути равно разнице между значениями, которые ввел юзер
 TrySumIfOdd(param1, param2, out sumBetweenTwo);
+System.Console.WriteLine(sumBetweenTwo);
