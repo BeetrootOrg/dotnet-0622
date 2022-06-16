f50f8ec2cd19dfe7ba733503ebd5b7beac6adde8
Danylo Strus <danylo.strus@gmail.com>
2022-06-16 11:27:33 +0300

    BubbleSortArray

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,30 +1,48 @@
-﻿void WriteArr(int[] arr)
+﻿
+void PrintArray(params int[] array)
 {
-    foreach (var elem in arr)
+    foreach (var item in array)
     {
-        System.Console.WriteLine(elem);
+        System.Console.Write($"{item} ");
     }
+    System.Console.WriteLine();
 }
-
-void WriteArrFr(int[] arr)
+int[] BubbleSortArray(int[] array)
 {
-   for (var index = 0; index < arr.Length; index++)
-   {
-    System.Console.WriteLine(arr[index]);
-   }
-}
-void ArrRecurssion(int[] arr)
+    var tempArray = new int[array.Length];
+    Array.Copy(array, tempArray, array.Length);
+    int tempVariable;
+    for (int i = 0; i < tempArray.Length - 1; i++)
+    {
+        for (int j = i + 1; j < tempArray.Length; j++)
+        {
+            if (tempArray[i] > tempArray[j])
+            {
+                tempVariable = tempArray[i];
+                tempArray[i] = tempArray[j];
+                tempArray[j] = tempVariable;
+            }
+        }
+    }
+    return tempArray;
+};
+
+int[] SortArrayForKinds(int[] array)
 {
-    
-}
+    var tempArray = new int[array.Length];
+    Array.Copy(array, tempArray, array.Length);
+    Array.Sort(tempArray);
+    return tempArray;
+};
+var initialArray = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+var initialArrayReverse = new[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+var initialArrayRandom = new[] { 26, 12, 6, 34, 18, 3, 2, 20, 0, 98 };
 
-int[] array = { 1, 2, 3 };
-
-var array2 = new[] { 1, 2, 3 };//+
-var array3 = new int[] { 1, 2, 3 };//++
-
-var array4 = new int[3] { 1, 2, 3 };
-var array5 = new int[3];
-
-WriteArr(array);
-WriteArrFr(array2);
+System.Console.WriteLine("initialArray");
+PrintArray(initialArray);
+System.Console.WriteLine("SortArrayForKinds");
+var mass1 = SortArrayForKinds(initialArray);
+PrintArray(mass1);
+System.Console.WriteLine("BubbleSortArray");
+var mass2 = BubbleSortArray(initialArray); //initialArrayReverse  initialArrayRandom
+PrintArray(mass2);
