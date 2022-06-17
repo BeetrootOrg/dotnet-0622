a7d62751032320b0fdb93238b0bf8a174a2f2d4e
Oleksandr <sanelka77@yahoo.com>
2022-06-18 00:24:04 +0300

    base task part 1 - 4

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,4 @@
-﻿
-// 04 - Methods
+﻿// 04 - Methods
 // HOMEWORK
 // Not your boring middle school homework — these tasks will help you in real life! Have fun with your assignment for this topic:
 //
@@ -106,13 +105,52 @@ internal class Program
         }
         return min;
     }
+
+    static bool TrySumIfOdd(int a, int b, out long res)
+    {
+        bool succ = false;
+        long num = 0;
+        res = 0;
+        if (a < b)
+        {
+            num = b;
+            while(num >= a)
+            {
+                res += num;
+                --num;
+            }
+        }
+        else
+        {
+            num = a;
+            while(num >= b)
+            {
+                res += num;
+                --num;
+            }
+        }
+        if (res % 2 != 0)
+        {
+            succ = true;
+        }
+        else
+        {
+            succ = false;
+        }
+        return succ;
+    }
+    
     private static void Main(string[] args)
     {
+        long res;
         Console.WriteLine(Define_Max(23, 76));
         Console.WriteLine(Define_Max(23, 76, 88.88f));
         Console.WriteLine(Define_Max(23, 76, 88.88f, 99.99));
         Console.WriteLine(Define_Min(99, 77));
         Console.WriteLine(Define_Min(99, 77, 55.55f));
         Console.WriteLine(Define_Min(99, 77, 55.55f, 33.33));
+
+        Console.WriteLine($"{TrySumIfOdd(1, 2, out res)}, res = {res}");
+        Console.WriteLine($"res = {res}, {TrySumIfOdd(1, 2, out res)}");
     }
 }
