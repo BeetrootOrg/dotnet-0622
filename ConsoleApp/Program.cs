0578ff5daa79621a3dde39a6ace2f1c7240afbbf
AndryM <103665742+AndryMinoMax@users.noreply.github.com>
2022-06-17 19:05:12 +0300

    HomeWork3

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+//Homework :)
+
+string x1;
+string y1;
+Console.WriteLine("input x");
+x1= Console.ReadLine();
+Console.WriteLine("input y");
+y1 = Console.ReadLine();
+
+int x = Int32.Parse(x1);
+int y = Int32.Parse(y1);
+int sum = 0;
+if (x < y)
+{
+    while (x <= y)
+    {
+        sum = sum + x;
+        x++;
+
+    }
+}
+else if (x > y)
+{
+
+    while (y <= x)
+    {
+        sum = sum + y;
+        y++;
+    }
+}
+else
+{
+    sum = x;
+}
+
+Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
