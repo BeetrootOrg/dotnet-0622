898805b7033d470b8f8b55b84aa5fd12a79a9569
d.misik <misik.dmitriy@gmail.com>
2022-06-18 10:15:30 +0300

    comparison

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,3 +9,14 @@ System.Console.WriteLine(c2);
 var c3 = '\n';
 System.Console.WriteLine(c3);
 System.Console.WriteLine("new line above");
+
+// random symbol
+var c4 = '\u05F0';
+System.Console.WriteLine(c4);
+
+// equals
+System.Console.WriteLine("EQUALITY");
+System.Console.WriteLine('a' == 'a');
+System.Console.WriteLine('b' > 'a');
+System.Console.WriteLine('a' < 'c');
+System.Console.WriteLine('A' < 'a');
