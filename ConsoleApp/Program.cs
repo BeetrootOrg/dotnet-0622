360284544a3bf4e58c55ced2d2ab381885f41a3c
d.misik <misik.dmitriy@gmail.com>
2022-06-18 10:22:46 +0300

    char check

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,16 @@
-﻿// the same below
+﻿void CharCheck(char symbol)
+{
+    System.Console.WriteLine($"Checking symbol {symbol}:");
+    System.Console.WriteLine($"\t- Is ASCII = {char.IsAscii(symbol)}");
+    System.Console.WriteLine($"\t- Is Digit = {char.IsDigit(symbol)}");
+    System.Console.WriteLine($"\t- Is Letter = {char.IsLetter(symbol)}");
+    System.Console.WriteLine($"\t- Is Lower = {char.IsLower(symbol)}");
+    System.Console.WriteLine($"\t- Is Upper = {char.IsUpper(symbol)}");
+    System.Console.WriteLine($"\t- Is Number = {char.IsNumber(symbol)}");
+    System.Console.WriteLine($"\t- Is Punctuation = {char.IsPunctuation(symbol)}");
+}
+
+// the same below
 var c1 = 'a';
 var c2 = '\u0061';
 
@@ -20,3 +32,11 @@ System.Console.WriteLine('a' == 'a');
 System.Console.WriteLine('b' > 'a');
 System.Console.WriteLine('a' < 'c');
 System.Console.WriteLine('A' < 'a');
+
+// check chars
+CharCheck('a');
+CharCheck('A');
+CharCheck('1');
+CharCheck(',');
+CharCheck('/');
+CharCheck('\u1234');
