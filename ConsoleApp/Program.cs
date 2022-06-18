31cd26f063d7da915cbfdfd0fe1c19d9dd9ad2fd
d.misik <misik.dmitriy@gmail.com>
2022-06-18 10:54:44 +0300

    methods

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,37 +1,42 @@
-﻿void CharCheck(char symbol)
+﻿using System;
+using System.Threading;
+
+using static System.Console;
+
+void CharCheck(char symbol)
 {
-    System.Console.WriteLine($"Checking symbol {symbol}:");
-    System.Console.WriteLine($"\t- Is ASCII = {char.IsAscii(symbol)}");
-    System.Console.WriteLine($"\t- Is Digit = {char.IsDigit(symbol)}");
-    System.Console.WriteLine($"\t- Is Letter = {char.IsLetter(symbol)}");
-    System.Console.WriteLine($"\t- Is Lower = {char.IsLower(symbol)}");
-    System.Console.WriteLine($"\t- Is Upper = {char.IsUpper(symbol)}");
-    System.Console.WriteLine($"\t- Is Number = {char.IsNumber(symbol)}");
-    System.Console.WriteLine($"\t- Is Punctuation = {char.IsPunctuation(symbol)}");
+    WriteLine($"Checking symbol {symbol}:");
+    WriteLine($"\t- Is ASCII = {char.IsAscii(symbol)}");
+    WriteLine($"\t- Is Digit = {char.IsDigit(symbol)}");
+    WriteLine($"\t- Is Letter = {char.IsLetter(symbol)}");
+    WriteLine($"\t- Is Lower = {char.IsLower(symbol)}");
+    WriteLine($"\t- Is Upper = {char.IsUpper(symbol)}");
+    WriteLine($"\t- Is Number = {char.IsNumber(symbol)}");
+    WriteLine($"\t- Is Punctuation = {char.IsPunctuation(symbol)}");
 }
 
 // the same below
 var c1 = 'a';
 var c2 = '\u0061';
 
-System.Console.WriteLine(c1);
-System.Console.WriteLine(c2);
+WriteLine(c1);
+WriteLine(c2);
 
 // new line
 var c3 = '\n';
-System.Console.WriteLine(c3);
-System.Console.WriteLine("new line above");
+WriteLine(c3);
+WriteLine("new line above");
 
 // random symbol
 var c4 = '\u05F0';
-System.Console.WriteLine(c4);
+WriteLine(c4);
 
 // equals
-System.Console.WriteLine("EQUALITY");
-System.Console.WriteLine('a' == 'a');
-System.Console.WriteLine('b' > 'a');
-System.Console.WriteLine('a' < 'c');
-System.Console.WriteLine('A' < 'a');
+WriteLine("EQUALITY");
+WriteLine('a' == 'a');
+WriteLine('b' > 'a');
+WriteLine('a' < 'c');
+WriteLine('A' < 'a');
 
 // check chars
 CharCheck('a');
@@ -42,12 +47,12 @@ CharCheck('/');
 CharCheck('\u1234');
 
 // transformations
-System.Console.WriteLine("TRANSFORMATIONS");
-System.Console.WriteLine(char.ToLower('A'));
-System.Console.WriteLine(char.ToUpper('a'));
+WriteLine("TRANSFORMATIONS");
+WriteLine(char.ToLower('A'));
+WriteLine(char.ToUpper('a'));
 
 var str = "some string";
-System.Console.WriteLine(str);
+WriteLine(str);
 
 // new line string
 var str1 = "Long text\r\n" + "next line.";
@@ -55,24 +60,40 @@ var str2 = "Long text\r\nnext line.";
 var str3 = @"Long text
 next line.";
 
-System.Console.WriteLine(str1 == str2);
-System.Console.WriteLine(str2 == str3);
+WriteLine(str1 == str2);
+WriteLine(str2 == str3);
 
 // put variable in string
 var a = 1;
 var b = 2;
 var sum = a + b;
 
-System.Console.WriteLine("SUM");
-System.Console.WriteLine(a + "+" + b + "=" + sum);
-System.Console.WriteLine(string.Format("{0}+{1}={2}", a, b, sum));
-System.Console.WriteLine($"{a}+{b}={sum}");
+WriteLine("SUM");
+WriteLine(a + "+" + b + "=" + sum);
+WriteLine(string.Format("{0}+{1}={2}", a, b, sum));
+WriteLine($"{a}+{b}={sum}");
 
-System.Console.WriteLine("BACKSLASH");
-System.Console.WriteLine(@"\");
-System.Console.WriteLine("\\");
-System.Console.WriteLine("\\n");
-System.Console.WriteLine(@"\n");
-System.Console.WriteLine($"{'\\'}{'n'}");
+WriteLine("BACKSLASH");
+WriteLine(@"\");
+WriteLine("\\");
+WriteLine("\\n");
+WriteLine(@"\n");
+WriteLine($"{'\\'}{'n'}");
 
 // methods in string
+var test1 = "test";
+var test2 = "TEST";
+WriteLine($"Testing string {test1}");
+WriteLine($"\tClone() = {test1.Clone()}");
+WriteLine($"\t't'.CompareTo('T') = {'t'.CompareTo('T')}");
+WriteLine($"\tCompareTo('{test2}') = {test1.CompareTo(test2)}");
+WriteLine($"\tContains('e') = {test1.Contains('e')}");
+WriteLine($"\tContains('te') = {test1.Contains("te")}");
+WriteLine($"\tContains('b') = {test1.Contains('b')}");
+WriteLine($"\tContains('be') = {test1.Contains("be")}");
+WriteLine($"\tEndsWith('st') = {test1.EndsWith("st")}");
+WriteLine($"\tEndsWith('ST') = {test1.EndsWith("ST")}");
+WriteLine($"\tEndsWith('ST', OrdinalIgnoreCase) = {test1.EndsWith("ST", StringComparison.OrdinalIgnoreCase)}");
+
+WriteLine(Thread.CurrentThread.CurrentCulture);
+WriteLine(DateTime.Now.ToString());
