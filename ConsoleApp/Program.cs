b1201e1b2d0a562b76e2ccfc9b3f5a1b5f12a713
Oles Borys <oles.borys@gmail.com>
2022-06-19 17:27:58 +0200

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,71 +1,19 @@
-﻿internal class Program
+﻿int[] BubbleSort (int[] arr)
 {
-    static int ProvideMaxValue (int number1, int number2) 
+    var copy = new int[arr.Length];
+    Array.Copy(arr, copy, arr.Length);
+    var iterations = copy.Length;
+    for (var i = 0; i < iterations - 1; i++)
     {
-        if (number1 >= number2)
+        for (var index = 0; index < iterations - i - 1; index++)
         {
-            return number1;
-        }
-        return number2;
-    }
-    static int ProvideMaxValue (int number1, int number2, int number3, int number4)
-    {
-        if (ProvideMaxValue (number1, number2) >= ProvideMaxValue(number3, number4))
-        {
-            return ProvideMaxValue (number1, number2);
-        }
-        return ProvideMaxValue(number3, number4);
-    }
-    static int ProvideMinValue (int number1, int number2)
-    {
-        if (number1 <= number2)
-        {
-            return number1;
-        }
-        return number2;
-
-    }
-    static int ProvideMinValue (int number1, int number2, int number3, int number4)
-    {
-        if (ProvideMinValue(number1, number2) <= ProvideMinValue(number3, number4))
-        {
-            return ProvideMinValue(number1, number2);
-        }
-        return ProvideMinValue(number3, number4);
-    }
-    static bool TrySumIfOdd (int number1, int number2, out int sum)
-    {
-        int sum1 = number1 + number2;
-        if (sum1 % 2 == 0)
-        {
-            sum = 0;
-            return false;
-        }
-        else
-        {
-            sum = sum1;
-            return true;
+            if (copy[index] > copy[index + 1])
+            {
+                var temp = copy[index];
+                copy[index] = copy[index + 1]; 
+                copy[index + 1] = temp;
+            }
         }
     }
-    static string Repeat (string x, int y)
-    {
-       if (y <= 0) return "Incorrect number of amounts";
-       if (y == 1) return x;
-       return x + Repeat(x, --y);
-    }
-    private static void Main(string[] args)
-    {
-        int biggerNumber = ProvideMaxValue(14, 12);
-        Console.WriteLine(biggerNumber);
-        int smallerNumber = ProvideMinValue(14, 12);
-        Console.WriteLine(smallerNumber);
-        bool result = TrySumIfOdd(7,4, out var sum);
-        Console.WriteLine($"{result}, {sum}");
-        int evenBiggerNumber = ProvideMaxValue(10, 12, 14, 15);
-        Console.WriteLine(evenBiggerNumber);
-        int evenSmallerNumber = ProvideMinValue(10, 12, 14, 15);
-        Console.WriteLine(evenSmallerNumber);
-        string something = Repeat("str", 10);
-        Console.WriteLine(something);
-    }
+    return copy;
 }
