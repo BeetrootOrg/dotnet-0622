0029bc66ff7437d3ffca7a5f68580781b512413b
d.misik <misik.dmitriy@gmail.com>
2022-06-19 17:33:39 +0300

    bin serializer

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,25 +1,7 @@
-﻿using System;
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
-void PrintArray(params int[] array)
-{
-    Console.Write(string.Join(",", array));
-    Console.WriteLine();
-}
-
-int[] Sort(int[] arr)
-{
-    // algorithm goes here
-    return arr;
-}
-
-var arr1 = new[] { 1, 2, 3, 4, 5 };
-var arr2 = new[] { 5, 4, 3, 2, 1 };
-var arr3 = new[] { 3, 1, 4, 5, 2 };
-var arr4 = Array.Empty<int>();
-var arr5 = new[] { 1, 1, 1, 1, 1 };
-
-PrintArray(Sort(arr1));
-PrintArray(Sort(arr2));
-PrintArray(Sort(arr3));
-PrintArray(Sort(arr4));
-PrintArray(Sort(arr5));
+var randomArray = new[] { (1, true), (2, false) };
+var binaryFormatter = new BinaryFormatter();
+using var file = File.OpenWrite("text.bin");
+binaryFormatter.Serialize(file, randomArray);
