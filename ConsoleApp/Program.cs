c369df3cc3e9e26772129af88f2614111e81bedc
d.misik <misik.dmitriy@gmail.com>
2022-06-20 19:35:30 +0300

    main menu

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,40 +1,15 @@
-﻿using System;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using static System.Console;
 
-void WriteArray(byte[] arr)
+void MainMenu()
 {
-    Console.WriteLine($"Length: {arr.Length}");
-    Console.WriteLine(string.Join(", ", arr));
+    WriteLine("Welcome to phone book!");
+    WriteLine();
+    WriteLine("Menu:");
+    WriteLine("\t1 - Show all contacts");
+    WriteLine("\t2 - Add new contact");
+    WriteLine("\t3 - Update contact");
+    WriteLine("\t4 - Remove contact");
+    WriteLine("\t0 - Exit");
 }
 
-var text = @"Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-Impedit iste nihil, soluta illum mollitia excepturi voluptatum officiis dignissimos 
-libero dicta ut atque aut dolore alias molestiae adipisci debitis. 
-Voluptatum repellendus ipsam cumque fugit quisquam accusantium laborum delectus, 
-unde ullam nesciunt. Iure aut cumque, illum dicta accusamus atque ipsum et provident.";
-
-var bytes = Encoding.UTF8.GetBytes(text);
-WriteArray(bytes);
-
-var result = Encoding.UTF8.GetString(bytes);
-Console.WriteLine(result);
-
-var regex = new Regex(@"^[-]*\d+$");
-var strings = new[]
-{
-    "",
-    "-",
-    "1",
-    "123",
-    "000",
-    "abc",
-    "-1",
-    "---1",
-    "12a"
-};
-
-foreach (var item in strings)
-{
-    Console.WriteLine($"{item}: {regex.IsMatch(item)}");
-}
+MainMenu();
