0e9d9e6c50137100d10b060931350c8182256465
d.misik <misik.dmitriy@gmail.com>
2022-06-22 20:40:04 +0300

    methods

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,31 +1,31 @@
 ﻿using System;
 
-using static System.Console;
-
-string buffer;
-WriteLine("X:");
-buffer = ReadLine();
-
-if (!int.TryParse(buffer, out var x))
+internal class Program
 {
-    WriteLine("Invalid input");
-    return;
+    static int Max(int a, int b) => a < b ? b : a;
+    static int Max(int a, int b, int c) => Max(Max(a, b), c);
+    static int Max(int a, int b, int c, int d) => Max(Max(a, b, c), d);
+
+    static bool TrySumIfOdd(int a, int b, out int sum)
+    {
+        sum = a + b;
+        return sum % 2 == 1;
+    }
+
+    static string Repeat(string str, int num)
+    {
+        if (str == null) return null;
+        if (str == string.Empty) return string.Empty;
+        if (num < 0) return null;
+        if (num == 0) return string.Empty;
+        return str + Repeat(str, num - 1);
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Repeat("str", 3));
+        Console.WriteLine(Repeat("str", 1));
+        Console.WriteLine(Repeat("str", 0));
+        Console.WriteLine(Repeat("str", -1));
+    }
 }
-
-WriteLine("Y:");
-buffer = ReadLine();
-
-if (!int.TryParse(buffer, out var y))
-{
-    WriteLine("Invalid input");
-    return;
-}
-
-int sum = 0;
-var max = Math.Max(x, y);
-for (var i = Math.Min(x, y); i <= max; i++)
-{
-    sum += i;
-}
-
-WriteLine($"Sum = {sum}");
