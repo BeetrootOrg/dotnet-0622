b82bfcd5188a1ac63b5bc06ab6aabe88e61fab5c
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-06-22 13:59:54 +0300

    InputArray()

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,21 +2,18 @@
 {
     static int[] QuickSort(int[] array, int minIndex, int maxIndex)
     {
-        if (minIndex >= maxIndex) return array;
-            
-        int pivotIndex = GetPivotIndex(array, minIndex, maxIndex);
+        if (minIndex >= maxIndex) return array; 
+        int pivotIndex = GetPivot(array, minIndex, maxIndex);
         //left sort
         QuickSort(array, minIndex, pivotIndex - 1);
         //right sort
         QuickSort(array, pivotIndex + 1, maxIndex);
-
         return array;
     }
 
-    static int GetPivotIndex(int[] array, int minIndex, int maxIndex)
+    static int GetPivot(int[] array, int minIndex, int maxIndex)
     {
         int pivot = minIndex - 1;
-
         for (int i = minIndex; i <= maxIndex; i++)
         {
             if (array[i] < array[maxIndex])
@@ -28,32 +25,39 @@
 
         pivot++;
         Swap(ref array[pivot], ref array[maxIndex]);
-
         return pivot;
     }
 
     static void Swap(ref int leftItem, ref int rightItem)
     {
         int temp = leftItem;
-
         leftItem = rightItem;
-
         rightItem = temp;
+    }
+
+    static int[] InputArray() 
+    {
+        Console.Write("Input size array: ");
+        int n = Convert.ToInt16(Console.ReadLine());
+        int[] inputArray = new int[n];
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++) inputArray[i] = rnd.Next(-10, 10);
+
+        Console.WriteLine("Disordered array: ");
+        foreach (int element in inputArray) Console.Write($"{element}, ");
+        return inputArray;
     }
         
     static void Main()
     {
-        Console.Write("Input size array: ");
-        int n = Convert.ToInt16(Console.ReadLine());
-        int[]  inputArray = new int[n];
-        Random rnd = new Random();
-        for (int i = 0; i < n; i++) inputArray[i] = rnd.Next(-10, 10);
         
-        Console.WriteLine($"Disordered array: {string.Join(", ", inputArray)}");
-
+        int[] inputArray =  InputArray();
         int[] sortedArray = QuickSort(inputArray, 0, inputArray.Length - 1);
+        System.Console.WriteLine();
+        Console.WriteLine("Sordered array: ");
+        foreach (int element in sortedArray) Console.Write($"{element}, ");
 
-        Console.WriteLine($"Sorted array: {string.Join(", ", sortedArray)}");
+        Console.ReadKey();
     }
 
 }
