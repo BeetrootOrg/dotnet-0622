6287f474ec0c74804877490c7d156a57a7fd0b11
Oles Borys <oles.borys@gmail.com>
2022-06-22 16:53:18 +0200

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,79 +1,169 @@
 ﻿using System;
-bool Compare (string str1, string str2)
+using System.IO;
+using System.Text;
+using static System.Console;
+const string filename = "data.csv";
+
+void Exit()
 {
-    if (str1.Length != str2.Length)
-    {
-        return false;
-    } 
-    for (var item = 0; item < str1.Length; item++)
-    {
-        if (str1[item] != str2[item])
-        {
-            return false;
-        }
-        
-    }
-    return true;
+    Environment.Exit(0);
 }
 
-(int alphabeticChars, int digits, int specChar) Analyze (string str)
+void ShowRow((string, string, string) row)
 {
-    int charCount = 0;
-    int didgitCount = 0;
-    int specCount = 0;
-    for (var item = 0; item < str.Length; item++)
+    var (firstName, lastName, phone) = row;
+    WriteLine("{0,-15} {1,-15} {2,-15}", firstName, lastName, phone);
+}
+
+(string, string, string)[] ReadContacts(string file)
+{
+    var lines = File.ReadAllLines(file);
+    var contacts = new (string, string, string)[lines.Length];
+    for (int i = 0; i < lines.Length; i++)
     {
-        
-        if (char.IsLetter(str[item]))
+        var items = lines[i].Split(',');
+        contacts[i] = (items[0], items[1], items[2]);
+    }
+
+    return contacts;
+}
+
+void ShowAll()
+{
+    // 1. read content from file
+    // 2. iterate in array of contacts
+    // 3. show contact rows
+    Clear();
+
+    var contacts = ReadContacts(filename);
+
+    ShowRow(("First Name", "Last Name", "Phone"));
+    foreach (var contact in contacts)
+    {
+        ShowRow(contact);
+    }
+
+    WriteLine("Press any key to continue...");
+    ReadKey();
+}
+
+void SearchContact()
+{
+    Clear();
+
+    WriteLine("Enter your search request: ");
+    var searchSequence = ReadLine();
+    var contacts = ReadContacts(filename);
+    ShowRow(("First Name", "Last Name", "Phone"));
+    foreach (var contact in contacts)
+    {
+        if (contact.Item1.Equals(searchSequence) || contact.Item2.Equals(searchSequence) || contact.Item3.Equals(searchSequence))
         {
-            charCount++;
+            ShowRow(contact);
         }
-        else if (char.IsNumber(str[item]))
+    }
+    WriteLine ("Please find your search result above, press any key to continue...");
+    ReadKey();
+}
+
+string Serialize((string firstName, string lastName, string phone) row) => $"{row.firstName},{row.lastName},{row.phone}";
+
+void AddNewContact()
+{
+    Clear();
+
+    WriteLine("Enter first name:");
+    var firstName = Console.ReadLine();
+
+    WriteLine("Enter last name:");
+    var lastName = Console.ReadLine();
+
+    WriteLine("Enter phone:");
+    var phone = Console.ReadLine();
+
+    File.AppendAllLines(filename, new[] { Serialize((firstName, lastName, phone)) });
+
+    WriteLine("Contact saved, press any key to continue");
+    ReadKey();
+}
+
+void RemoveContact()
+{
+    Clear();
+
+    WriteLine("Enter phone to remove:");
+    var phoneToRemove = Console.ReadLine();
+
+    var contacts = ReadContacts(filename);
+    var newContacts = new (string, string, string)[contacts.Length];
+    Array.Copy(contacts, newContacts, contacts.Length);
+
+    var index = 0;
+
+    while (index < newContacts.Length)
+    {
+        if (newContacts[index].Item3 == phoneToRemove)
         {
-            didgitCount++;
+            newContacts[index] = newContacts[^1];
+            Array.Resize(ref newContacts, newContacts.Length - 1);
         }
         else
         {
-            specCount++;
+            ++index;
         }
     }
-    return (charCount, didgitCount, specCount);
+
+    var csvBuilder = new StringBuilder();
+    foreach (var contact in newContacts)
+    {
+        csvBuilder.AppendLine(Serialize(contact));
+    }
+
+    File.WriteAllText(filename, csvBuilder.ToString());
+
+    WriteLine($"{contacts.Length - newContacts.Length} Contact(s) removed, press any key to continue");
+    ReadKey();
 }
-string Sort (string str)
+
+void MainMenu()
 {
-    str = str.ToLower();
-    var arr = str.ToCharArray();
-    Array.Sort(arr);
-    var str1 = string.Empty;
-    str1 = string.Join("", arr);
-    return str1;
+    Clear();
+
+    WriteLine("Welcome to phone book!");
+    WriteLine();
+    WriteLine("Menu:");
+    WriteLine("\t1 - Show all contacts");
+    WriteLine("\t2 - Add new contact");
+    WriteLine("\t3 - Update contact");
+    WriteLine("\t4 - Remove contact");
+    WriteLine("\t5 - Search by First Name, Second Name or Phone Number");
+    WriteLine("\t0 - Exit");
+
+    var key = ReadKey();
+
+    switch (key.Key)
+    {
+        case ConsoleKey.D0:
+            Exit();
+            break;
+        case ConsoleKey.D1:
+            ShowAll();
+            break;
+        case ConsoleKey.D2:
+            AddNewContact();
+            break;
+        case ConsoleKey.D4:
+            RemoveContact();
+            break;
+        case ConsoleKey.D5:
+            SearchContact();
+            break;
+        default:
+            break;
+    }
 }
-string[] Duplicate (string str)
+
+while (true)
 {
-    string intermediateResult = "";
-    string str1 = str.ToLower();
-    for (int i = 0; i < str1.Length; i++)
-    {
-        char currentChar = str1[i];
-        for (int j = i + 1; j < str1.Length; j++)
-        {
-            if (str1[j] == currentChar)
-            {
-                intermediateResult += currentChar;
-            }
-        }
-    }
-    var resiltString = "";
-    var unique = new HashSet<char>(intermediateResult);
-    foreach (char c in unique)
-    {
-        resiltString += c;
-    }
-    string[] result = null;
-    Array.Resize(ref result, resiltString.Length);
-    for (int i = 0; i < resiltString.Length; i++)
-    {
-        result[i] = new string(resiltString[i], 1);
-    }
-    return result;
+    MainMenu();
 }
