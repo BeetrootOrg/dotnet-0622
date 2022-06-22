6572983fbb09a45c0f0293951e74f41ceda1bc6f
yurner000 <sysoev.yu@gmail.com>
2022-06-22 19:12:32 +0300

    last

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,54 +1,148 @@
-﻿using System.Text.RegularExpressions; 			
-public class Program
+﻿using System;
+using System.IO;
+using System.Text;
+
+using static System.Console;
+
+const string filename = "data.csv";
+
+void Exit()
 {
-	public static void Main()
-	{
-		SumNumbers();
-	}
-	
-	private static void SumNumbers()
-	{
-		int fNumber, sNumber;
-		Console.WriteLine("\nPlease, enter the first number: ");
-		do
-		{
-			var input1 = Console.ReadLine();
-			if (Regex.IsMatch(input1, @"^\d+$")) // ^ beginning of string, \d single digit, $ end of string
-			{
-				fNumber = Convert.ToInt32(input1); 
-                break;
-			}
-			else
-			{
-				Console.WriteLine("Input provided is invalid. Please enter a correct first integer number and run again ");
-                return;
-			}
-		} while (true);
+    Environment.Exit(0);
+}
 
-		Console.WriteLine("\nPlease,enter second number: ");
-		do
-		{
-			var input2 = Console.ReadLine();
-			if (Regex.IsMatch(input2, @"^\d+$")) // ^ beginning of string, \d single digit, $ end of string
-			{
-				sNumber = Convert.ToInt32(input2); 
-                break;
-			}
-			else
-			{
-				Console.WriteLine("Input provided is invalid. Please enter a correct second integer number and run again ");
-                return;
-			}
-		} while (true);
+void ShowRow((string, string, string) row)
+{
+    var (firstName, lastName, phone) = row;
+    WriteLine("{0,-15} {1,-15} {2,-15}", firstName, lastName, phone);
+}
 
-		int min = Math.Min(fNumber, sNumber);
-		int max = Math.Max(fNumber, sNumber);
-		int result = 0;
-		for (int i = min; i <= max; i++)
-		{
-			result = result + i;
-		}
+(string, string, string)[] ReadContacts(string file)
+{
+    var lines = File.ReadAllLines(file);
+    var contacts = new (string, string, string)[lines.Length];
+    for (int i = 0; i < lines.Length; i++)
+    {
+        var items = lines[i].Split(',');
+        contacts[i] = (items[0], items[1], items[2]);
+    }
 
-		Console.WriteLine("The sum of Numbers between " + min + " and " + max + " is: " + result.ToString());
-	}
+    return contacts;
+}
+
+void ShowAll()
+{
+    // 1. read content from file
+    // 2. iterate in array of contacts
+    // 3. show contact rows
+    Clear();
+
+    var contacts = ReadContacts(filename);
+
+    ShowRow(("First Name", "Last Name", "Phone"));
+    foreach (var contact in contacts)
+    {
+        ShowRow(contact);
+    }
+
+    WriteLine("Press any key to continue...");
+    ReadKey();
+}
+
+string Serialize((string firstName, string lastName, string phone) row) => $"{row.firstName},{row.lastName},{row.phone}";
+
+void AddNewContact()
+{
+    Clear();
+
+    WriteLine("Enter first name:");
+    var firstName = Console.ReadLine();
+
+    WriteLine("Enter last name:");
+    var lastName = Console.ReadLine();
+
+    WriteLine("Enter phone:");
+    var phone = Console.ReadLine();
+
+    File.AppendAllLines(filename, new[] { Serialize((firstName, lastName, phone)) });
+
+    WriteLine("Contact saved, press any key to continue");
+    ReadKey();
+}
+
+void RemoveContact()
+{
+    Clear();
+
+    WriteLine("Enter phone to remove:");
+    var phoneToRemove = Console.ReadLine();
+
+    var contacts = ReadContacts(filename);
+    var newContacts = new (string, string, string)[contacts.Length];
+    Array.Copy(contacts, newContacts, contacts.Length);
+
+    var index = 0;
+
+    while (index < newContacts.Length)
+    {
+        if (newContacts[index].Item3 == phoneToRemove)
+        {
+            newContacts[index] = newContacts[^1];
+            Array.Resize(ref newContacts, newContacts.Length - 1);
+        }
+        else
+        {
+            ++index;
+        }
+    }
+
+    var csvBuilder = new StringBuilder();
+    foreach (var contact in newContacts)
+    {
+        csvBuilder.AppendLine(Serialize(contact));
+    }
+
+    File.WriteAllText(filename, csvBuilder.ToString());
+
+    WriteLine($"{contacts.Length - newContacts.Length} Contact(s) removed, press any key to continue");
+    ReadKey();
+}
+
+void MainMenu()
+{
+    Clear();
+
+    WriteLine("Welcome to phone book!");
+    WriteLine();
+    WriteLine("Menu:");
+    WriteLine("\t1 - Show all contacts");
+    WriteLine("\t2 - Add new contact");
+    WriteLine("\t3 - Update contact");
+    WriteLine("\t4 - Remove contact");
+    WriteLine("\t0 - Exit");
+
+    var key = ReadKey();
+
+    switch (key.Key)
+    {
+        case ConsoleKey.D0:
+            Exit();
+            break;
+        case ConsoleKey.D1:
+            ShowAll();
+            break;
+        case ConsoleKey.D2:
+            AddNewContact();
+            break;
+        case ConsoleKey.D4:
+            RemoveContact();
+            break;
+        default:
+            break;
+    }
+}
+
+while (true)
+{
+    MainMenu();
 }
