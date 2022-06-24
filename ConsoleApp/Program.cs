992750f298d29958d06029c83b15087ba2baa682
Tikhon Lavrov <Tykhon.Lavrov@koroka.solutions>
2022-06-24 23:40:18 +0300

    String Homework Done

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,16 +1,14 @@
-﻿internal class Program
+﻿struct StringAnalyzer
 {
-    static void WriteArray(int[] arr)
+    public int numberOfLetters;
+    public int numberOfDigits;
+    public int numberOfCharacters;
+}
+internal class Program
+{
+    static char[] SelectionSort(char[] arr)
     {
-        foreach (var item in arr)
-        {
-            Console.Write($"{item} ");
-        }
-        System.Console.WriteLine();
-    }     
-    static int[] SelectionSort(int[] arr)
-    {
-        int[] copyArr = new int[arr.Length];
+        char[] copyArr = new char[arr.Length];
         Array.Copy(arr, copyArr, arr.Length);
 
         for (int i = 0; i < copyArr.Length - 1; i++)
@@ -31,10 +29,117 @@
         }
         return copyArr;
     }
+    static bool Compare(string firstStr, string secondStr)
+    {
+        if (firstStr.Length != secondStr.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < firstStr.Length; i++)
+        {
+            if (firstStr[i] != secondStr[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    static StringAnalyzer? Analyze(string str)
+    {
+        if (String.IsNullOrEmpty(str))
+        {
+            return null;
+        }
+        StringAnalyzer analyzer = new StringAnalyzer();
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (Char.IsLetter(str[i]))
+            {
+                analyzer.numberOfLetters++;
+            }
+            if (Char.IsDigit(str[i]))
+            {
+                analyzer.numberOfDigits++;
+            }
+            if (Char.IsSymbol(str[i]))
+            {
+                analyzer.numberOfCharacters++;
+            }
+        }
+
+        return analyzer;
+    }
+
+    static string Sort(string str)
+    {
+        if (String.IsNullOrEmpty(str))
+        {
+            return null;
+        }
+
+        return new string(SelectionSort(str.ToLower().ToCharArray()));
+    }
+
+    static char[] Duplicate(string str)
+    {
+        if (String.IsNullOrEmpty(str))
+        {
+            return null;
+        }
+
+        str = str.ToLower();
+        string stringToReturn = String.Empty;
+        for (int i = 0; i < str.Length; i++)
+        {
+            for (int j = i + 1; j < str.Length; j++)
+            {
+                if (str[i] == str[j] && !stringToReturn.Contains(str[i]) && Char.IsLetter(str[i]))
+                {
+                    stringToReturn += str[i];
+                }
+            }
+        }
+
+        if (String.IsNullOrEmpty(stringToReturn))
+        {
+            return Array.Empty<char>();
+        }
+
+        return stringToReturn.ToCharArray();
+    }
     private static void Main(string[] args)
-    {       
-        int[] myArray = {159, 433, 187, 58, 367};
-        WriteArray(myArray);
-        WriteArray(SelectionSort(myArray));
+    {
+        //Compare
+        System.Console.WriteLine(Compare("Hello", "Hello"));
+        System.Console.WriteLine(Compare("Hello", "hello"));
+        System.Console.WriteLine(Compare("1234", "1234 "));
+        System.Console.WriteLine(Compare("Yes", "No"));
+
+        //Analyze
+        string testStringToAnalyze = "|T3st str1ng 2 analyz3|";
+        StringAnalyzer? analyzer = Analyze(testStringToAnalyze);
+        if (analyzer is not null)
+        {
+            System.Console.WriteLine("\nTest string has:");
+            System.Console.WriteLine($"\t{analyzer.Value.numberOfLetters} letters");
+            System.Console.WriteLine($"\t{analyzer.Value.numberOfDigits} digits");
+            System.Console.WriteLine($"\t{analyzer.Value.numberOfCharacters} characters");
+        }
+
+        //Sort
+        System.Console.WriteLine($"\n{Sort("Gello")}");
+        System.Console.WriteLine(Sort("Xenotransplantation"));
+        System.Console.WriteLine(Sort("Pulchritudinous"));
+        System.Console.WriteLine($"{Sort("Psychotomimetic")}\n");
+
+        //Duplicate 
+        char[] myCharArray = Duplicate("Arigato gozaimasu!");
+        foreach (var item in myCharArray)
+        {
+            Console.Write(item + " ");
+        }
     }
 }
