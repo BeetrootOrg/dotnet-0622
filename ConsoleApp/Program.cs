83f98f7af9ed1bcd0fe45fe5cf49a9abd55ae83c
Oleksandr <sanelka77@yahoo.com>
2022-06-25 00:18:00 +0300

    Selection Sort

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] orig = { 8, 3, 6, 1, 0, 2, 9, 4, 7, 5 };
+
+int[] SelectionSort(int[] arr)
+{
+    int a, b, min;
+    int[] copy;
+
+    copy = new int[arr.Length];
+    Array.Copy(arr, copy, arr.Length);
+    for (a = 0; a < copy.Length; ++a)
+    {
+        for (b = min = a; b < copy.Length; ++b)
+        {
+            if (copy[min] > copy[b])
+            {
+                min = b;
+            }
+        }
+        b = min;
+        min = copy[b];
+        copy[b] = copy[a];
+        copy[a] = min;
+    }
+    return copy;
+}
+
+int[] SelectionSort(int[] arr)
+{
+    int a, b, min;
+    int[] copy;
+
+    copy = new int[arr.Length];
+    Array.Copy(arr, copy, arr.Length);
+    for (a = 0; a < copy.Length; ++a)
+    {
+        for (b = min = a; b < copy.Length; ++b)
+        {
+            if (copy[min] > copy[b])
+            {
+                min = b;
+            }
+        }
+        b = min;
+        min = copy[b];
+        copy[b] = copy[a];
+        copy[a] = min;
+    }
+    return copy;
+}
+Console.WriteLine();
+foreach (var pos in SelectionSort(orig))
+{
+    Console.Write($"{pos}, ");
+}
+Console.WriteLine();
