86180f82fd61cde2a7e0eb9c5949e91cdb7b3751
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-06-24 23:39:02 +0300

    string

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,90 +1,54 @@
 ﻿class Program
 {
-    //bubbleSort
-    static int[] BubbleSort(int[] array)
+    static bool Compare(string str1, string str2)
     {
-        int temp;
-        for (int i = 0; i < array.Length; i++)
+        for (int i = 0; i < str1.Length; i++)
         {
-            for (int j = 0; j < array.Length-1; j++)
+            if (str1[i] != str2[i]) return false;
+        }
+        return true;
+    }
+
+    static Tuple<int, int, int> Analyze(string str)
+    {
+        int letter = 0, number = 0, specSymbol = 0;
+        for (var i = 0; i < str.Length; i++)
+            if (char.IsLetter(str[i])) letter++;
+            else if (char.IsNumber(str[i])) number++;
+            else specSymbol++;
+        return Tuple.Create(letter, number, specSymbol);
+    }
+
+    static string Sort(string str)
+    {
+        str = str.ToLower();
+        var arr = str.ToCharArray();
+        Array.Sort(arr);
+        string sort = new string(arr);
+        return sort;
+    }
+ 
+    static string Duplicate(string str)
+    {
+        string dupStr = "";
+        str = str.ToLower();
+        for (int i = 0; i < str.Length; i++)
+        {
+            for (int j = i+1; j < str.Length; j++)
             {
-                if (array[j] > array[j + 1])
-                {
-                    temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
-                } 
+                if(str[i] == str[j]) dupStr += str[i];
             }
         }
-        return array;
+        return dupStr;
     }
 
-    //QuickSort
-    static int[] QuickSort(int[] array, int minIndex, int maxIndex)
-    {
-        if (minIndex >= maxIndex) return array; 
-        int pivotIndex = GetPivot(array, minIndex, maxIndex);
-        //left sort
-        QuickSort(array, minIndex, pivotIndex - 1);
-        //right sort
-        QuickSort(array, pivotIndex + 1, maxIndex);
-        return array;
-    }
-
-    static int GetPivot(int[] array, int minIndex, int maxIndex)
-    {
-        int pivot = minIndex - 1;
-        for (int i = minIndex; i <= maxIndex; i++)
-        {
-            if (array[i] < array[maxIndex])
-            {
-                pivot++;
-                Swap(ref array[pivot], ref array[i]);
-            }
-        }
-
-        pivot++;
-        Swap(ref array[pivot], ref array[maxIndex]);
-        return pivot;
-    }
-
-    static void Swap(ref int leftItem, ref int rightItem)
-    {
-        int temp = leftItem;
-        leftItem = rightItem;
-        rightItem = temp;
-    }
-
-    static int[] InputArray() 
-    {
-        Console.Write("Input size array: ");
-        int n = Convert.ToInt16(Console.ReadLine());
-        int[] inputArray = new int[n];
-        Random rnd = new Random();
-        for (int i = 0; i < n; i++) inputArray[i] = rnd.Next(-10, 10);
-
-        Console.WriteLine("Disordered array: ");
-        foreach (int element in inputArray) Console.Write($"{element}, ");
-        return inputArray;
-    }
-        
     static void Main()
-    {   
-        //BubbleSort
-        int[] inputArray = InputArray();
-        
-        int[] sortedArrayBubble = BubbleSort(inputArray);
-        Console.WriteLine();
-        Console.WriteLine("BubbleSort: ");
-        foreach (int element in sortedArrayBubble) Console.Write($"{element}, ");
-        
-        //QuickSort
-        int[] sortedArrayQuick = QuickSort(inputArray, 0, inputArray.Length - 1);
-        Console.WriteLine();
-        Console.WriteLine("QuickSort: ");
-        foreach (int element in sortedArrayQuick) Console.Write($"{element}, ");
-
-        Console.ReadKey();
+    {
+        string str1 = "string3@% >//";
+        string str2 = "String";
+        Console.WriteLine(Compare(str1, str2));
+        Console.WriteLine(Analyze(str1));
+        Console.WriteLine(Sort("Hello"));
+        Console.WriteLine(Duplicate("Hello and hi"));
     }
-
 }
