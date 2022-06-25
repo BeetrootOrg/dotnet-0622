2d56f02e104055353aca3b3095e97f8b87802fa7
Oleksandr <sanelka77@yahoo.com>
2022-06-25 23:15:41 +0300

    Compare

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,25 @@
 ﻿
+bool Compare(string val1, string val2)
+{
+    int a = 0; //index
+    bool res = true;
+    if (val1 is null & val2 is null)
+    {
+        return false;
+    }
+    if (val1.Length < 1 & val2.Length < 1 & val1.Length != val2.Length)
+    {
+        return false;
+    }
+    a = val1.Length;
+    while (a > -1)
+    {
+        if (val1[a] != val2[a])
+        {
+            res = false;
+            break;
+        }
+        --a;
+    }
+    return res;
+}
