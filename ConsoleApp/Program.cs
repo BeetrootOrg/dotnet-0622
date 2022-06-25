50ceaf668215c8a244096666b7b16f4875ca66b5
yurner000 <sysoev.yu@gmail.com>
2022-06-25 10:44:33 +0300

    persons

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,218 +1,37 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿var person1 = new Person("Dima", "Misik", 25);
+var person2 = new Person("Dima", "Misik", 25);
+var person3 = new Person("A", "B", 42);
+System.Console.WriteLine(person1.GetPerson());
+System.Console.WriteLine(person1 == person2);
+System.Console.WriteLine(person1.Equals(person2));
+System.Console.WriteLine(person1.CompareTo(person2));
+System.Console.WriteLine(person1.CompareTo(person3));
 
-using static System.Console;
+// Compilation error
+// person1._firstName;
 
-const string filename = "data.csv";
-
-void Exit()
+class Person
 {
-    Environment.Exit(0);
-}
+    string _firstName = "Placeholder for FN";
+    string _lastName = "Placeholder for LN";
+    int _age = 42;
 
-void ShowRow((string, string, string) row)
-{
-    var (firstName, lastName, phone) = row;
-    WriteLine("{0,-15} {1,-15} {2,-15}", firstName, lastName, phone);
-}
-
-(string, string, string)[] ReadContactsTemp(string file)
-{
-    if (!File.Exists(file))
+    public Person(string firstName, string lastName, int age)
     {
-        return Array.Empty<(string, string, string)>();
+        _firstName = firstName;
+        _lastName = lastName;
+        _age = age;
     }
 
-    var lines = File.ReadAllLines(file);
-    var contacts = new (string, string, string)[lines.Length];
-    for (int i = 0; i < lines.Length; i++)
+    public string GetPerson()
     {
-        var items = lines[i].Split(',');
-        if (items.Length != 3)
-        {
-            WriteLine($"ERROR WHILE READING {i + 1} LINE FROM {filename}.");
-            continue;
-        }
-
-        contacts[i] = (items[0], items[1], items[2]);
+        return $"First Name: {_firstName}; Last Name: {_lastName}; Age: {_age}";
     }
 
-    return contacts;
-}
-
-(string, string, string)[] ReadContacts(string file)
-{
-    try
+    public bool CompareTo(Person person)
     {
-        var lines = File.ReadAllLines(file);
-        var contacts = new (string, string, string)[lines.Length];
-        for (int i = 0; i < lines.Length; i++)
-        {
-            var items = lines[i].Split(',');
-
-            try
-            {
-                contacts[i] = (items[0], items[1], items[2]);
-            }
-            catch (IndexOutOfRangeException ioore)
-            {
-                WriteLine($"ERROR WHILE READING {i + 1} LINE FROM {filename}. Message: {ioore.Message}");
-            }
-        }
-
-        return contacts;
-    }
-    catch (FileNotFoundException)
-    {
-        return Array.Empty<(string, string, string)>();
-    }
-}
-
-void ShowAll()
-{
-    // 1. read content from file
-    // 2. iterate in array of contacts
-    // 3. show contact rows
-    Clear();
-
-    var contacts = ReadContactsTemp(filename);
-
-    ShowRow(("First Name", "Last Name", "Phone"));
-    foreach (var contact in contacts)
-    {
-        ShowRow(contact);
-    }
-
-    WriteLine("Press any key to continue...");
-    ReadKey();
-}
-
-string Serialize((string firstName, string lastName, string phone) row) => $"{row.firstName},{row.lastName},{row.phone}";
-
-void ValidateNonEmpty(string value, string message = null)
-{
-    if (string.IsNullOrWhiteSpace(value))
-    {
-        throw new ArgumentException(message, nameof(value));
-    }
-}
-
-void AddNewContact()
-{
-    try
-    {
-        Clear();
-
-        WriteLine("Enter first name:");
-        var firstName = Console.ReadLine();
-        ValidateNonEmpty(firstName, "First name should be non-empty");
-
-        WriteLine("Enter last name:");
-        var lastName = Console.ReadLine();
-        ValidateNonEmpty(lastName, "Last name should be non-empty");
-
-        WriteLine("Enter phone:");
-        var phone = Console.ReadLine();
-        ValidateNonEmpty(lastName, "Phone should be non-empty");
-
-        File.AppendAllLines(filename, new[] { Serialize((firstName, lastName, phone)) });
-
-        WriteLine("Contact saved, press any key to continue");
-        ReadKey();
-    }
-    catch (ArgumentException)
-    {
-        WriteLine("baran");
-        throw;
-    }
-    finally
-    {
-        WriteLine("finally done");
-    }
-}
-
-void RemoveContact()
-{
-    Clear();
-
-    WriteLine("Enter phone to remove:");
-    var phoneToRemove = Console.ReadLine();
-
-    var contacts = ReadContacts(filename);
-    var newContacts = new (string, string, string)[contacts.Length];
-    Array.Copy(contacts, newContacts, contacts.Length);
-
-    var index = 0;
-
-    while (index < newContacts.Length)
-    {
-        if (newContacts[index].Item3 == phoneToRemove)
-        {
-            newContacts[index] = newContacts[^1];
-            Array.Resize(ref newContacts, newContacts.Length - 1);
-        }
-        else
-        {
-            ++index;
-        }
-    }
-
-    var csvBuilder = new StringBuilder();
-    foreach (var contact in newContacts)
-    {
-        csvBuilder.AppendLine(Serialize(contact));
-    }
-
-    File.WriteAllText(filename, csvBuilder.ToString());
-
-    WriteLine($"{contacts.Length - newContacts.Length} Contact(s) removed, press any key to continue");
-    ReadKey();
-}
-
-void MainMenu()
-{
-    Clear();
-
-    WriteLine("Welcome to phone book!");
-    WriteLine();
-    WriteLine("Menu:");
-    WriteLine("\t1 - Show all contacts");
-    WriteLine("\t2 - Add new contact");
-    WriteLine("\t3 - Update contact");
-    WriteLine("\t4 - Remove contact");
-    WriteLine("\t0 - Exit");
-
-    var key = ReadKey();
-
-    switch (key.Key)
-    {
-        case ConsoleKey.D0:
-            Exit();
-            break;
-        case ConsoleKey.D1:
-            ShowAll();
-            break;
-        case ConsoleKey.D2:
-            AddNewContact();
-            break;
-        case ConsoleKey.D4:
-            RemoveContact();
-            break;
-        default:
-            break;
-    }
-}
-
-while (true)
-{
-    try
-    {
-        MainMenu();
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine($"Unhandled error: {e.Message}");
-        ReadKey();
+        return _firstName == person._firstName &&
+            _lastName == person._lastName &&
+            _age == person._age;
     }
 }
