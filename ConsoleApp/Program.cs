7e2202077bf8033c76e4b82d9e9eb79941fe33f6
Oleksandr <sanelka77@yahoo.com>
2022-06-25 17:30:01 +0300

    base task

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,39 +1,54 @@
-﻿int[] orig = { 8, 3, 6, 1, 0, 2, 9, 4, 7, 5 };
-
+﻿
 int[] SelectionSort(int[] arr)
 {
     int a, b, min;
-    int[] copy;
+    int[] arrCopy = new int[arr.Length];
+    Array.Copy(arr, arrCopy, arr.Length);
 
-    copy = new int[arr.Length];
-    Array.Copy(arr, copy, arr.Length);
-    for (a = 0; a < copy.Length; ++a)
+    for (a = 0; a < arrCopy.Length - 1; ++a)
     {
-        for (b = min = a; b < copy.Length; ++b)
+        min = a;
+        for (b = min + 1; b < arrCopy.Length; ++b)
         {
-            if (copy[min] > copy[b])
+            min = arrCopy[min] > arrCopy[b] ? b : min;
+        }
+        b = arrCopy[a];
+        arrCopy[a] = arrCopy[min];
+        arrCopy[min] = b;
+    }
+    return arrCopy;
+}
+
+int[] BubbleSortUpperLight(int[] arr) //Variant 1. Upper a light object up.
+{
+    int a, b, min;
+    int[] copy = new int[arr.Length];
+    Array.Copy(arr, copy, arr.Length);
+
+    for (a = 0; a < copy.Length - 1; ++a)
+    {
+        for (b = 0; b < copy.Length - 1 - a; ++b)
+        {
+            if (copy[b] < copy[b + 1])
             {
-                min = b;
+                min = copy[b];
+                copy[b] = copy[b + 1];
+                copy[b + 1] = min;
             }
         }
-        b = min;
-        min = copy[b];
-        copy[b] = copy[a];
-        copy[a] = min;
     }
     return copy;
 }
 
-int[] BubbleSort(int[] arr)
+int[] BubbleSortLowerHeavy(int[] arr) //Variant 2. Lower heavy object down
 {
-    int a, b, min = 0;
-    int[] copy;
-
-    copy = new int[arr.Length];
+    int a, b, min;
+    int[] copy = new int[arr.Length];
     Array.Copy(arr, copy, arr.Length);
-    for (a = 0; a < copy.Length; ++a)
+
+    for (a = 0; a < copy.Length - 1; ++a)
     {
-        for (b = 0; b < copy.Length - 1; ++b)
+        for (b = 0; b < copy.Length - 1 - a; ++b)
         {
             if (copy[b] > copy[b + 1])
             {
