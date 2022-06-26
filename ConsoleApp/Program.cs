6efe318f1bf40775dd798a14cebaff9c8b677901
Andriy.Gulpa <andriy.gulpa@gmail.com>
2022-06-26 18:06:48 +0300

    BubleSorted

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,27 +1,36 @@
-﻿int[] asd = { 31, 3, 2, 32, 6, 56, 45, 4, -2, 6 };
-
-int[] BubleSort(int[] array)
+﻿void WritelineArray(int[] arr)
 {
-    var tempArray = new int[array.Length];
-    Array.Copy(array, tempArray, array.Length);
-
-    int tempVariable;
-    for (int i = 0; i < tempArray.Length - 1; i++)
+    foreach (var element in arr)
     {
+        System.Console.WriteLine(element);
+    }
+}
+ 
+int[] BubleSorted(int[] arr)
+{
+    var copy = new int[arr.Length];
+    Array.Copy(arr, copy, arr.Length);
 
-        for (int j = i + 1; j < tempArray.Length; j++)
+    for (var i = 1; i < copy.Length; ++i)
+    {
+        for (var j = 0; j < copy.Length; j++)
         {
-
-            if (tempArray[i] > tempArray[j])
+            if (copy[j] > copy[j + 1])
             {
-                tempVariable = tempArray[i];
-                tempArray[i] = tempArray[j];
-                tempArray[j] = tempVariable;
+                var temp = copy[j];
+                copy[j] = copy[j + 1];
+                copy[j+1] = temp;
             }
         }
     }
-    Console.WriteLine(tempVariabl);
-};
+    return copy;
+}
 
+//Sort
+System.Console.WriteLine("Normal sorted");
+WritelineArray(BubleSorted(new[] { 1, 2, 3, 4, 5 }));
+System.Console.WriteLine("Rreverse sorted");
+WritelineArray(BubleSorted(new[] { 5, 4, 3, 2, 1 }));
+System.Console.WriteLine("Random number");
+WritelineArray(BubleSorted(new[] { 5, 4, 3, 2, 1, 4, 12, 33, 4, 2, 5, 54 }));
 
-BubleSort(asd);
