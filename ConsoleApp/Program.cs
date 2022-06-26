a70ce2834139f595ba0c3f54cb995c4e2b4e6c64
iVanyaVas <vanya.iaschenko@gmail.com>
2022-06-26 12:47:27 +0300

    SringCompare & StringAnalyze redone

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,18 @@
-﻿static bool StringCompare(string str1, string str2) => str1 == str2;
+﻿static bool StringCompare(string str1, string str2)
+{   
+
+    if(str1.Length != str2.Length) 
+    {
+        return false;
+    }
+    
+    for(int i = 0 ; i< str1.Length; ++i)
+    {
+        if(str1[i] != str2[i]) return false;
+    }
+
+    return true;
+}
 static (int,int,int)StringAnalyze(string str)
 {
     int digits = 0, alphabetic = 0, other = 0;
@@ -14,7 +28,10 @@ static (int,int,int)StringAnalyze(string str)
             alphabetic += 1;
             continue;
         }
-        other++;
+        if(char.IsSymbol(i) || char.IsPunctuation(i))
+        {
+            other++;
+        }
     }
 
 return (digits,alphabetic,other);
@@ -81,7 +98,7 @@ static char[] StringDuplicate(string str1, string str2)
 }
 
 
-string str1 = "ABCD";
+string str1 = "ABCD,;/$%";
 string str2 = "ABBAKK";
 string str3 = "ABBAKK";
 Console.WriteLine(StringCompare(str2, str3));
