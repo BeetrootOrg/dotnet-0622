76fe35e599106b870b8ac0851479427281a7210b
d.misik <misik.dmitriy@gmail.com>
2022-06-27 20:54:33 +0300

    strings

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,26 +1,97 @@
 ﻿using System;
 
-using ConsoleApp;
+using static System.Console;
 
-void PrintArray(params int[] array)
+bool Compare(string val1, string val2)
 {
-    Console.Write(string.Join(",", array));
-    Console.WriteLine();
+    if (val1 == null && val2 == null)
+    {
+        return true;
+    }
+
+    // val1?.Length -> val1 != null ? val1.Length : null
+    if (val1?.Length != val2?.Length)
+    {
+        return false;
+    }
+
+    for (var i = 0; i < val1.Length; ++i)
+    {
+        if (val1[i] != val2[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-int[] Sort(int[] arr)
+(int, int, int) Analyze(string value)
 {
-    return new QuickSort().Sort(arr);
+    var result = (0, 0, 0);
+    foreach (var symbol in value)
+    {
+        if (char.IsLetter(symbol))
+        {
+            ++result.Item1;
+        }
+        else if (char.IsDigit(symbol))
+        {
+            ++result.Item2;
+        }
+        else if (char.IsSymbol(symbol))
+        {
+            ++result.Item3;
+        }
+    }
+
+    return result;
 }
 
-var arr1 = new[] { -5, 0, 1, 5, 17 };
-var arr2 = new[] { -42, -5, 7, 15, 100 };
-var arr3 = new[] { 42, -1, 14, 22, 5 };
-var arr4 = Array.Empty<int>();
-var arr5 = new[] { 42, 42, 42, 42, 42 };
+string Sort(string value)
+{
+    var chars = value.ToLower().ToCharArray();
+    Array.Sort(chars);
+    return new string(chars);
+}
 
-PrintArray(Sort(arr1));
-PrintArray(Sort(arr2));
-PrintArray(Sort(arr3));
-PrintArray(Sort(arr4));
-PrintArray(Sort(arr5));
+char[] Duplicate(string value)
+{
+    var duplicates = new char[value.Length / 2];
+    var index = 0;
+
+    foreach (var symbol in value.ToLower())
+    {
+        if (char.IsWhiteSpace(symbol))
+        {
+            continue;
+        }
+
+        if (value.IndexOf(symbol) != value.LastIndexOf(symbol))
+        {
+            var alreadyAdded = false;
+            for (var i = 0; i < index; ++i)
+            {
+                if (duplicates[i] == symbol)
+                {
+                    alreadyAdded = true;
+                    break;
+                }
+            }
+
+            if (!alreadyAdded)
+            {
+                duplicates[index++] = symbol;
+            }
+        }
+    }
+
+    Array.Resize(ref duplicates, index);
+    return duplicates;
+}
+
+WriteLine(Compare("hell", "hello"));
+WriteLine(Compare("hell", "hello"));
+WriteLine(Analyze("hell"));
+WriteLine(Sort("hell"));
+WriteLine(Duplicate("Hello, World"));
