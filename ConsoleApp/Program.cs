2936aa024961d7d4c839af35a891f42ad493014d
d.misik <misik.dmitriy@gmail.com>
2022-06-29 20:24:55 +0300

    equals

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,72 +1,5 @@
 ﻿using static System.Console;
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        var animal1 = new Animal
-        {
-            EyesColor = EyesColor.Black,
-            Lifestyle = Lifestyle.Meat,
-            Size = Size.Large,
-            Name = "animal"
-        };
-
-        var cat1 = new Cat
-        {
-            Breed = Breed.Red,
-            EyesColor = EyesColor.Brown,
-            Lifestyle = Lifestyle.Meat,
-            Name = "Grafield",
-            Size = Size.XLarge
-        };
-
-        var cat2 = new Cat
-        {
-            Breed = Breed.SimpleFree,
-            EyesColor = EyesColor.Black,
-            Lifestyle = Lifestyle.Grass,
-            Name = "Casper",
-            Size = Size.Medium
-        };
-
-        animal1.Say();
-        cat1.Say();
-
-        Animal animal2 = cat1;
-        // below is possible as well
-        // animal2 = new Dog();
-
-        animal2.Say();
-
-        A b = new B();
-        A d = new D();
-        b.Method();
-        d.Method();
-
-        CallMethod(new A());
-        CallMethod(new B());
-        CallMethod(new C());
-        CallMethod(new D());
-
-        MakeAnimalSaySomething(animal1);
-        MakeAnimalSaySomething(cat1);
-        MakeAnimalSaySomething(new Dog());
-
-        WriteLine("BAD");
-        MakeAnimalSaySomethingBad(animal1);
-        MakeAnimalSaySomethingBad(cat1);
-        MakeAnimalSaySomethingBad(new Dog());
-
-        WriteLine("SIZES");
-        WriteLine(cat1.GetSize());
-        WriteLine(cat2.GetSize());
-    }
-
-    static void CallMethod(A instance) => instance.Method();
-    static void MakeAnimalSaySomething(Animal animal) => animal.Say();
-    static void MakeAnimalSaySomethingBad(Animal animal) => animal.SayBad();
-}
 
 enum Lifestyle
 {
@@ -107,6 +40,32 @@ class Animal
         Size.XLarge => 8,
         _ => -1
     };
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        var animal = obj as Animal;
+        if (animal == null)
+        {
+            return false;
+        }
+
+        return Lifestyle == animal.Lifestyle &&
+            EyesColor == animal.EyesColor &&
+            Size == animal.Size &&
+            Name == animal.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string ToString() => $"Lifestyle={Lifestyle};EyesColor={EyesColor};Size={Size};Name={Name}";
 }
 
 class Dog : Animal
@@ -125,6 +84,8 @@ class Cat : Animal
         Size.XLarge => 16,
         _ => base.GetSize()
     };
+
+    public override string ToString() => $"{base.ToString()};Breed={Breed}";
 }
 
 enum Breed
@@ -150,4 +111,99 @@ class C : B
 
 class D : C
 {
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var animal1 = new Animal
+        {
+            EyesColor = EyesColor.Black,
+            Lifestyle = Lifestyle.Meat,
+            Size = Size.Large,
+            Name = "animal"
+        };
+
+        var cat1 = new Cat
+        {
+            Breed = Breed.Red,
+            EyesColor = EyesColor.Brown,
+            Lifestyle = Lifestyle.Meat,
+            Name = "Garfield",
+            Size = Size.XLarge
+        };
+
+        var cat2 = new Cat
+        {
+            Breed = Breed.SimpleFree,
+            EyesColor = EyesColor.Black,
+            Lifestyle = Lifestyle.Grass,
+            Name = "Casper",
+            Size = Size.Medium
+        };
+
+        var cat3 = new Cat
+        {
+            Breed = Breed.Red,
+            EyesColor = EyesColor.Brown,
+            Lifestyle = Lifestyle.Meat,
+            Name = "Garfield",
+            Size = Size.XLarge
+        };
+
+        var cat4 = new Cat
+        {
+            Breed = Breed.SimpleFree,
+            EyesColor = EyesColor.Brown,
+            Lifestyle = Lifestyle.Meat,
+            Name = "Garfield",
+            Size = Size.XLarge
+        };
+
+        animal1.Say();
+        cat1.Say();
+
+        Animal animal2 = cat1;
+        // below is possible as well
+        // animal2 = new Dog();
+
+        animal2.Say();
+
+        A b = new B();
+        A d = new D();
+        b.Method();
+        d.Method();
+
+        CallMethod(new A());
+        CallMethod(new B());
+        CallMethod(new C());
+        CallMethod(new D());
+
+        MakeAnimalSaySomething(animal1);
+        MakeAnimalSaySomething(cat1);
+        MakeAnimalSaySomething(new Dog());
+
+        WriteLine("BAD");
+        MakeAnimalSaySomethingBad(animal1);
+        MakeAnimalSaySomethingBad(cat1);
+        MakeAnimalSaySomethingBad(new Dog());
+
+        WriteLine("SIZES");
+        WriteLine(cat1.GetSize());
+        WriteLine(cat2.GetSize());
+
+        WriteLine("Inherited from object");
+        WriteLine($"animal1.ToString(): {animal1.ToString()}");
+        WriteLine($"cat1.ToString(): {cat1.ToString()}");
+        WriteLine($"cat1.Equals(cat2): {cat1.Equals(cat2)}");
+        WriteLine($"cat1.Equals(cat3): {cat1.Equals(cat3)}");
+        WriteLine($"cat1.Equals(cat4): {cat1.Equals(cat4)}");
+        WriteLine($"cat1.GetHashCode(): {cat1.GetHashCode()}");
+        WriteLine($"cat3.GetHashCode(): {cat3.GetHashCode()}");
+    }
+
+    static void CallMethod(A instance) => instance.Method();
+    static void MakeAnimalSaySomething(Animal animal) => animal.Say();
+    static void MakeAnimalSaySomethingBad(Animal animal) => animal.SayBad();
 }
