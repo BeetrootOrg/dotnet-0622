3207ca6ddf672d816e309c5688c85c9e22588a28
Andriy.Gulpa <andriy.gulpa@gmail.com>
2022-06-30 11:24:55 +0300

    home work

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,24 +1,131 @@
-﻿using static System.Console;
+﻿using System;
+using System.IO;
+using System.Text;
+
+using static System.Console;
+
+const string filename = "data.csv";
+
 void Exit()
 {
     Environment.Exit(0);
 }
 
+void ShowRow((string, string, string) row)
+{
+    var (firstName, lastName, phone) = row;
+    WriteLine("{0,-15} {1,-15} {2,-15}", firstName, lastName, phone);
+}
+
+(string, string, string)[] ReadContacts(string file)
+{
+    var lines = File.ReadAllLines(file);
+    var contacts = new (string, string, string)[lines.Length];
+    for (int i = 0; i < lines.Length; i++)
+    {
+        var items = lines[i].Split(',');
+        contacts[i] = (items[0], items[1], items[2]);
+    }
+
+    return contacts;
+}
+
 void ShowAll()
 {
+    // 1. read content from file
+    // 2. iterate in array of contacts
+    // 3. show contact rows
     Clear();
-    var contacts = new[]
+
+    var contacts = ReadContacts(filename);
+
+    ShowRow(("First Name", "Last Name", "Phone"));
+    foreach (var contact in contacts)
     {
-        ("First", "Last", "+3801234567"),
-        ("f", "L", "+380123578"),
-    };
-    WriteLine("{0,-15} {1,-15} {2,-15}", "First Name", "Last Name", "Phone");
-    foreach (var (firstName, lastName, phone) in contacts)
-    {
-        WriteLine("{0,-15} {1,-15} {2,-15}", firstName, lastName, phone);
-        
+        ShowRow(contact);
     }
-    WriteLine("Press eny kay to continue...");
+
+    WriteLine("Press any key to continue...");
+    ReadKey();
+}
+
+string Serialize((string firstName, string lastName, string phone) row) => $"{row.firstName},{row.lastName},{row.phone}";
+
+void AddNewContact()
+{
+    Clear();
+
+    WriteLine("Enter first name:");
+    var firstName = Console.ReadLine();
+
+    WriteLine("Enter last name:");
+    var lastName = Console.ReadLine();
+
+    WriteLine("Enter phone:");
+    var phone = Console.ReadLine();
+
+    File.AppendAllLines(filename, new[] { Serialize((firstName, lastName, phone)) });
+
+    WriteLine("Contact saved, press any key to continue");
+    ReadKey();
+}
+
+void RemoveContact()
+{
+    Clear();
+
+    WriteLine("Enter phone to remove:");
+    var phoneToRemove = Console.ReadLine();
+
+    var contacts = ReadContacts(filename);
+    var newContacts = new (string, string, string)[contacts.Length];
+    Array.Copy(contacts, newContacts, contacts.Length);
+
+    var index = 0;
+
+    while (index < newContacts.Length)
+    {
+        if (newContacts[index].Item3 == phoneToRemove)
+        {
+            newContacts[index] = newContacts[^1];
+            Array.Resize(ref newContacts, newContacts.Length - 1);
+        }
+        else
+        {
+            ++index;
+        }
+    }
+
+    var csvBuilder = new StringBuilder();
+    foreach (var contact in newContacts)
+    {
+        csvBuilder.AppendLine(Serialize(contact));
+    }
+
+    File.WriteAllText(filename, csvBuilder.ToString());
+
+    WriteLine($"{contacts.Length - newContacts.Length} Contact(s) removed, press any key to continue");
+    ReadKey();
+}
+
+void SearchContact()
+{
+    Clear();
+    
+    WriteLine("Enter first or last name:");
+    string searchContact = ReadLine();
+    var contact = ReadContacts(filename);
+    ShowRow(("First Name", "Last Name", "Phone"));
+    for (int i = 0; i < contact.Length; i++)
+    {
+        if (searchContact == contact[i].Item1 || searchContact == contact[i].Item2)
+        ShowRow(contact[i]);
+    else{
+        WriteLine("No such contact");
+    }
+    }
+    
+    WriteLine("Press any key to continue...");
     ReadKey();
 }
 
@@ -26,16 +133,18 @@ void MainMenu()
 {
     Clear();
 
-    Console.WriteLine("Welcome to phone book!");
-    Console.WriteLine();
+    WriteLine("Welcome to phone book!");
+    WriteLine();
     WriteLine("Menu:");
     WriteLine("\t1 - Show all contacts");
     WriteLine("\t2 - Add new contact");
     WriteLine("\t3 - Update contact");
     WriteLine("\t4 - Remove contact");
+    WriteLine("\t5 - Search contact");
     WriteLine("\t0 - Exit");
 
     var key = ReadKey();
+
     switch (key.Key)
     {
         case ConsoleKey.D0:
@@ -44,14 +153,21 @@ void MainMenu()
         case ConsoleKey.D1:
             ShowAll();
             break;
-        default:
-            MainMenu();
+        case ConsoleKey.D2:
+            AddNewContact();
             break;
-
+            case ConsoleKey.D5:
+            SearchContact();
+            break;
+        case ConsoleKey.D4:
+            RemoveContact();
+            break;
+        default:
+            break;
     }
 }
+
 while (true)
 {
-MainMenu();
+    MainMenu();
 }
-
