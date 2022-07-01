cac0b7a499ecc9af5f8374c4b5d959a6312e8afa
Killjaen <killjaen@gmail.com>
2022-07-01 21:56:42 +0300

    class_diag

--- ConsoleApp/Person.cs
+++ ConsoleApp/Person.cs
@@ -6,11 +6,10 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    class Customer : Person
-    {
-        public void Drive(Car car)
-        {
-            car.testdrive();
-        }
+    class Person
+    {  
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
     }
 }
