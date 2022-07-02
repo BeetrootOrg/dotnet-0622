d7be646b9bd708f1bb39d73f780f519e008912cc
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-07-02 22:59:44 +0300

    change class library

--- ConsoleApp/Library/Library.cs
+++ ConsoleApp/Library/Library.cs
@@ -1,7 +1,10 @@
+namespace ConsoleApp.Library;
 class Library
 {
     public string NameLibrary { get; init; }
     public string AddressLibrary { get; init; }
     public string PhoneNumberLibrary { get; init; }
     public string EmailLibrary { get; init; }
+    public Book Books { get; init; }
+    public MemberCards Member { get; init; }
 }
