1d2282f586a337e3bec2ace47a9abb5568a88949
Anarik <106695465+Anariks@users.noreply.github.com>
2022-07-02 10:38:44 +0300

    cells

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,45 +1,29 @@
-﻿using ConsoleApp;
-using static System.Console;
-
-
-
-var pupil = new Pupil
+﻿
+public class GameOfLife
 {
-    FirstName = "Mark",
-    LastName = "Maksimovich",
-    SchoolClass = SchoolClass.B10,
-    DateBirth = new DateTime(2015, 12, 31)
-};  
+    public char[,] Execute(char[,] cells)
+    {   
+        Console.WriteLine (cells.Length);
+        for (int i = 0; i < cells.GetLength(0); i++)
+        {
+            for (int j = 0; j < cells.GetLength(1); j++)
+            {
+                int aliveAroundSum = 0;
+                int deadAroundSum = 0;
+                
+                if  cells                                     
+                if (cells[i,j] == '*')
+                {
+                    
 
-Teacher teacher1 = new Teacher 
-{
-    FirstName = "Klara",
-    LastName = "Ericsson",
-    Subject = Subject.Chemestry
-};
+                }  
 
-Person somePerson = new Person
-{
-    FirstName = "ivan",
-    LastName = "ivanov",
-    DateBirth = new DateTime(2035, 11, 12)
-};
+            }
+            
+            /// 
+            /// </summary>
+        }
 
-Console.WriteLine (pupil);
-Console.WriteLine (pupil.FirstName);
-Console.WriteLine (pupil.LastName);
-Console.WriteLine (pupil.SchoolClass);
-Console.WriteLine (pupil.DateBirth);
-
-Console.WriteLine (teacher1.FirstName);
-Console.WriteLine (teacher1.LastName);
-
-WriteLine(somePerson.GetPerson());
-WriteLine(pupil.GetPerson());
-WriteLine(teacher1.GetPerson());
-
-var lesson1 = new Lesson(SchoolClass.A11, ClassRoom._16, teacher1, new DateTime(2022,07, 02, 10, 0, 0));
-
-WriteLine (lesson1);
-
-
+        return cells;
+    }
+}
