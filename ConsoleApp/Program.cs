79c867dd75dd0912b87cf67b97204263b37854f5
Nahlenko Oleksandr <noapioss@gmail.com>
2022-07-02 13:43:41 +0300

    final

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,66 +2,25 @@
 {
     public char[,] Execute(char[,] cells)
     {
-        int i;
-        char[,] copy = new char[cells.GetLength(0), cells.GetLength(1)];
-        char[,] extraCells = new char[cells.GetLength(0) + 2, cells.GetLength(1) + 2];
-        char[,] temp = new char[cells.GetLength(0) + 2, cells.GetLength(1) + 2];
-
-        for (int j = 0; j < temp.GetLength(0); j++)
-        {
-            for (int k = 0; k < temp.GetLength(1); k++)
-            {
-                extraCells[j, k ] = '.';
-                temp[j, k ] = '.';
-            }
-        }
-
-        for (int j = 0; j < cells.GetLength(0); j++)
-        {
-            for (int k = 0; k < cells.GetLength(1); k++)
-            {
-                extraCells[j + 1, k + 1] = cells[j, k];
-            }
-        }
-
+        char[,] result = new char[cells.GetLength(0), cells.GetLength(1)];
+        char[,] copy = new char[cells.GetLength(0) + 2, cells.GetLength(1) + 2];
         
-        for (i = 0; i < 1; i++)
-        {
-            for (int j = 1; j < extraCells.GetLength(0) - 1; j++)
+        for (int i = 0; i < cells.GetLength(0); i++)
+            for (int j = 0; j < cells.GetLength(1); j++)
             {
-                for (int k = 1; k < extraCells.GetLength(1) - 1; k++)
-                {
-                    int count = CountOfLive(extraCells, j, k);
-                    if (extraCells[j, k] == 1)
-                    {
-                        if (count == 2 || count == 3) temp[j, k] = '*';
-                    }
-                    else
-                    {
-                        if (count == 3) temp[j, k] = '*';
-                    }
-                }
+                result[i,j] = '.';
+                copy[i+1, j+1] = cells[i,j];
             }
 
-            for (int j = 1; j < extraCells.GetLength(0) - 1; j++)
+        for (int i = 0; i < cells.GetLength(0); i++)
+            for (int j = 0; j < cells.GetLength(1); j++)
             {
-                for (int k = 1; k < extraCells.GetLength(1) - 1; k++)
-                {
-                    extraCells[j, k] = temp[j, k];
-                    temp[j, k] = '.';
-                }
-            }
-        }
+                int count = CountOfLive(copy, i+1, j+1);
+                if ((count == 2 || count == 3) && copy[i+1, j+1] == '*') result[i,j] = '*';
+                if (count == 3 && copy[i+1, j+1] == '.') result[i,j] = '*';
+            }               
 
-        for (int j = 0; j < cells.GetLength(0); j++)
-        {
-            for (int k = 0; k < cells.GetLength(1); k++)
-            {
-                copy[j, k] = extraCells[j + 1, k + 1];
-            }
-        }
-
-        return copy;
+        return result;
     }
 
 
@@ -77,10 +36,11 @@
         if (a[i + 1, j] == '*') live++;
         if (a[i + 1, j + 1] == '*') live++;
         return live;
-
     }
+
+    
     private static void Main(string[] args)
     {
-        
+       
     }
 }
