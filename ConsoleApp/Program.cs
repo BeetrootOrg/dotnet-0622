5c9ec7c117edfcf2f887d83e207721adf869788b
Tikhon Lavrov <Tykhon.Lavrov@koroka.solutions>
2022-07-02 15:30:41 +0300

    text with exceptions

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ using static System.Console;
 internal class Program
 {
     const string filename = "data.csv";
+
     static void Exit()
     {
         Environment.Exit(0);
@@ -18,17 +19,56 @@ internal class Program
         WriteLine("{0,-15} {1,-15} {2,-15}", firstName, lastName, phone);
     }
 
-    static (string, string, string)[] ReadContacts(string file)
+    static (string, string, string)[] ReadContactsTemp(string file)
     {
+        if (!File.Exists(file))
+        {
+            return Array.Empty<(string, string, string)>();
+        }
+
         var lines = File.ReadAllLines(file);
         var contacts = new (string, string, string)[lines.Length];
         for (int i = 0; i < lines.Length; i++)
         {
             var items = lines[i].Split(',');
+            if (items.Length != 3)
+            {
+                WriteLine($"ERROR WHILE READING {i + 1} LINE FROM {filename}.");
+                continue;
+            }
+
             contacts[i] = (items[0], items[1], items[2]);
         }
 
         return contacts;
+    }
+
+    static (string, string, string)[] ReadContacts(string file)
+    {
+        try
+        {
+            var lines = File.ReadAllLines(file);
+            var contacts = new (string, string, string)[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var items = lines[i].Split(',');
+
+                try
+                {
+                    contacts[i] = (items[0], items[1], items[2]);
+                }
+                catch (IndexOutOfRangeException ioore)
+                {
+                    WriteLine($"ERROR WHILE READING {i + 1} LINE FROM {filename}. Message: {ioore.Message}");
+                }
+            }
+
+            return contacts;
+        }
+        catch (FileNotFoundException)
+        {
+            return Array.Empty<(string, string, string)>();
+        }
     }
 
     static void ShowAll()
@@ -38,7 +78,7 @@ internal class Program
         // 3. show contact rows
         Clear();
 
-        var contacts = ReadContacts(filename);
+        var contacts = ReadContactsTemp(filename);
 
         ShowRow(("First Name", "Last Name", "Phone"));
         foreach (var contact in contacts)
@@ -52,23 +92,46 @@ internal class Program
 
     static string Serialize((string firstName, string lastName, string phone) row) => $"{row.firstName},{row.lastName},{row.phone}";
 
+    static void ValidateNonEmpty(string value, string message = null)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentException(message, nameof(value));
+        }
+    }
+
     static void AddNewContact()
     {
-        Clear();
+        try
+        {
+            Clear();
 
-        WriteLine("Enter first name:");
-        var firstName = ReadLine().Trim();
+            WriteLine("Enter first name:");
+            var firstName = ReadLine();
+            ValidateNonEmpty(firstName, "First name should be non-empty");
 
-        WriteLine("Enter last name:");
-        var lastName = ReadLine().Trim();
+            WriteLine("Enter last name:");
+            var lastName = ReadLine();
+            ValidateNonEmpty(lastName, "Last name should be non-empty");
 
-        WriteLine("Enter phone:");
-        var phone = ReadLine().Trim();
+            WriteLine("Enter phone:");
+            var phone = ReadLine();
+            ValidateNonEmpty(lastName, "Phone should be non-empty");
 
-        File.AppendAllLines(filename, new[] { Serialize((firstName, lastName, phone)) });
+            File.AppendAllLines(filename, new[] { Serialize((firstName, lastName, phone)) });
 
-        WriteLine("Contact saved, press any key to continue");
-        ReadKey();
+            WriteLine("Contact saved, press any key to continue");
+            ReadKey();
+        }
+        catch (ArgumentException)
+        {
+            WriteLine("baran");
+            throw;
+        }
+        finally
+        {
+            WriteLine("finally done");
+        }
     }
 
     static void RemoveContact()
@@ -97,83 +160,16 @@ internal class Program
             }
         }
 
-        RewriteContacts(newContacts);
-
-        WriteLine($"{contacts.Length - newContacts.Length} Contact(s) removed, press any key to continue");
-        ReadKey();
-    }
-    static void SearchContact()
-    {
-        Clear();
-        var contacts = ReadContacts(filename);
-        WriteLine("Enter Contact First or Last Name:");
-        string searchKey = ReadLine();
-
-        int counter = 0;
-        foreach (var (firstName, lastName, phoneNumber) in contacts)
-        {
-            if (firstName.ToLower() == searchKey.ToLower() || lastName.ToLower() == searchKey.ToLower())
-            {
-                ShowRow((firstName, lastName, phoneNumber));
-                ++counter;
-            }
-        }
-        if (counter == 0)
-        {
-            WriteLine("No Contacts Found!");
-        }
-        else
-        {
-            WriteLine($"{counter} {(counter == 1 ? "contact was" : "contacts were")} found");
-        }
-        WriteLine("Press any key to continue...");
-        ReadKey();
-
-    }
-    static void UpdateContact()
-    {
-        Clear();
-        var contacts = ReadContacts(filename);
-        WriteLine("Provide Information About The Contact You Would Like To Update");
-        WriteLine("First Name:");
-        string firstName = ReadLine();
-        WriteLine("Last Name:");
-        string lastName = ReadLine();
-        bool isContactFound = false;
-
-        for (int i = 0; i < contacts.Length; i++)
-        {
-            if (contacts[i].Item1.ToLower() == firstName.ToLower() && contacts[i].Item2.ToLower() == lastName.ToLower())
-            {
-                isContactFound = true;
-                WriteLine($"Enter a new phone number for {firstName} {lastName}:");
-                string phoneNumber = ReadLine();
-                contacts[i].Item3 = phoneNumber;
-                WriteLine("Contact After Update:");
-                ShowRow(contacts[i]);
-                break; // let's assume that there will be no contacts with same First Name and Last Name 
-            }
-        }
-        if (isContactFound)
-        {
-            RewriteContacts(contacts);
-        }
-        else
-        {
-            WriteLine("No Contacts Found With such First Name and Last Name!");
-        }
-        WriteLine("Press any key to continue...");
-        ReadKey();
-    }
-    static void RewriteContacts((string, string, string)[] contacts)
-    {
         var csvBuilder = new StringBuilder();
-        foreach (var contact in contacts)
+        foreach (var contact in newContacts)
         {
             csvBuilder.AppendLine(Serialize(contact));
         }
 
         File.WriteAllText(filename, csvBuilder.ToString());
+
+        WriteLine($"{contacts.Length - newContacts.Length} Contact(s) removed, press any key to continue");
+        ReadKey();
     }
 
     static void MainMenu()
@@ -185,9 +181,8 @@ internal class Program
         WriteLine("Menu:");
         WriteLine("\t1 - Show all contacts");
         WriteLine("\t2 - Add new contact");
-        WriteLine("\t3 - Search contact");
+        WriteLine("\t3 - Update contact");
         WriteLine("\t4 - Remove contact");
-        WriteLine("\t5 - Update contact");
         WriteLine("\t0 - Exit");
 
         var key = ReadKey();
@@ -203,26 +198,26 @@ internal class Program
             case ConsoleKey.D2:
                 AddNewContact();
                 break;
-            case ConsoleKey.D3:
-                SearchContact();
-                break;
             case ConsoleKey.D4:
                 RemoveContact();
-                break;
-            case ConsoleKey.D5:
-                UpdateContact();
                 break;
             default:
                 break;
         }
     }
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-
-
         while (true)
         {
-            MainMenu();
+            try
+            {
+                MainMenu();
+            }
+            catch (Exception e)
+            {
+                WriteLine($"Unhandled error: {e.Message}");
+                ReadKey();
+            }
         }
     }
 }
