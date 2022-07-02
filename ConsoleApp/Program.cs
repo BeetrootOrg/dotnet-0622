778fb9a40c1437e9ebd7a11f734aa9e93e7a0f63
d.misik <misik.dmitriy@gmail.com>
2022-07-02 11:28:51 +0300

    protected

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -22,14 +22,31 @@ class Class42 : IFourtyTwo, ISum
 abstract class Animal
 {
     public abstract void Say();
+    public void MakeStunt()
+    {
+        WriteLine("Prepare for the stunt...");
+        MakeStuntInternal();
+        WriteLine("DONE! BRAVO!");
+    }
+
+    protected virtual void MakeStuntInternal() => WriteLine("No stunt :(");
 }
 
 class Cat : Animal
 {
     public override void Say() => WriteLine("meow");
+
+    protected override void MakeStuntInternal() => WriteLine("IM ON TWO LEGS, BITCH");
 }
 
 class Dog : Animal
+{
+    public override void Say() => WriteLine("raf");
+
+    protected override void MakeStuntInternal() => WriteLine("RAF RAF, I'M DANGER");
+}
+
+class Cockroach : Animal
 {
     public override void Say() => WriteLine("raf");
 }
@@ -39,8 +56,14 @@ class Program
     private static void Main()
     {
         Animal cat1 = new Cat();
+        Dog dog1 = new Dog();
+
         MakeAnimalSay(cat1);
-        MakeAnimalSay(new Dog());
+        MakeAnimalSay(dog1);
+
+        MakeAnimalStunt(cat1);
+        MakeAnimalStunt(dog1);
+        MakeAnimalStunt(new Cockroach());
 
         var fourtyTwo = new Class42();
         Show42(fourtyTwo);
@@ -48,6 +71,7 @@ class Program
     }
 
     private static void MakeAnimalSay(Animal animal) => animal.Say();
+    private static void MakeAnimalStunt(Animal animal) => animal.MakeStunt();
     private static void Show42(IFourtyTwo fourtyTwo) => WriteLine(fourtyTwo.FourtyTwo());
     private static void ShowSum(ISum sum) => WriteLine(sum.Sum(3, 5));
 }
