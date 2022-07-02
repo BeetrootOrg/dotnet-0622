be813b75fe02b363a5e7e2de13a527095b255c6d
Nahlenko Oleksandr <noapioss@gmail.com>
2022-07-02 10:20:03 +0300

    fixed

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,10 @@
 ﻿internal class Program
 {
-  public static int[,] GetGeneration(int[,] cells, int generation)
+  public static char[,] Execute(char[,] cells)
     {
         int i;
-        int[,] copy = new int[cells.GetLength(0), cells.GetLength(1)];
-        int[,] extraCells = new int[cells.GetLength(0) + 2, cells.GetLength(1) + 2];
+        char[,] copy = new char[cells.GetLength(0), cells.GetLength(1)];
+        char[,] extraCells = new char[cells.GetLength(0) + 2, cells.GetLength(1) + 2];
 
         for (int j = 0; j < cells.GetLength(0); j++)
         {
@@ -14,8 +14,8 @@
             }
         }
 
-        int[,] temp = new int[cells.GetLength(0) + 2, cells.GetLength(1) + 2];
-        for (i = 0; i < generation; i++)
+        char[,] temp = new char[cells.GetLength(0) + 2, cells.GetLength(1) + 2];
+        for (i = 0; i < 1; i++)
         {
             for (int j = 1; j < extraCells.GetLength(0) - 1; j++)
             {
@@ -24,11 +24,11 @@
                     int count = CountOfLive(extraCells, j, k);
                     if (extraCells[j, k] == 1)
                     {
-                        if (count == 2 || count == 3) temp[j, k] = 1;
+                        if (count == 2 || count == 3) temp[j, k] = '*';
                     }
                     else
                     {
-                        if (count == 3) temp[j, k] = 1;
+                        if (count == 3) temp[j, k] = '*';
                     }
                 }
             }
@@ -38,7 +38,7 @@
                 for (int k = 1; k < extraCells.GetLength(1) - 1; k++)
                 {
                     extraCells[j, k] = temp[j, k];
-                    temp[j, k] = 0;
+                    temp[j, k] = '.';
                 }
             }
         }
@@ -55,17 +55,17 @@
     }
 
 
-    static int CountOfLive(int[,] a, int i, int j)
+    static int CountOfLive(char[,] a, int i, int j)
     {
         int live = 0;
-        if (a[i - 1, j - 1] == 1) live++;
-        if (a[i - 1, j] == 1) live++;
-        if (a[i - 1, j + 1] == 1) live++;
-        if (a[i, j - 1] == 1) live++;
-        if (a[i, j + 1] == 1) live++;
-        if (a[i + 1, j - 1] == 1) live++;
-        if (a[i + 1, j] == 1) live++;
-        if (a[i + 1, j + 1] == 1) live++;
+        if (a[i - 1, j - 1] == '*') live++;
+        if (a[i - 1, j] == '*') live++;
+        if (a[i - 1, j + 1] == '*') live++;
+        if (a[i, j - 1] == '*') live++;
+        if (a[i, j + 1] == '*') live++;
+        if (a[i + 1, j - 1] == '*') live++;
+        if (a[i + 1, j] == '*') live++;
+        if (a[i + 1, j + 1] == '*') live++;
         return live;
 
     }
