f0821d33348bd661b82003f189afc699cae7ead6
PeEpEGa <106858735+PeEpEGa@users.noreply.github.com>
2022-07-02 11:33:58 +0300

    GameOfLife

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,89 +1,69 @@
-﻿Console.WriteLine("Compare method:");
-string str_1 = "aufwbferfu";
-string str_2 = "aufwbferfu";
-Console.WriteLine(Compare(str_1, str_2));
-Console.WriteLine();
-
-static bool Compare(string comp_1, string comp_2)
+﻿public class GameOfLife
 {
-    if (comp_1.Length != comp_2.Length) return false;
-    else
+    public  static char[,] Execute(char[,] cells)
     {
-        for (int i = 0; i < comp_1.Length; i++)
+        char[,] copy = new char[cells.GetLength(0), cells.GetLength(1)];
+        Array.Copy(cells,copy,cells.Length);
+        for (int i = 0; i < cells.GetLength(0); i++)
         {
-            if (comp_1[i] != comp_2[i]) return false;
-        }
-        return true;
-    }
-}
-
-
-Console.WriteLine("Sort method:");
-string temp = "bicadw";
-string res = Sort(temp);
-Console.WriteLine(res);
-Console.WriteLine();
-
-static string Sort(string strToSort)
-{
-    char[] symbolsToSort = strToSort.ToCharArray();
-    Array.Sort(symbolsToSort);
-    return new string(symbolsToSort);
-}
-
-
-Console.WriteLine("Duplicate method:");
-string test = "aaaaaaabbbbbbbjjasdnjgnnuabbz";
-char[] chars = Duplicate(test);
-foreach (var item in chars)
-{
-    Console.Write(item + " ");
-}
-Console.WriteLine("\n");
-
-static char[] Duplicate(string inputStr)
-{
-    string duplicateChars = "";
-    for (int i = 0; i < inputStr.Length; i++)
-    {
-        if (inputStr.IndexOf(inputStr[i]) != inputStr.LastIndexOf(inputStr[i]))
-        {
-            duplicateChars += inputStr[i];
-            while (inputStr.IndexOf(inputStr[i]) != inputStr.LastIndexOf(inputStr[i]))
+            for (int j = 0; j < cells.GetLength(1); j++)
             {
-                inputStr = inputStr.Remove(inputStr.LastIndexOf(inputStr[i]), 1);
+                int count = 0;
+                string elements = "";
+                if (i == 0 || j == 0 || i == cells.GetLength(0) - 1 || j == cells.GetLength(1) - 1) continue;
+                else
+                {
+                    elements += cells[i - 1, j - 1] == '*' ? '*' : null;
+                    elements += cells[i - 1, j] == '*' ? '*' : null;
+                    elements += cells[i - 1, j + 1] == '*' ? '*' : null;
+                    elements += cells[i, j - 1] == '*' ? '*' : null;
+                    elements += cells[i, j + 1] == '*' ? '*' : null;
+                    elements += cells[i + 1, j - 1] == '*' ? '*' : null;
+                    elements += cells[i + 1, j] == '*' ? '*' : null;
+                    elements += cells[i + 1, j + 1] == '*' ? '*' : null;
+                    count = elements.Length;
+                    count = elements.Length >= 2 && elements.Length < 4 ? 1 : 0;
+                    switch (cells[i,j])
+                    {
+                        case '*':
+                            {
+                                switch (count)
+                                {
+                                    case 0:
+                                        {
+                                            copy[i, j] = '.';
+                                            break;
+                                        }
+                                    case 1:
+                                        {
+                                            copy[i, j] = '*';
+                                            break;
+                                        }
+                                }
+                                break;
+                            }
+                        case '.':
+                            {
+                                switch (elements.Length == 3)
+                                {
+                                    case false:
+                                        {
+                                            copy[i, j] = '.';
+                                            break;
+                                        }
+                                    case true:
+                                        {
+                                            copy[i, j] = '*';
+                                            break;
+                                        }
+                                }
+                                break;
+                            }
+                    }
+                    
+                }
             }
         }
+        return copy;
     }
-    char[] dupChars = duplicateChars.ToCharArray();
-    return dupChars;
-}
-
-
-Console.WriteLine("Analyze method:");
-string result = "1ghif46sf4/>a,awe<?sad";
-Tuple<int, int, int> resultTuple = Analyze(result);
-Console.WriteLine(resultTuple.ToString());
-
-static Tuple<int, int, int> Analyze(string strToAnalyze)
-{
-    int alphabet = 0;
-    int digit = 0;
-    int specialCharacters = 0;
-    for (int i = 0; i < strToAnalyze.Length; i++)
-    {
-        if (Char.IsLetter(strToAnalyze[i]))
-        {
-            alphabet++;
-        }
-        else if (Char.IsDigit(strToAnalyze[i]))
-        {
-            digit++;
-        }
-        else
-        {
-            specialCharacters++;
-        }
-    }
-    return Tuple.Create(alphabet, digit, specialCharacters);
 }
