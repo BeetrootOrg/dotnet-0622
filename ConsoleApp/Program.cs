61fbb9ab3d41fb54bd008f54e7144338cd30e38e
d.misik <misik.dmitriy@gmail.com>
2022-07-02 10:58:37 +0300

    abstract

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,11 +5,17 @@ namespace ConsoleApp;
 abstract class Animal
 {
     public abstract void Say();
+    public int FourtyTwo() => 42;
 }
 
 class Cat : Animal
 {
     public override void Say() => WriteLine("meow");
+}
+
+class Dog : Animal
+{
+    public override void Say() => WriteLine("raf");
 }
 
 class Program
@@ -18,7 +24,12 @@ class Program
     {
         Animal cat1 = new Cat();
         MakeAnimalSay(cat1);
+        MakeAnimalSay(new Dog());
+
+        MakeAnimal42(cat1);
+        MakeAnimal42(new Dog());
     }
 
     private static void MakeAnimalSay(Animal animal) => animal.Say();
+    private static void MakeAnimal42(Animal animal) => WriteLine(animal.FourtyTwo());
 }
