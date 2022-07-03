5aa6cc2553c3a042f725eca163195c70ec7b7a9f
Killjaen <killjaen@gmail.com>
2022-07-03 15:00:28 +0300

    GetType

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Weapon[] inventory = { new M17(), new Ballista(), new Bow() };
+
+            foreach (var item in inventory)
+            {
+                person.Showinfo(item);
+                person.Fire(item);
+            }
+
+        }
+    }
+
+    abstract class Weapon
+    {
+        public abstract void Fire();
+        public void Showinfo()
+        {
+            Console.WriteLine(GetType().Name);
+        }
+
+    }
+
+    class Person
+    {
+        public void Fire(Weapon weapon)
+        {
+            weapon.Fire();
+        }
+        public void Showinfo(Weapon weapon)
+        {
+            weapon.Showinfo();
+        }
+
+    }
+
+    class M17 : Weapon
+    {
+        public override void Fire()
+        {
+            Console.WriteLine("тататататата");
+            Console.WriteLine();
+        }
+    }
+
+    class Ballista : Weapon
+    {
+        public override void Fire()
+        {
+            Console.WriteLine("ВЖЖЖЖУУУУХ");
+            Console.WriteLine();
+        }
+    }
+
+    class Bow : Weapon
+    {
+        public override void Fire()
+        {
+            Console.WriteLine("ПиииууууТрррррррр");
+            Console.WriteLine();
+        }
+    }
+}
+
+
