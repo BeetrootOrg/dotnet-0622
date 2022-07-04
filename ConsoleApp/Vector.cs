a61b20fa9a087b78d0d35f62a0deb4c0b9ecd0d7
d.misik <misik.dmitriy@gmail.com>
2022-07-04 19:24:08 +0300

    ToString

--- ConsoleApp/Vector.cs
+++ ConsoleApp/Vector.cs
@@ -1,7 +1,20 @@
+using System;
+
 namespace ConsoleApp;
 
-struct Vector
+struct Vector : IEquatable<Vector>
 {
     public double X { get; init; }
     public double Y { get; init; }
+
+    public bool Equals(Vector other)
+    {
+        return X == other.X &&
+            Y == other.Y;
+    }
+
+    public override string ToString()
+    {
+        return $"{{X: {X}, Y: {Y}}}";
+    }
 }
