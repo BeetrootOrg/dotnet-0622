b7b46ad8f31ab3749ad57e78abe9d207f6e73f0d
d.misik <misik.dmitriy@gmail.com>
2022-07-04 19:20:21 +0300

    struct

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,77 +1,22 @@
-﻿using static System.Console;
+﻿using ConsoleApp;
 
-namespace ConsoleApp;
+using static System.Console;
 
-interface IFourtyTwo
+var vector1 = new Vector
 {
-    int FourtyTwo();
-}
+    X = 5,
+    Y = 10
+};
 
-interface ISum
+var vector2 = new Vector
 {
-    int Sum(int a, int b);
-}
+    X = 5,
+    Y = 10
+};
 
-class Class42 : IFourtyTwo, ISum
-{
-    public int FourtyTwo() => 42;
-
-    public int Sum(int a, int b) => a + b;
-}
-
-abstract class Animal
-{
-    public abstract void Say();
-    public void MakeStunt()
-    {
-        WriteLine("Prepare for the stunt...");
-        MakeStuntInternal();
-        WriteLine("DONE! BRAVO!");
-    }
-
-    protected virtual void MakeStuntInternal() => WriteLine("No stunt :(");
-}
-
-class Cat : Animal
-{
-    public override void Say() => WriteLine("meow");
-
-    protected override void MakeStuntInternal() => WriteLine("IM ON TWO LEGS, BITCH");
-}
-
-class Dog : Animal
-{
-    public override void Say() => WriteLine("raf");
-
-    protected override void MakeStuntInternal() => WriteLine("RAF RAF, I'M DANGER");
-}
-
-class Cockroach : Animal
-{
-    public override void Say() => WriteLine("raf");
-}
-
-class Program
-{
-    private static void Main()
-    {
-        Animal cat1 = new Cat();
-        Dog dog1 = new Dog();
-
-        MakeAnimalSay(cat1);
-        MakeAnimalSay(dog1);
-
-        MakeAnimalStunt(cat1);
-        MakeAnimalStunt(dog1);
-        MakeAnimalStunt(new Cockroach());
-
-        var fourtyTwo = new Class42();
-        Show42(fourtyTwo);
-        ShowSum(fourtyTwo);
-    }
-
-    private static void MakeAnimalSay(Animal animal) => animal.Say();
-    private static void MakeAnimalStunt(Animal animal) => animal.MakeStunt();
-    private static void Show42(IFourtyTwo fourtyTwo) => WriteLine(fourtyTwo.FourtyTwo());
-    private static void ShowSum(ISum sum) => WriteLine(sum.Sum(3, 5));
-}
+WriteLine($"vector1.ToString() = {vector1}");
+WriteLine($"vector1.Equals(vector2) = {vector1.Equals(vector2)}");
+// Compilation error below
+// WriteLine($"vector1 == vector2 = {vector1 == vector2}");
+WriteLine($"vector1.GetHashCode() = {vector1.GetHashCode()}");
+WriteLine($"vector2.GetHashCode() = {vector2.GetHashCode()}");
---
+++ ConsoleApp/Vector.cs
@@ -0,0 +1,7 @@
+namespace ConsoleApp;
+
+struct Vector
+{
+    public double X { get; init; }
+    public double Y { get; init; }
+}
