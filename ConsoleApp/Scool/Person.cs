c1f6a9ba3e8b41a19bd2298f4e46252b00c37652
ztgarry <as.tkachuk@gmail.com>
2022-07-05 15:24:06 +0300

    чс

---
+++ ConsoleApp/Scool/Person.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp.Scool;
+
+class Person
+{
+    public string FirstName { get; init; }
+    public string LastName { get; init; }
+    public DateTime DateOfBirth { get; init; }
+    private string _email { get; set; }
+}
