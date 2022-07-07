ed7d0ca4a2489b81f5342d2cc305119cbae479c6
Nahlenko Oleksandr <noapioss@gmail.com>
2022-07-07 17:09:03 +0300

    reversi

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,26 +14,267 @@ char[,] Calculate(char[,] field, char turn)
     {
         for (int j = 0; j < field.GetLength(1); j++)
         {
-            if (field[i,j] == '.') 
+            if (field[i,j] == turn) 
             {
-                if (Near(temp, notTurn, i+1, j+1)) temp[i+1,j+1] = '0';
+                foreach(var point in RayCast(field, notTurn, i, j))
+                {
+                    temp[point.Item1+1, point.Item2+1] = 'O';
+                }
             }
         }
     }
-    return temp;
+    char[,] result = new char[field.GetLength(0),field.GetLength(1)];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j] = temp[i+1,j+1];       
+        }
+    }
+    return result;
 }
 
-bool Near(char[,] field, char notTurn, int i, int j)
+List<(int,int)> RayCast(char[,] field, char notTurn, int x, int y)
 {
-    if (field[i-1,j-1] == notTurn ||
-        field[i-1,j] == notTurn ||
-        field[i-1,j+1] == notTurn ||
-        field[i,j-1] == notTurn ||
-        field[i,j+1] == notTurn ||
-        field[i+1,j-1] == notTurn ||
-        field[i+1,j] == notTurn ||
-        field[i+1,j+1] == notTurn) return true;
-    else return false;
+    List<(int,int)> result = new List<(int,int)>();
+    if ((x,y) != RayCastUp(field, notTurn, x, y)) result.Add(RayCastUp(field, notTurn, x, y));
+    if ((x,y) != RayCastUpRight(field, notTurn, x, y)) result.Add(RayCastUpRight(field, notTurn, x, y));
+    if ((x,y) != RayCastRight(field, notTurn, x, y)) result.Add(RayCastRight(field, notTurn, x, y));
+    if ((x,y) != RayCastDownRight(field, notTurn, x, y)) result.Add(RayCastDownRight(field, notTurn, x, y));
+    if ((x,y) != RayCastDown(field, notTurn, x, y)) result.Add(RayCastDown(field, notTurn, x, y));
+    if ((x,y) != RayCastDownLeft(field, notTurn, x, y)) result.Add(RayCastDownLeft(field, notTurn, x, y));
+    if ((x,y) != RayCastLeft(field, notTurn, x, y)) result.Add(RayCastLeft(field, notTurn, x, y));
+    if ((x,y) != RayCastUpLeft(field, notTurn, x, y)) result.Add(RayCastUpLeft(field, notTurn, x, y));
+    return result;
+}
+
+
+(int, int) RayCastUp(char[,] field, char notTurn, int x, int y)
+{
+    char turn = notTurn == 'B' ? 'W' : 'B';
+    int s = 0, i = x, j = y;
+    while (i > 0)
+    {
+        i--;
+        if (field[i,j] == notTurn) 
+        {
+            s++;
+            continue;
+        }
+        if (field[i,j] == turn)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s == 0)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s > 0)
+        {
+            return (i, j);
+        }
+    }
+    return (x,y);    
+}
+
+(int, int) RayCastDown(char[,] field, char notTurn, int x, int y)
+{
+    char turn = notTurn == 'B' ? 'W' : 'B';
+    int s = 0, i = x, j = y;
+    while (i < field.GetLength(0)-1)
+    {
+        i++;
+        if (field[i,j] == notTurn) 
+        {
+            s++;
+            continue;
+        }
+        if (field[i,j] == turn)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s == 0)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s > 0)
+        {
+            return (i, j);
+        }
+    }
+    return (x,y);    
+}
+
+(int, int) RayCastLeft(char[,] field, char notTurn, int x, int y)
+{
+    char turn = notTurn == 'B' ? 'W' : 'B';
+    int s = 0, i = x, j = y;
+    while (j > 0)
+    {
+        j--;
+        if (field[i,j] == notTurn) 
+        {
+            s++;
+            continue;
+        }
+        if (field[i,j] == turn)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s == 0)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s > 0)
+        {
+            return (i, j);
+        }
+    }
+    return (x,y);    
+}
+
+(int, int) RayCastRight(char[,] field, char notTurn, int x, int y)
+{
+    char turn = notTurn == 'B' ? 'W' : 'B';
+    int s = 0, i = x, j = y;
+    while (j <field.GetLength(1)-1)
+    {
+        j++;
+        if (field[i,j] == notTurn) 
+        {
+            s++;
+            continue;
+        }
+        if (field[i,j] == turn)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s == 0)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s > 0)
+        {
+            return (i, j);
+        }
+    }
+    return (x,y);      
+}
+
+(int, int) RayCastUpRight(char[,] field, char notTurn, int x, int y)
+{
+    char turn = notTurn == 'B' ? 'W' : 'B';
+    int s = 0, i = x, j = y;
+    while (i > 0 && j <field.GetLength(1)-1)
+    {
+        i--;
+        j++;
+        if (field[i,j] == notTurn) 
+        {
+            s++;
+            continue;
+        }
+        if (field[i,j] == turn)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s == 0)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s > 0)
+        {
+            return (i, j);
+        }
+    }
+    return (x,y);     
+}
+
+(int, int) RayCastDownRight(char[,] field, char notTurn, int x, int y)
+{
+    char turn = notTurn == 'B' ? 'W' : 'B';
+    int s = 0, i = x, j = y;
+    while (i < field.GetLength(0)-1 && j <field.GetLength(1)-1)
+    {
+        i++;
+        j++;
+        if (field[i,j] == notTurn) 
+        {
+            s++;
+            continue;
+        }
+        if (field[i,j] == turn)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s == 0)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s > 0)
+        {
+            return (i, j);
+        }
+    }
+    return (x,y);    
+}
+
+(int, int) RayCastDownLeft(char[,] field, char notTurn, int x, int y)
+{
+    char turn = notTurn == 'B' ? 'W' : 'B';
+    int s = 0, i = x, j = y;
+    while (j > 0 && i < field.GetLength(0)-1)
+    {
+        i++;
+        j--;
+        if (field[i,j] == notTurn) 
+        {
+            s++;
+            continue;
+        }
+        if (field[i,j] == turn)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s == 0)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s > 0)
+        {
+            return (i, j);
+        }
+    }
+    return (x,y);      
+}
+
+(int, int) RayCastUpLeft(char[,] field, char notTurn, int x, int y)
+{
+    char turn = notTurn == 'B' ? 'W' : 'B';
+    int s = 0, i = x, j = y;
+    while (i > 0 && j > 0)
+    {
+        i--;
+        j--;
+        if (field[i,j] == notTurn) 
+        {
+            s++;
+            continue;
+        }
+        if (field[i,j] == turn)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s == 0)
+        {
+            break;
+        }
+        if (field[i,j] == '.' && s > 0)
+        {
+            return (i, j);
+        }
+    }
+    return (x,y);     
 }
 
 void WriteArray(char[,] arr)
@@ -59,4 +300,4 @@ char[,] input = new char[8,8] { {'.','.','.','.','.','.','.','.'},
 
 WriteArray(input);
 Console.WriteLine();
-WriteArray(Calculate(input, 'W'));
+WriteArray(Calculate(input, 'B'));
