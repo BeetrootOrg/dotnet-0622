43499e2615c541d9025d2efb78f6771a32d95a3f
Killjaen <killjaen@gmail.com>
2022-07-07 22:42:45 +0300

    Done

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp;
+
+
+MyStack<int> stack1 = new MyStack<int>();
+
+
+stack1.Push(1);
+stack1.Push(2);
+stack1.Push(3);
+stack1.Push(4);
+stack1.Push(5);
+stack1.Push(6);
+
+
+
+
+
+
