aacbc05b8d16b07a905d244a55eebbca4557f9ff
Anarik <106695465+Anariks@users.noreply.github.com>
2022-07-07 14:49:13 +0300

    Stack is done

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using static System.Console;
-//namespace ConsoleApp;
+namespace ConsoleApp;
 using ConsoleApp;
 
 class Program 
@@ -8,7 +8,31 @@ class Program
     public static void Main() 
     {
         
+    var stack = new Stack<string>();
+
+   stack.Push("first element");
+   stack.Push("second element");
+   stack.Push("last element");
+    WriteLine(stack.Peek());
+    WriteLine(stack.Count());  
+
+    WriteLine(stack.Pop());
+    WriteLine(stack.Peek());
+    WriteLine(stack.Count());
+
+
+   // stack.Clear();
+    //WriteLine(stack.Peek());
+    WriteLine(stack.Count());  
+    stack.Push("last-last element");
+    WriteLine(stack.Peek());
+    WriteLine(stack.Count());  
+
     
-        
+    string [] arr = new string[2];
+    stack.CopyTo(arr);
+    ShowArray(arr);
+    void ShowArray<T>(T[] arr) => WriteLine(string.Join(", ", arr));
+
     }
 }
