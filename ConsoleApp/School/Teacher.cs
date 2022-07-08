4e5e2fabee032a94d2a1eafc987de3a2f45b819e
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-07-08 17:57:50 +0300

    person

--- ConsoleApp/School/Teacher.cs
+++ ConsoleApp/School/Teacher.cs
@@ -2,9 +2,8 @@ namespace ConsoleApp.School;
 
 class Teacher : Person
 {
-    public Teacher(string firstName, string lastName,
-        string middleName,
-        byte age, 
+    public Teacher(string firstName, string lastName, string middleName,
+        byte age,
         Subject subject,
         ClassRoom classRoom)
     {
@@ -16,8 +15,6 @@ class Teacher : Person
         classRoom = ClassRoom;
     }
     public string MiddleName { get; set; }
-    public Subject Subject { get; private set; }
-    public ClassRoom ClassRoom { get; private set; }
-
-    public string GetNameTeacher() => $"First Name = {FirstName};Last Name = {LastName}; Age = {Age};  Subject = {Subject}; ClassRoom {ClassRoom}";
+    public Subject Subject { get; set; }
+    public ClassRoom ClassRoom { get; set; }
 }
