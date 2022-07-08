c91cd0f95024f10dbf720fd6071338ac913b019e
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-07-08 17:59:59 +0300

    delete garbage

--- ConsoleApp/Library/Author.cs
+++
@@ -1,7 +0,0 @@
-namespace ConsoleApp.Library;
-
-class Author
-{
-    public string FirstNameAuthor { get; init; }
-    public string LastNameAuthor { get; init; }
-}
--- ConsoleApp/Library/Language.cs
+++
@@ -1,8 +0,0 @@
-namespace ConsoleApp.Library;
-enum Language
-{
-    Ukraine,
-    English,
-    German,
-    Italian
-}
