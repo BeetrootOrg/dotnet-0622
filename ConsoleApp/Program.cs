a377b5ef285fc7be207def467c589ec97357e24d
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-07-08 17:58:59 +0300

    change person

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,13 +2,12 @@
 
 public class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        var teacher = new Teacher(firstName: "Ivan", lastName: "Ivanovych",
-        middleName: "Ivanov", age: 50, subject: Subject.Mathematics, classRoom: ClassRoom._101);
-        System.Console.WriteLine(teacher.GetNameTeacher());
+        Teacher teacher = new Teacher(firstName: "Ivan", lastName: "Ivanovych",
+            middleName: "Ivanov", age: 50, subject: Subject.Mathematics, classRoom: ClassRoom._101);
 
-        var student = new Student(firstName: "Vovochka", lastname: "Durachok", age: 6, classesAtSchool: Classes.FirstClass);
-        System.Console.WriteLine(student.GetNameStudent());
+        Student student = new Student(firstName: "Vovochka", lastName: "Durachok", age: 6,
+            classesAtSchool: Classes.FirstClass);
     }
 }
--- ConsoleApp/School/Student.cs
+++ ConsoleApp/School/Student.cs
@@ -3,15 +3,13 @@ namespace ConsoleApp.School;
 
 public class Student : Person
 {
-    public Student(string firstName, string lastname, byte age, Classes classesAtSchool)
+    public Student(string firstName, string lastName, byte age, Classes classesAtSchool)
     {
+
         firstName = FirstName;
-        lastname = LastName;
+        lastName = LastName;
         age = Age;
         classesAtSchool = ClassesAtSchool;
     }
-    
     public Classes ClassesAtSchool { get; set; }
-
-    public string GetNameStudent () => $"First Name = {FirstName};Last Name = {LastName}; Age = {Age}; ClassesAtSchool = {ClassesAtSchool}";
 }
