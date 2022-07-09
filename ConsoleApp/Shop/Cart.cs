76c60510065a624d457bcc9061e2f88547ed66cf
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-07-09 19:44:12 +0300

    AddItem

--- ConsoleApp/Shop/Cart.cs
+++ ConsoleApp/Shop/Cart.cs
@@ -1,7 +1,13 @@
+using System;
 using System.Collections.Generic;
 namespace ConsoleApp.Shop;
 
 class Cart
 {
     public List<ByItem> Items { get; set; }
+
+    public void AddItem(Goods goods, int _amount)
+    {
+        throw new NotImplementedException();
+    }
 }
