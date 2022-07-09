9c3c02f83f32d896cb6e565bfb3fff1a71d0d78b
d.misik <misik.dmitriy@gmail.com>
2022-07-09 10:47:00 +0300

    dictionary add/tryadd/set

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using ConsoleApp;
 
@@ -99,3 +100,53 @@ WriteLine(persons.Contains(new Person
     LastName = "l",
     Weight = 1
 }));
+
+var arr1 = Array.Empty<Person>();
+var arr2 = new Person[1];
+var arr3 = new Person[2];
+var arr4 = new Person[3];
+
+// persons.CopyTo(arr1);
+// persons.CopyTo(arr2);
+persons.CopyTo(arr3);
+persons.CopyTo(arr4);
+
+Print(arr1);
+Print(arr2);
+Print(arr3);
+Print(arr4);
+
+var phoneBook = new Dictionary<string, Person>();
+
+phoneBook.Add("+12345", person);
+phoneBook.Add("+2467", new Person());
+Print(phoneBook);
+
+// Exception below cause element already added
+// phoneBook.Add("+2467", new Person
+// {
+//     FirstName = "A",
+//     LastName = "B",
+//     Height = 123,
+//     Weight = 12
+// });
+
+var result = phoneBook.TryAdd("+2467", new Person
+{
+    FirstName = "A",
+    LastName = "B",
+    Height = 123,
+    Weight = 12
+});
+
+WriteLine($"TryAdd = {result}");
+
+phoneBook["+2467"] = new Person
+{
+    FirstName = "A",
+    LastName = "B",
+    Height = 123,
+    Weight = 12
+};
+
+Print(phoneBook);
