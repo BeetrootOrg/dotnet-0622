4689c9faa93a52f83fea3f24df245f205685a7cb
Oles Borys <oles.borys@gmail.com>
2022-07-09 04:40:15 +0200

    homework

---
+++ ConsoleApp/Customer.cs
@@ -0,0 +1,6 @@
+namespace ConsoleApp;
+
+public class Customer : Person
+{
+    public bool IsPremium { get; set; }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,47 +1,126 @@
-﻿using System;
-public class GameOfLife
+﻿using ConsoleApp;
+using System;
+
+var someCustomer = new Customer();
+
+Product product1 = new Product
 {
-    public static char[,] Execute(char[,] cells)
-    {
-        char[,] result = new char[cells.GetLength(0), cells.GetLength(1)];
-        char[,] copy = new char[cells.GetLength(0) + 2, cells.GetLength(1) + 2];
+    Name = "Swatch",
+    Description = "Quartz",
+    Price = 150
+};
 
-        for (var i = 0; i < cells.GetLength(0); ++i)
-        {
-            for (var j = 0; i < cells.GetLength(1); ++j)
-            {
-                result[i, j] = '.';
-                copy[i + 1, j + 1] = cells[i, j];
-            }
-        }
-        for (int i = 0; i < cells.GetLength(0); i++)
+Product product2 = new Product
+{
+    Name = "Rolex",
+    Description = "Automatic",
+    Price = 15000
+};
 
-            for (int j = 0; j < cells.GetLength(1); j++)
-            {
-                int count = LiveCount(copy, i+1, j+1);
-                if ((count == 2 || count == 3) && copy[i+1, j+1] == '*') result[i,j] = '*';
-                if (count == 3 && copy[i+1, j+1] == '.') result[i,j] = '*';
-            }
+Product product3 = new Product
+{
+    Name = "Citizen",
+    Description = "Solar",
+    Price = 250
+};
 
-
-        return result;
-    }
-    public static int LiveCount(char[,] l, int i, int j)
-    {
-    int live = 0;
-    if (l[i - 1, j - 1] == '*') ++live;
-    if (l[i - 1, j    ] == '*') ++live;
-    if (l[i - 1, j + 1] == '*') ++live;
-    if (l[i,     j - 1] == '*') ++live;
-    if (l[i,     j + 1] == '*') ++live;
-    if (l[i + 1, j - 1] == '*') ++live;
-    if (l[i + 1, j    ] == '*') ++live;
-    if (l[i + 1, j + 1] == '*') ++live;
-    return live;
-    }
+Console.WriteLine("_____Welcome to the shop! :D_____");
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
+Console.Clear();
+Console.WriteLine("Please enter your First Name:");
+var firtName = Console.ReadLine();
+if (string.IsNullOrEmpty(firtName))
+{
+    Console.Clear();
+    Console.WriteLine("Buuu :(");
+}
+else 
+{
+    someCustomer.FirstName = firtName;
 }
 
+Console.WriteLine("Please enter your Last Name:");
+var lastName = Console.ReadLine();
+if (string.IsNullOrEmpty(lastName))
+{
+    Console.Clear();
+    Console.WriteLine("Buuu :(");
+}
+else
+{
+    someCustomer.LastName = lastName;
+}
 
-// 123
-// 4 6
-// 789
+Console.WriteLine("Please enter your Age:");
+int age = Int32.Parse(Console.ReadLine());
+if (age == 42)
+{
+    someCustomer.Age = age;
+    someCustomer.IsPremium = true;
+    product1.Price = 75;
+    product2.Price = 13000;
+    product3.Price = 199;
+}
+else
+{
+    someCustomer.Age = age;
+    someCustomer.IsPremium = false;
+}
+
+Console.Clear();
+Console.WriteLine("Thank you for the account information! :)");
+
+    Console.WriteLine("Shop Menu:");
+    Console.WriteLine("\t1 - Show all products");
+    Console.WriteLine("\t2 - Enter cart");
+    Console.WriteLine("\t0 - Exit");
+
+    var key = Console.ReadKey();
+
+    switch (key.Key)
+    {
+        case ConsoleKey.D0:
+            Environment.Exit(0);
+            break;
+        case ConsoleKey.D1:
+            Console.Clear();
+            Product.ListProduct(product1);
+            Product.ListProduct(product2);
+            Product.ListProduct(product3);
+            Console.WriteLine("Please add products to the cart by entering their numbers 1, 2 or 3");
+            var myCart = new Cart();
+            var productsSelected = Console.ReadLine();
+            if (string.IsNullOrEmpty(productsSelected))
+            {
+                Console.Clear();
+                Console.WriteLine("Brrrr!");
+            }
+            for (var i = 0; i < productsSelected.Length; i++)
+            {
+                if (productsSelected[i] == '1')
+                {
+                    myCart.AddToCart(product1);
+                }
+                if (productsSelected[i] == '2')
+                {
+                    myCart.AddToCart(product2);
+                }
+                if (productsSelected[i] == '3')
+                {
+                    myCart.AddToCart(product3);
+                }
+            }
+            myCart.PrintCart();
+            break;
+        case ConsoleKey.D2:
+            var myCart1 = new Cart();
+            myCart1.PrintCart();
+            break;
+        default:
+            break;
+    }
+
+
+
+
