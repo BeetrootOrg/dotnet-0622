15576feeaca5f5df61143edf84f27df3b0de96da
yurner000 <sysoev.yu@gmail.com>
2022-07-10 02:59:27 +0300

    Finish

--- ConsoleApp/LibVisitors.cs
+++
@@ -1,7 +0,0 @@
-namespace ConsoleApp.LibraryClass;
-class LibVisitor
-{
-    public string FirstName { get; init; }
-    public string LastName { get; init; }
-    public Order[] TakenBooks { get; set; }
-} 
