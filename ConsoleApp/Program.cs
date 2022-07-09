c0698e480c8fa9c99876fa941313cd0704bddd07
Veronika Telelym <ntelelym@gmail.com>
2022-07-09 17:33:27 +0300

    Initial

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,1 @@
-﻿using ConsoleApp.School;
-
-
-Lesson firstLesson = new Lesson
-{
-Subject = "Math",
-Time = "09:00"
-};
-
-Schedule firstGrade = Schedule.CreateSchedule(DayOfWeek.Monday, new Lesson[] {firstLesson}, 2);
+﻿
--- ConsoleApp/School/Lesson.cs
+++
@@ -1,7 +0,0 @@
-namespace ConsoleApp.School;
-
-class Lesson
-{
-    public string Subject { get; init; }
-    public string Time { get; init; }
-}
