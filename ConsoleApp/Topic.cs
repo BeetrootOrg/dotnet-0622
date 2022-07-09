ec59dbe4676e7c6f1e9957fca3450efd4834736c
Killjaen <killjaen@gmail.com>
2022-07-09 17:19:43 +0300

    Done as in example

---
+++ ConsoleApp/Topic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Topic
+    {
+        public string TopicName { get; set; }
+        public string Variant1 { get; set; }
+        public string Variant2 { get; set; }
+
+        public int var1Counter {get; set; }
+
+        public int var2Counter {get; set; }
+
+    }
+}
