a80e6fe2d5df8999cbd94097b5eb746438db889d
Anarik <106695465+Anariks@users.noreply.github.com>
2022-07-10 12:00:56 +0300

    vote_start

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,31 +8,7 @@ class Program
     public static void Main() 
     {
         
-    var stack = new Stack<string>();
-
-   stack.Push("first element");
-   stack.Push("second element");
-   stack.Push("last element");
-    WriteLine(stack.Peek());
-    WriteLine(stack.Count());  
-
-    WriteLine(stack.Pop());
-    WriteLine(stack.Peek());
-    WriteLine(stack.Count());
-
-
-   // stack.Clear();
-    //WriteLine(stack.Peek());
-    WriteLine(stack.Count());  
-    stack.Push("last-last element");
-    WriteLine(stack.Peek());
-    WriteLine(stack.Count());  
-
-    
-    string [] arr = new string[2];
-    stack.CopyTo(arr);
-    ShowArray(arr);
-    void ShowArray<T>(T[] arr) => WriteLine(string.Join(", ", arr));
+   
 
     }
 }
---
+++ ConsoleApp/Vote.cs
@@ -0,0 +1,4 @@
+using System;
+using static System.Console;
+namespace ConsoleApp;
+
