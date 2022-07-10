cd4322e0e00d92f7d0d5c2a544433cff7cdd6f2f
iVanyaVas <vanya.iaschenko@gmail.com>
2022-07-10 12:32:02 +0300

    redone

--- ConsoleApp/InternetShop/Emploee.cs
+++ ConsoleApp/InternetShop/Emploee.cs
@@ -4,5 +4,5 @@ internal class Emploee : Client
 {
     private int _emploeeId { get; init; }
     private decimal _emploeeSalary { get; set; }
-    private DateTime dateTime { get; set; }
+    private DateTime _workingHours { get; set; }
 }
