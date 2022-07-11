645be9e82415d552faa2481ab96e130e1809f35d
d.misik <misik.dmitriy@gmail.com>
2022-07-11 20:58:06 +0300

    snake

---
+++ ConsoleApp/Field.cs
@@ -0,0 +1,8 @@
+namespace ConsoleApp;
+
+class Field
+{
+    public Snake Snake { get; init; }
+    public Food Food { get; init; }
+    public int Size { get; init; } = 15;
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,67 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
+﻿using System.Threading;
 
 using ConsoleApp;
 
-using static System.Console;
+const int Size = 15;
 
-OnChangeHandler onChange = () => WriteLine("Property changed");
-var testClass = new TestClass(onChange);
-
-testClass.Number = 42;
-testClass.Text = "text";
-
-void OnCounterChangeHandler(Counter counter, int oldValue, int newValue)
+var snake = new Snake();
+var food = Food.Random(Size);
+var field = new Field
 {
-    WriteLine($"Counter value changed from {oldValue} to {newValue}");
-}
-
-void AnotherOnCounterChangeHandler(Counter counter, int oldValue, int newValue)
-{
-    WriteLine($"Counter value raised/decreased from {oldValue} to {newValue}");
-}
-
-var counter1 = new Counter();
-var counter2 = new Counter();
-
-counter1.OnCounterChanged += OnCounterChangeHandler;
-counter1.OnCounterChanged += AnotherOnCounterChangeHandler;
-
-counter2.OnCounterChanged += OnCounterChangeHandler;
-
-counter1.Increment();
-counter2.Increment();
-counter2.Decrement();
-
-counter1.OnCounterChanged -= AnotherOnCounterChangeHandler;
-counter1.Increment();
-
-void TimerCallback(object state)
-{
-    var counter = (Counter)state;
-    counter.Increment();
-    WriteLine($"Timer tick {counter.Count}");
-}
-
-var timer = new Timer(TimerCallback, new Counter(), TimeSpan.Zero, TimeSpan.FromSeconds(1));
-
-var array = new[]
-{
-    new Person("FIRST", "LAST", 18),
-    new Person("F", "L", 20),
-    new Person("f", "l", 16),
-    new Person("FF", "LL", 22),
+    Food = food,
+    Size = Size,
+    Snake = snake
 };
 
-void Show<T>(IEnumerable<T> collection)
+var renderer = new Renderer
 {
-    WriteLine(string.Join(", ", collection));
-}
+    Field = field
+};
 
-var number = new[] { 1, 2, 3, 4, 5, 6 };
+System.Console.Clear();
+renderer.Show();
+renderer.StartGame();
 
-var below18 = new FilterEnumerable<Person>(array, (person) => person.Age <= 18);
-var onlyEven = new FilterEnumerable<int>(number, (n) => n % 2 == 0);
-Show(below18);
-Show(onlyEven);
+Thread.Sleep(Timeout.Infinite);
