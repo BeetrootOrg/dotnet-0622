b8bc6fa7d1b94a5a76f65fb6165b2eb7b4c0a4a8
d.misik <misik.dmitriy@gmail.com>
2022-07-11 19:35:33 +0300

    event

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,3 +7,28 @@ var testClass = new TestClass(onChange);
 
 testClass.Number = 42;
 testClass.Text = "text";
+
+void OnCounterChangeHandler(Counter counter, int oldValue, int newValue)
+{
+    WriteLine($"Counter value changed from {oldValue} to {newValue}");
+}
+
+void AnotherOnCounterChangeHandler(Counter counter, int oldValue, int newValue)
+{
+    WriteLine($"Counter value raised/decreased from {oldValue} to {newValue}");
+}
+
+var counter1 = new Counter();
+var counter2 = new Counter();
+
+counter1.OnCounterChanged += OnCounterChangeHandler;
+counter1.OnCounterChanged += AnotherOnCounterChangeHandler;
+
+counter2.OnCounterChanged += OnCounterChangeHandler;
+
+counter1.Increment();
+counter2.Increment();
+counter2.Decrement();
+
+counter1.OnCounterChanged -= AnotherOnCounterChangeHandler;
+counter1.Increment();
