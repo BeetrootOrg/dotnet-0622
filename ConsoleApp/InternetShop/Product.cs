8a6278914726835757adf01539dae3b6ff3eb48d
Danylo Strus <danylo.strus@gmail.com>
2022-07-12 16:44:04 +0300

    metod

--- ConsoleApp/InternetShop/Product.cs
+++ ConsoleApp/InternetShop/Product.cs
@@ -1,18 +1,24 @@
 namespace ConsoleApp.InternetShop;
+
+
 class Product : IAddNewProd
 {
     public string ProductName { get; set; }
     public ProductType ProductType { get; set; }
     public int ProductPrice { get; init; }
-     public int Quantity { get; set; }
+    public int Quantity { get; set; }
 
-    public void AddNewProduct()
+
+    public void ShowProduct()
     {
-        throw new NotImplementedException();
+      System.Console.WriteLine($"Product name:{ProductName}, Product type: {ProductType}, Price:{ProductPrice}, Quantity:{Quantity}");
     }
 }
 
+
+
 interface IAddNewProd
 {
-    public void AddNewProduct();
+    public void ShowProduct();
 }
+
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,8 +2,22 @@
 
 var client1 = new Buyer { FirstName = "Tom", LastName = "Jobs", NumberPhone = "+38900099999" };
 
-var prod1 = new Product { ProductName = "Lenovo Legion 5", ProductType = ProductType.LaptopsAndComputers, ProductPrice = 1500, Quantity = 5 };
-var prod2 = new Product { ProductName = "iPhone 4s", ProductType = ProductType.Smartphones, ProductPrice = 400, Quantity = 1 };
+var prod1 = new Product
+{
+    ProductName = "Lenovo Legion 5",
+    ProductType = ProductType.LaptopsAndComputers,
+    ProductPrice = 1500,
+    Quantity = 5
+};
+var prod2 = new Product
+{
+    ProductName = "iPhone 4s",
+    ProductType = ProductType.Smartphones,
+    ProductPrice = 400,
+    Quantity = 1
+};
+
+prod1.ShowProduct();
 
 var receipt1 = new[]
 {
