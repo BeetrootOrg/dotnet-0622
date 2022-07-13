ce1fb4baadb8846215abd774072d355323934a83
d.misik <misik.dmitriy@gmail.com>
2022-07-13 20:40:10 +0300

    big number

---
+++ ConsoleApp/BigNumber.cs
@@ -0,0 +1,22 @@
+namespace ConsoleApp;
+
+class BigNumber
+{
+    private readonly string _number;
+
+    public BigNumber()
+    {
+        _number = "0";
+    }
+
+    public BigNumber(string number)
+    {
+        _number = number;
+    }
+
+    public static BigNumber operator +(BigNumber bigNumber)
+    {
+        // sum result;
+        return new BigNumber();
+    }
+}
