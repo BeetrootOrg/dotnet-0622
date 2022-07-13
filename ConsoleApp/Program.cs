627415115ed1ee44f6ace1894636031edbaa643e
Anarik <106695465+Anariks@users.noreply.github.com>
2022-07-13 16:59:34 +0300

    start

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using static System.Console;
-namespace ConsoleApp;
-using ConsoleApp;
-
-class Program 
-{
-    public static void Main() 
-    {
-
-     var menu = new Menu();
-     menu.MainMenu();
-    
-//    WriteLine (Topic.Tittle);
-
-    }
-}
+﻿
