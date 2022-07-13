5231c81ffcca6cca0195f34093792ae9502ce753
Oleksandr <sanelka77@yahoo.com>
2022-07-13 20:57:26 +0300

    start

---
+++ ConsoleApp/BigNumber.cs
@@ -0,0 +1,36 @@
+namespace ConsoleApp;
+
+class BigNumber
+{
+    private readonly string _number;
+
+    public BigNumber()
+    {
+        _number = "0";
+    }
+
+    public BigNumber(string number)
+    {
+        _number = number;
+    }
+
+    public static BigNumber operator +(BigNumber bigNumber1, BigNumber bigNumber2)
+    {
+        string buf = "";
+        string res = "";
+        
+        return new BigNumber();
+    }
+
+    public static BigNumber operator -(BigNumber bigNumber1, BigNumber bigNumber2)
+    {
+        // sum result;
+        return new BigNumber();
+    }
+
+    public static BigNumber operator *(BigNumber bigNumber1, BigNumber bigNumber2)
+    {
+        // sum result;
+        return new BigNumber();
+    }
+}
