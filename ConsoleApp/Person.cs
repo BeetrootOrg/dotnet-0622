dd37081fd1bca9eec7c5f43a439f9eb82e739f13
yurner000 <sysoev.yu@gmail.com>
2022-07-13 19:12:14 +0300

    ff

--- ConsoleApp/Person.cs
+++
@@ -1,8 +0,0 @@
-namespace Shop;
-
-internal class Person
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public int Age { get; set; }
-}
--- ConsoleApp/Staff.cs
+++
@@ -1,7 +0,0 @@
-namespace Shop;
-
-internal class Staff : Person
-{
-    private string _jobTitile { get; init; }
-    private decimal _salary { get; set; }
-}
