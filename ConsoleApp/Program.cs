17c22997e24aa4c144794f8860422fec44144fc0
ztgarry <as.tkachuk@gmail.com>
2022-07-14 15:50:35 +0300

    str1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,33 +10,10 @@ class Program : MainMenu
 {
     static void Main(string[] args)
     {
-        //         //         Client MyClient1 = new Client();
-        //         //         MyClient1.AddNewPerson();
-
-        //         //         Employer MyEmployer1 = new Employer();
-        //         //         MyEmployer1.AddNewPerson();
-
-        //         //         Sku Sku1 = new Sku();
-        //         //         Sku1.AddNewSku();
-        // while (true)
-        // {
         while (true)
         {
             MainMenu MyMenu = new MainMenu();
             MyMenu.FirstMenu();
         }
-
-
-        // while (true)
-        //  {
-        //      MyMenu();
-        // 
-
-
-        //     //             Menu();
-        // }
     }
 }
-
-
-//FirstMenu()
