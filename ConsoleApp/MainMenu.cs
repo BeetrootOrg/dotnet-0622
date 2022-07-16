3c95ead67fb6aa7df39f2af66b2ef4e5deefbbae
ztgarry <as.tkachuk@gmail.com>
2022-07-16 12:37:00 +0300

    ф

---
+++ ConsoleApp/MainMenu.cs
@@ -0,0 +1,37 @@
+/*
+Создать меню из нескольких пунктов
+1) Greate Vote
+Enter topic
+--topic---
+Enter comma-setareted options
+yes. no
+END
+
+2) Vote for somethings
+choose topic 
+1) Topic 1
+2) Topic 2
+click 1
+1 Yes
+2 No
+Click 2
+1 Yes
+2 No
+END
+
+3) Show results
+Choose topic
+1. Topic1
+2. Topic2
+Click 1 
+1. Yes
+2. No
+
+0) Exit
+
+
+*/
+class MainMenu
+{
+    static public void
+}
