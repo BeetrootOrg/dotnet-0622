7983e177deaef854247f3c074ebbb9970aa77de5
iVanyaVas <vanya.iaschenko@gmail.com>
2022-07-17 11:40:31 +0300

    init

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,13 +1,104 @@
-﻿using ConsoleApp.MyStack;
+﻿using ConsoleApp.Vote;
 
-MyStack<string> myStack = new MyStack<string>();
+VoteList dataBase = new VoteList();
 
-myStack.Push("a", "b", "c", "d");
-Console.WriteLine(myStack.Peek());
-myStack.Pop();
-Console.WriteLine(myStack.Peek());
-myStack.Pop();
-Console.WriteLine(myStack.Peek());
-string[] list = new string[2];
-list = myStack.CopyTo();
-Console.WriteLine(list);
+
+Menu();
+
+void DrawMenuOptions()
+{
+    Console.Clear();
+    Console.WriteLine("1: Create Vote");
+    Console.WriteLine("2: Vote for something");
+    Console.WriteLine("3: Show Results");
+    Console.WriteLine("0: Exit");
+}
+
+void Menu()
+{
+    while (true)
+    {
+        DrawMenuOptions();
+
+        var option = Console.ReadKey();
+
+        switch (option.Key)
+        {
+
+            //Create Vote
+            case ConsoleKey.D1:
+                {
+                    CreateVote();
+                    break;
+                }
+            //Vote for something
+            case ConsoleKey.D2:
+                {
+                    VoteForSomething();
+                    break;
+                }
+            //Show Results
+            case ConsoleKey.D3:
+                {
+                    ShowResults();
+                    break;
+                }
+            //Show the Tables we have
+            case ConsoleKey.D0:
+                {
+                    
+                   return;
+                }
+
+        }
+    }
+}
+
+void CreateVote()
+{
+    Console.Clear();
+    Console.WriteLine("Input Vote Topic: ");
+    var voteTopic = Console.ReadLine();
+
+    if (!dataBase.CreateVoteTopic(voteTopic.ToLower())) return;
+
+    string message = "Topic is Created!";
+    message.PressAnyKeyToProcced();
+}
+
+void VoteForSomething()
+{
+    Console.Clear();
+    Console.WriteLine("Input Vote Topic: ");
+    var voteTopic = Console.ReadLine();
+
+
+    Console.WriteLine("Vote Variants for YES: yes , yeah , yah , y , 1 , ok ");
+    Console.WriteLine("Vote Variants for NO: no , nah , n , 0");
+
+    Console.WriteLine("Write your vote answer: ");
+    var voteAnswer = Console.ReadLine();
+
+    if (!dataBase.ToVote(voteTopic.ToLower(), voteAnswer.ToLower())) return;
+
+    string message = "Your Vote saved!";
+    message.PressAnyKeyToProcced();
+
+}
+
+void ShowResults()
+{
+    Console.Clear();
+    Console.WriteLine("Input Vote Topic: ");
+    var voteTopic = Console.ReadLine();
+    
+    Console.WriteLine($"-- {voteTopic} --");
+
+    dataBase.Show(voteTopic.ToLower());
+
+    string message = "";
+    message.PressAnyKeyToProcced();
+
+}
+
+
---
+++ ConsoleApp/Vote/VoteList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Vote
+{
+    internal class VoteList
+    {
+        List<Vote> votes { get; set; }
+        public VoteList()
+        {
+            votes = new List<Vote>();
+        }
+
+        public VoteList(Vote vote) : this()
+        {
+            votes.Add(vote);
+        }
+
+        public bool ToVote(string voteTopic, string answer)
+        {
+            Vote vote = FindVote(voteTopic);
+            if (vote == null)
+            {
+                string message = "Didn`t find such vote topic";
+                message.PressAnyKeyToProcced();
+                return false;
+            }
+
+
+            if (!vote.WriteAnswer(answer)) 
+            {
+                return false;
+            }
+            return true;
+        }
+        public Vote FindVote(Vote vote)
+        {
+            Vote findedVote;
+
+            foreach (Vote v in votes)
+            {
+                if (vote.VoteTopic == v.VoteTopic)
+                {
+                    findedVote = v;
+                    return findedVote;
+                }
+            }
+            return null;
+        }
+
+        public Vote FindVote(string voteName)
+        {
+            Vote toFind = new Vote(voteName);
+            Vote findedVote = FindVote(toFind);
+
+            return findedVote == null ? null : findedVote;
+        }
+
+        public bool CreateVoteTopic(string voteTopic)
+        {
+            if(FindVote(voteTopic) != null)
+            {
+                string message = "The topic is already exist";
+                message.PressAnyKeyToProcced();
+                return false;
+            }
+            Vote topic = new Vote(voteTopic);
+            votes.Add(topic);
+            return true;
+        }
+
+        public void Show(string voteTopic)
+        {
+           Vote show =  FindVote(voteTopic);
+            Console.WriteLine($"YES Votes: {show.GetYesVotes}");
+            Console.WriteLine($"NO Votes: {show.GetNoVotes}");
+        }
+
+    }
+}
