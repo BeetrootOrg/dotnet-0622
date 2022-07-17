bef1a9d095f8b5212e3998ad43114833363799a0
iVanyaVas <vanya.iaschenko@gmail.com>
2022-07-17 11:36:59 +0300

    init

---
+++ ConsoleApp/MyStack/Element.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.MyStack
+{
+    internal class Element<T>
+    {
+        public T Data { get; set; }
+        public Element<T> NextElement { get; set; }
+        public Element(T data)
+        {
+            Data = data;
+        }
+    }
+}
