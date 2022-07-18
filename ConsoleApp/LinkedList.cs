a15371999d1dfcc73b30b5f58afa80f9a49af493
ztgarry <as.tkachuk@gmail.com>
2022-07-18 22:23:47 +0300

    dsa

--- ConsoleApp/LinkedList.cs
+++ ConsoleApp/LinkedList.cs
@@ -1,14 +1,42 @@
+using System;
 namespace ConsoleApp;
+//using System.Collections.Generic;
+
 
 class LinkedList<T>
 {
     private record ListItem // create exempl one of list
     {
-        public T Value { get; init}
-        public ListItem Next {get; set}
+        public T Value { get; init;}
+        public ListItem Next { get; set;}
     }
-private ListItem _head;
+    private ListItem _head;
+    public int Length {get; private set;}
 
+
+    public void Add(T value)
+    {
+        var newItem = new ListItem
+        {
+            Next = null,
+            Value = value
+        };
+        if (_head == null)
+        {
+            _head = newItem;
+        }
+        else
+        {
+            var item = _head;
+            while (item.Next != null)
+            {
+                item = item.Next;
+            }
+            item.Next = newItem;
+
+        }
+
+    }
 
 
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,20 +1,36 @@
-﻿using System.Collections.Generic;
-namespace ConsoleApp;
+﻿using ConsoleApp;
+using static System.Console;
+//using System.Collections.Generic;
 
-class Program : MainMenu
+// class Program : MainMenu
+// {
+
+
+//     static void Main(string[] args)
+//     {
+
+// while (true)
+// {
+//     MainMenu MyMenu = new MainMenu();
+//     MyMenu.FirstMenu();
+// }
+internal class Program
 {
-
-
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-
-        while (true)
-        {
-            MainMenu MyMenu = new MainMenu();
-            MyMenu.FirstMenu();
-        }
-  
-        
-
+        Clear();
+        var list = new LinkedList<int>();
+        list.Add(1);
+        WriteLine("1");
+        list.Add(2);
+        WriteLine("2");
+        list.Add(3);
+        WriteLine("3");
     }
 }
+
+
+
+
+//     }
+// }
