e45fd484b124e0cef8e570dbc50b36112274ea20
ztgarry <as.tkachuk@gmail.com>
2022-07-19 14:15:56 +0300

    qws

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,36 +1,53 @@
-﻿using ConsoleApp;
+﻿
+using ConsoleApp;
 using static System.Console;
-//using System.Collections.Generic;
-
-// class Program : MainMenu
-// {
 
 
-//     static void Main(string[] args)
-//     {
+Clear();
 
-// while (true)
-// {
-//     MainMenu MyMenu = new MainMenu();
-//     MyMenu.FirstMenu();
-// }
-internal class Program
+var list = new LinkedList<int>();
+//LinkedList<int> linkedList = new LinkedList<int>();
+list.Add(1);
+WriteLine("1");
+list.Add(2);
+WriteLine("2");
+list.Add(3);
+WriteLine("3");
+
+
+class LinkedList<T>
 {
-    private static void Main(string[] args)
+    private record ListItem // create exempl one of list
     {
-        Clear();
-        var list = new LinkedList<int>();
-        list.Add(1);
-        WriteLine("1");
-        list.Add(2);
-        WriteLine("2");
-        list.Add(3);
-        WriteLine("3");
+        public T Value { get; init; }
+        public ListItem Next { get; set; }
+    }
+    private ListItem _head;
+    public int Length { get; private set; }
+
+
+    public void Add(T value)
+    {
+        var newItem = new ListItem
+        {
+            Next = null,
+            Value = value
+        };
+        if (_head == null)
+        {
+            _head = newItem;
+        }
+        else
+        {
+            var item = _head;
+            while (item.Next != null)
+            {
+                item = item.Next;
+            }
+            item.Next = newItem;
+
+        }
+
     }
 }
 
-
-
-
-//     }
-// }
