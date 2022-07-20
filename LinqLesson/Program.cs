16a79e26640e3de7824a89b1d933b2922c340e43
d.misik <misik.dmitriy@gmail.com>
2022-07-20 19:35:39 +0300

    count

--- LinqLesson/Program.cs
+++ LinqLesson/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+
+using static System.Console;
 
 namespace LinqLesson
 {
@@ -9,6 +12,14 @@ namespace LinqLesson
 		static void Main(string[] args)
 		{
 			var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+
+			// 1. count males
+			var males = persons.Count(person => person.Gender == Gender.Male);
+			WriteLine($"Males: {males}");
+
+			// 2. count females
+			var females = persons.Count(person => person.Gender == Gender.Female);
+			WriteLine($"Females: {females}");
 		}
 	}
 }
