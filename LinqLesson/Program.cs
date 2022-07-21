96dbc113379f470bea6be6f54f76501ad051be9a
Anarik <106695465+Anariks@users.noreply.github.com>
2022-07-21 19:43:06 +0300

    min/max distanses

--- LinqLesson/Program.cs
+++ LinqLesson/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-
 using static System.Console;
 
 namespace LinqLesson
@@ -53,6 +52,55 @@ namespace LinqLesson
 			{
 				WriteLine($"{item.Key} has {item.Count()} people");
 			}
+
+			// 8. common tags
+			var allTagsSelected = persons.Select(x => x.Tags);
+			var allTags = persons.SelectMany(x => x.Tags);
+			var tagsGroups = allTags.GroupBy(x => x);
+			var mainTag = tagsGroups.MaxBy(x => x.Count()).Key;
+
+			var personsWithSameTag = persons.Where(x => x.Tags.Contains(mainTag)).Count();
+			WriteLine($"Persons with same tag '{mainTag}' are {personsWithSameTag}");
+
+            // 9. Parts of World (max latitude)
+
+            var maxNorth = persons.MaxBy( x => x.Latitude);
+            WriteLine ($"Person who live the most North point is {maxNorth.Name}");
+            
+            var maxSouth = persons.MinBy( x => x.Latitude);
+            WriteLine ($"Person who live the most South point is {maxSouth.Name}");
+
+            var maxEast = persons.MaxBy( x => x.Longitude);
+            WriteLine ($"Person who live the most South point is {maxEast.Name}");
+
+            var maxWest = persons.MinBy( x => x.Longitude);
+            WriteLine ($"Person who live the most South point is {maxWest.Name}");
+
+            //10.
+            //var distances = persons.GroupBy
+            var distanses = new List<(double, Person, Person)>();
+
+            for (int i = 0; i < persons.Count(); i++)
+            {   
+                for (int j = i + 1; j < persons.Count(); j++)
+                {   
+                    var person1 = persons.ElementAt(i);
+                    var person2 = persons.ElementAt(j);
+                    var distanceBtwPersons = Distance.CalcDistance(
+                            person1.Latitude, person2.Latitude,
+                            person1.Longitude, person2.Longitude);
+                    distanses.Add((distanceBtwPersons, person1, person2));           
+                }
+            }
+
+            var maxDistanseNote = distanses.MaxBy( x => x.Item1);
+            WriteLine($"Maximum distance is {(int)maxDistanseNote.Item1} km, between "
+                     + $"{maxDistanseNote.Item2.Name} and {maxDistanseNote.Item3.Name}");
+
+            var minDistanseNote = distanses.MinBy( x => x.Item1);
+            WriteLine($"Minimum distance is {(int)minDistanseNote.Item1} km, between "
+                     + $"{minDistanseNote.Item2.Name} and {minDistanseNote.Item3.Name}");
+
 		}
 	}
 }
