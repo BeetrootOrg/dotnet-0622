4a5fecf085a28a888f715d23ced457e3858254bc
Danylo Strus <danylo.strus@gmail.com>
2022-07-23 19:10:45 +0300

    rows

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,22 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Reflection;
 
-using ConsoleApp;
+Type myType = typeof(DateTime);
 
-
-var intType = typeof(Person);
-var assembly = intType.Assembly;
-System.Console.WriteLine($"Assymbly {assembly.FullName}");
-foreach (var assemblyType in assembly.GetTypes().Take(5))
+Console.WriteLine("Rows:");
+foreach (FieldInfo field in myType.GetFields(
+    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static))
 {
-    System.Console.WriteLine($"Type name: {assemblyType.Name}");
-    foreach (var methodInfo in assemblyType.GetMethods())
-    {
-        System.Console.WriteLine($"Metod info: {methodInfo.Name}");
-    }
+    string mod = "";
+    if (field.IsPublic)
+        mod += "public ";
+    else if (field.IsPrivate)
+        mod += "private ";
+    else if (field.IsAssembly)
+        mod += "internal ";
+    else if (field.IsFamily)
+        mod += "protected ";
+    else if (field.IsFamilyAndAssembly)
+        mod += "private protected ";
+    else if (field.IsFamilyOrAssembly)
+        mod += "protected internal ";
+    if (field.IsStatic) mod += "static ";
 
-
+    Console.WriteLine($"{mod}{field.FieldType.Name} {field.Name}");
 }
-
