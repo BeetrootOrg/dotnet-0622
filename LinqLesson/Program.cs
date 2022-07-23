81423578e6a6b2582b931df1a6068d5db20cb62a
Danylo Strus <danylo.strus@gmail.com>
2022-07-23 17:14:18 +0300

    farthest north/south/west/east

--- LinqLesson/Program.cs
+++ LinqLesson/Program.cs
@@ -7,28 +7,28 @@ using static System.Console;
 
 namespace LinqLesson
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
 
-			// 1. count males
-			var males = persons.Count(person => person.Gender == Gender.Male);
-			WriteLine($"Males: {males}");
+            // 1. find out who is located farthest north/south/west/east using latitude/longitude data
+            //north
+			var maxNorth = persons.MaxBy( x => x.Latitude);
+            WriteLine ($"The most North: {maxNorth.Name}");
+			//south
+			var maxSouth = persons.MinBy( x => x.Latitude);
+            WriteLine ($"The most North South: {maxSouth.Name}");
+			//west
+            var maxEast = persons.MaxBy( x => x.Longitude);
+            WriteLine ($"The most West South: {maxEast.Name}");
+			//east
+            var maxWest = persons.MinBy( x => x.Longitude);
+            WriteLine ($"The most Wast South: {maxWest.Name}");
 
-			// 2. count females
-			var females = persons.Count(person => person.Gender == Gender.Female);
-			WriteLine($"Females: {females}");
 
-			// 3. youngest person
-			var youngestAge = persons.Min(person => person.Age);
-			var youngestPerson = persons.MinBy(person => person.Age);
-			WriteLine($"Youngest person is {youngestPerson}");
 
-			// 4. oldest person
-			var oldestPerson = persons.MaxBy(x => x.Age);
-			WriteLine($"Oldest person is {oldestPerson}");
-		}
-	}
+        }
+    }
 }
