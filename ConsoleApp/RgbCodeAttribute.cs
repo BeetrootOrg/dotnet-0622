cf315ea32b2d126ad0acce5aeacf9b06f2da6e4a
d.misik <misik.dmitriy@gmail.com>
2022-07-23 11:28:22 +0300

    rgb code

--- ConsoleApp/RgbCodeAttribute.cs
+++ ConsoleApp/RgbCodeAttribute.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace ConsoleApp;
 
+[AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field)]
 class RgbCodeAttribute : Attribute
 {
     public string Code { get; init; }
