04ee03a69402162f0571b6e4337c652949711544
d.misik <misik.dmitriy@gmail.com>
2022-07-24 15:41:07 +0300

    snake

--- ConsoleApp/Renderer.cs
+++ ConsoleApp/Renderer.cs
@@ -4,6 +4,11 @@ namespace ConsoleApp;
 
 class Renderer
 {
+    private const char ClearSymbol = ' ';
+    private const char BorderSymbol = '\x2588';
+    private const char SnakeSymbol = '\x2588';
+    private const char FoodSymbol = '\x00F6';
+
     public GameField GameField { get; init; }
 
     private Point _lastSnakePart;
@@ -23,7 +28,7 @@ class Renderer
         foreach (var (x, y) in GameField.FieldBorder)
         {
             Console.SetCursorPosition(x, y);
-            Console.Write('*');
+            Console.Write(BorderSymbol);
         }
     }
 
@@ -34,27 +39,34 @@ class Renderer
         if (_lastSnakePart != null)
         {
             Console.SetCursorPosition(_lastSnakePart.X, _lastSnakePart.Y);
-            Console.Write(" ");
+            Console.Write(ClearSymbol);
         }
 
         if (_lastFood != null && !GameField.Food.SamePosition(_lastFood))
         {
-            Console.Write(" ");
+            Console.Write(ClearSymbol);
         }
 
         // snake
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.SetCursorPosition(GameField.Snake.Head.X, GameField.Snake.Head.Y);
+        Console.Write(SnakeSymbol);
+
         Console.ForegroundColor = ConsoleColor.Green;
-        foreach (var snakeBody in GameField.Snake)
+        foreach (var snakeBody in GameField.Snake.Body)
         {
             Console.SetCursorPosition(snakeBody.X, snakeBody.Y);
-            Console.Write("X");
+            Console.Write(SnakeSymbol);
             _lastSnakePart = new Point(snakeBody.X, snakeBody.Y);
         }
 
         // food
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.SetCursorPosition(GameField.Food.X, GameField.Food.Y);
-        Console.Write("Y");
+        Console.Write(FoodSymbol);
         _lastFood = new Point(GameField.Food.X, GameField.Food.Y);
+
+        // set cursor position outside the world
+        Console.SetCursorPosition(GameField.FieldBorder.Size + 1, GameField.FieldBorder.Size + 1);
     }
 }
