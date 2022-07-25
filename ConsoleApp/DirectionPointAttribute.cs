fa3b0f2e05fc00940ffce81f64743e5e332f5e2e
Anarik <106695465+Anariks@users.noreply.github.com>
2022-07-25 22:24:32 +0300

    new

--- ConsoleApp/DirectionPointAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace ConsoleApp;
-
-[AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field)]
-
-class DirectionPointAttribute : Attribute
-{
-    public string DirectionPoint {get; set;}
-}
