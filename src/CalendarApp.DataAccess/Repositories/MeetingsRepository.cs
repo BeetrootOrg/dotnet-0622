010308c1b48059bad3085dcfb68f8b114c1a8958
d.misik <misik.dmitriy@gmail.com>
2022-07-27 20:17:39 +0300

    cache file data

--- src/CalendarApp.DataAccess/Repositories/MeetingsRepository.cs
+++ src/CalendarApp.DataAccess/Repositories/MeetingsRepository.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using CalendarApp.Contracts;
 using CalendarApp.DataAccess.Repositories.Interfaces;
 using Newtonsoft.Json;
@@ -11,22 +10,38 @@ internal class MeetingsRepository : IMeetingsRepository
 {
 	private const string Filename = "data.json";
 
+	private readonly IList<Meeting> _meetings;
+
+	private MeetingsRepository(IList<Meeting> meetings)
+	{
+		_meetings = meetings;
+	}
+
 	public void AddMeeting(Meeting meeting)
 	{
-		var meetings = GetAllMeetings();
-		var newMeetings = meetings.Append(meeting);
-		var serialized = JsonConvert.SerializeObject(newMeetings);
+		_meetings.Add(meeting);
+		var serialized = JsonConvert.SerializeObject(_meetings);
 		File.WriteAllText(Filename, serialized);
 	}
 
 	public IEnumerable<Meeting> GetAllMeetings()
 	{
+		return _meetings;
+	}
+
+	public static MeetingsRepository Create()
+	{
+		IList<Meeting> meetings;
 		if (!File.Exists(Filename))
 		{
-			return Enumerable.Empty<Meeting>();
+			meetings = new List<Meeting>();
+		}
+		else
+		{
+			var serialized = File.ReadAllText(Filename);
+			meetings = JsonConvert.DeserializeObject<IList<Meeting>>(serialized);
 		}
 
-		var serialized = File.ReadAllText(Filename);
-		return JsonConvert.DeserializeObject<IEnumerable<Meeting>>(serialized);
+		return new MeetingsRepository(meetings);
 	}
 }
