b791c6889b320fac10635f55c18682be3bf1cf7b
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-07-28 17:11:28 +0300

    MainMenuPresenter

---
+++ src/CalendarApp.Console/Presenters/MainMenuPresenter.cs
@@ -0,0 +1,16 @@
+using CalendarApp.Console.Presenters.interfaces;
+
+namespace CalendarApp.Console.Presenters;
+
+class MainMenuPresenter : IPresenter
+{
+    public void Action()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Show()
+    {
+        throw new NotImplementedException();
+    }
+}
