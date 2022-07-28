4e850242d3b748fea627e64c1a61a5fddf738f28
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-07-28 21:49:33 +0300

    north/south/west/east

--- LinqLesson/Program.cs
+++ LinqLesson/Program.cs
@@ -13,6 +13,15 @@ namespace LinqLesson
         {
             var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
             
+            //1 - find out who is located farthest north/south/west/east using latitude/longitude data
+            var north = persons.MaxBy(person => person.Latitude);
+            WriteLine($"The person who is to the north: {north}");
+            var south = persons.MinBy(person => person.Latitude);
+            WriteLine($"The person who is to the south: {south}");
+            var east = persons.MaxBy(person => person.Longitude);
+            WriteLine($"The person who is to the east: {east}");
+            var west = persons.MinBy(person => person.Longitude);
+            WriteLine($"The person who is to the west: {west}");
         }
     }
 }
