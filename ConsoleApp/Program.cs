9019ef440cc29ac7ddfc27bbdf5f9625382c19f0
Oles Borys <oles.borys@gmail.com>
2022-07-28 20:49:39 +0200

    homework

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,14 +1,35 @@
 ﻿using System;
-
+using System.Linq;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var assembly = typeof(string).Assembly.Take(20);
-        foreach (var assemblyType in assembly)
+        var stringType = typeof(int);
+        var assembly = stringType.Assembly;
+
+        Console.WriteLine($"Assmebly full name: {assembly.FullName}");
+
+        foreach (var assemblyType in assembly.GetTypes().Take(40))
         {
-            Console.WriteLine($"Assembly name is");
+            Console.WriteLine($"Assembly type name is {assemblyType.Name}");
+            Console.WriteLine($"Type is Class {assemblyType.IsClass}");
+            Console.WriteLine($"Type is Interface {assemblyType.IsInterface}");
+
+            var methodsFromAssembly = assemblyType.GetMethods();
+
+            foreach (var method in methodsFromAssembly)
+            {
+                Console.WriteLine($"Method information {method}");
+                Console.WriteLine($"Method return type {method.ReturnType}");
+
+                var parameters = method.GetParameters().ToList();
+
+                foreach (var parameter in parameters)
+                {
+                    Console.WriteLine($"Method parameters {parameter.ParameterType}");
+                }
+            }
         }
 
     }
