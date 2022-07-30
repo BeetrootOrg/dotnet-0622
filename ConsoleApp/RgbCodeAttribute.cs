29b9198d3b89d227b0e8c9ccc1d34d41c963be4d
Anarik <106695465+Anariks@users.noreply.github.com>
2022-07-30 23:48:17 +0300

    some information

--- ConsoleApp/RgbCodeAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ConsoleApp;
-
-[AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field)]
-class RgbCodeAttribute : Attribute
-{
-    public string Code { get; init; }
-}
