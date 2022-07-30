99b41ffe7d0d09d328b8ee78ab4e79b621d0fb44
iVanyaVas <vanya.iaschenko@gmail.com>
2022-07-30 18:46:48 +0300

    init

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,104 +1,34 @@
-﻿using ConsoleApp.Vote;
+﻿using System;
+using System.Linq;
 
-VoteList dataBase = new VoteList();
+using static System.Console;
 
 
-Menu();
 
-void DrawMenuOptions()
+var type = typeof(string);
+var assembly = type.Assembly;
+WriteLine($"Type of Assembly = {assembly.FullName}");
+
+foreach (var assemblyTypes in assembly.GetTypes().Take(100))
 {
-    Console.Clear();
-    Console.WriteLine("1: Create Vote");
-    Console.WriteLine("2: Vote for something");
-    Console.WriteLine("3: Show Results");
-    Console.WriteLine("0: Exit");
-}
+    WriteLine($"Assembly name : {assemblyTypes.Name}");
+    WriteLine($"Assembly attributes: {assemblyTypes.Attributes}");
+    WriteLine($"Assemby class:  {assemblyTypes.IsClass}");
+    WriteLine($"Assembly Base Type: {assemblyTypes.BaseType}");
 
-void Menu()
-{
-    while (true)
+    var assemblyMethods = type.GetMethods();
+
+    foreach (var method in assemblyMethods)
     {
-        DrawMenuOptions();
+        WriteLine($"Mehod Name: {method.Name}");
+        WriteLine($"Method Atributes: {method.Attributes}");
+        WriteLine($"Method return type: {method.ReturnType}");
 
-        var option = Console.ReadKey();
-
-        switch (option.Key)
+        var methodParams = method.GetParameters().ToList();
+        foreach (var parameters in methodParams)
         {
-
-            //Create Vote
-            case ConsoleKey.D1:
-                {
-                    CreateVote();
-                    break;
-                }
-            //Vote for something
-            case ConsoleKey.D2:
-                {
-                    VoteForSomething();
-                    break;
-                }
-            //Show Results
-            case ConsoleKey.D3:
-                {
-                    ShowResults();
-                    break;
-                }
-            //Show the Tables we have
-            case ConsoleKey.D0:
-                {
-                    
-                   return;
-                }
-
+            WriteLine($"Method {method.Name} parameters: {parameters.ParameterType}");
         }
     }
 }
-
-void CreateVote()
-{
-    Console.Clear();
-    Console.WriteLine("Input Vote Topic: ");
-    var voteTopic = Console.ReadLine();
-
-    if (!dataBase.CreateVoteTopic(voteTopic.ToLower())) return;
-
-    string message = "Topic is Created!";
-    message.PressAnyKeyToProcced();
-}
-
-void VoteForSomething()
-{
-    Console.Clear();
-    Console.WriteLine("Input Vote Topic: ");
-    var voteTopic = Console.ReadLine();
-
-
-    Console.WriteLine("Vote Variants for YES: yes , yeah , yah , y , 1 , ok ");
-    Console.WriteLine("Vote Variants for NO: no , nah , n , 0");
-
-    Console.WriteLine("Write your vote answer: ");
-    var voteAnswer = Console.ReadLine();
-
-    if (!dataBase.ToVote(voteTopic.ToLower(), voteAnswer.ToLower())) return;
-
-    string message = "Your Vote saved!";
-    message.PressAnyKeyToProcced();
-
-}
-
-void ShowResults()
-{
-    Console.Clear();
-    Console.WriteLine("Input Vote Topic: ");
-    var voteTopic = Console.ReadLine();
-    
-    Console.WriteLine($"-- {voteTopic} --");
-
-    dataBase.Show(voteTopic.ToLower());
-
-    string message = "";
-    message.PressAnyKeyToProcced();
-
-}
-
 
