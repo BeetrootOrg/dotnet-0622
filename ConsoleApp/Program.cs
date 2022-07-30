20094a6d5189ad73caae8b45aa88973c7a1d41ba
Dwandry <Tihon.lavrov@gmail.com>
2022-07-31 01:34:45 +0300

    Temp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
+﻿using System;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Type stringType = typeof(string);
+        var assembly = stringType.Assembly;
+
     }
 }
