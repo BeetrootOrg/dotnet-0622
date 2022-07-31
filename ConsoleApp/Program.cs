d3374cf8abe1e9edd6aefa539e1274ef91aeca73
Killjaen <killjaen@gmail.com>
2022-07-31 19:18:38 +0300

    without async

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Linq;
+using System.Text;
+
+using ConsoleApp;
+
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        var url = "https://api.opensensemap.org/boxes/5dbcb7cec8eb60001bdcb18b?format=json";
+
+        Console.WriteLine("Hello! it's SenseBoxGrabber!");
+        var request = new SenseBoxResponse(url);
+        Console.WriteLine();
+        Console.WriteLine("Pulling request...");
+        request.Run();
+
+            Console.WriteLine("Done!");
+            Console.WriteLine();
+
+            var response = request.Response;
+
+            var json = JObject.Parse(response);
+
+            Console.WriteLine($"Station name {json["name"]}.");
+            Console.WriteLine($"Last data was at {json["lastMeasurementAt"]}.");
+            Console.WriteLine($"Station position {json["exposure"]}.");
+            var location = new StringBuilder();
+            foreach (var item in json["grouptag"])
+            {
+                location.Append(item);
+            }
+            Console.WriteLine("Location: " + location);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            foreach (var sensor in json["sensors"])
+            {
+                Console.WriteLine(sensor["title"] + " sensor.");
+                Console.WriteLine("Sensortype is " + sensor["sensorType"] + ".");
+                Console.WriteLine("Value is " + sensor["lastMeasurement"]["value"] + " " + sensor["unit"]+".");
+                Console.WriteLine();
+
+            }
+    }
+}
