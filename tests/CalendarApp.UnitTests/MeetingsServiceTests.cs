4bb8c6d0a65ef155d7c56537f0e6c2094f10039b
d.misik <misik.dmitriy@gmail.com>
2022-08-01 19:53:29 +0300

    shouldly & bogus

--- tests/CalendarApp.UnitTests/MeetingsServiceTests.cs
+++ tests/CalendarApp.UnitTests/MeetingsServiceTests.cs
@@ -1,9 +1,11 @@
 using System;
+using Bogus;
 using CalendarApp.Contracts;
 using CalendarApp.DataAccess.Repositories.Interfaces;
 using CalendarApp.Domain.Services;
 using CalendarApp.Domain.Services.Interfaces;
 using Moq;
+using Shouldly;
 
 namespace CalendarApp.UnitTests;
 
@@ -11,33 +13,36 @@ public class MeetingsServiceTests
 {
 	private readonly Mock<IMeetingsRepository> _meetingsRepository;
 	private readonly IMeetingsService _meetingsService;
+	private readonly Faker<Meeting> _meetingFaker;
 
 	public MeetingsServiceTests()
 	{
 		_meetingsRepository = new Mock<IMeetingsRepository>();
 		_meetingsService = new MeetingsService(_meetingsRepository.Object);
+
+		_meetingFaker = new Faker<Meeting>()
+			.RuleFor(x => x.Name, f => f.Random.Word())
+			.RuleFor(x => x.Timeframe, f =>
+			{
+				var start = f.Date.Past();
+
+				return new Timeframe
+				{
+					Start = start,
+					End = start.Add(f.Date.Timespan(TimeSpan.FromHours(2)))
+				};
+			})
+			.RuleFor(x => x.Room, f => new Room
+			{
+				Name = f.Random.Word()
+			});
 	}
 
 	[Fact]
 	public void GetAllMeetingsShouldReturnThem()
 	{
 		// Arrange
-		var meetings = new[]
-		{
-			new Meeting
-			{
-				Name = "meeting1",
-				Room = new Room
-				{
-					Name = "room1"
-				},
-				Timeframe = new Timeframe
-				{
-					Start = DateTime.Now.Subtract(TimeSpan.FromDays(1)),
-					End = DateTime.Now.Subtract(TimeSpan.FromHours(23)),
-				}
-			}
-		};
+		var meetings = _meetingFaker.GenerateBetween(5, 15);
 
 		_meetingsRepository.Setup(x => x.GetAllMeetings())
 			.Returns(meetings);
@@ -46,6 +51,6 @@ public class MeetingsServiceTests
 		var result = _meetingsService.GetAllMeetings();
 
 		// Assert
-		Assert.Same(meetings, result);
+		result.ShouldBeSameAs(meetings);
 	}
 }
