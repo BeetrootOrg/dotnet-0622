3fe0d8f0a41c03b322008c87bb907825d74e8aae
Anarik <106695465+Anariks@users.noreply.github.com>
2022-08-02 16:51:17 +0300

    works

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+using static System.Console;
+
+using ConsoleApp;
+using System.Threading;
+
+using var cancellationTokenSource = new CancellationTokenSource();
+// analogue to above
+
+var cancellationToken = cancellationTokenSource.Token;
+
+Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+    cancellationTokenSource.Cancel();
+};
+
+using var httpClient = new HttpClient
+{
+    BaseAddress = new Uri("https://meowfacts.herokuapp.com/"),
+    Timeout = TimeSpan.FromSeconds(5)
+};
+
+var factClient = new FactClient(httpClient);
+
+var result = await factClient.GetRandomFact(cancellationToken);
+WriteLine("Random fact generated!");
+
+WriteLine(result.Fact[0]);
