f6b4fec8010e8cbb635c132c39b2dda972fe7b54
PeEpEGa <106858735+PeEpEGa@users.noreply.github.com>
2022-08-03 15:17:10 +0300

    Homework-20-reflection

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,151 +1,16 @@
-﻿char[,] Reversi(char[,]field, char turn)
+﻿var assembly = typeof(char).Assembly;
+System.Console.WriteLine($"Assembly full name: {assembly.FullName}" +"\n");
+foreach (var assemblyType in assembly.GetTypes())
 {
-    char w = turn == 'B' ? 'W' : 'B';
+    System.Console.WriteLine($"Type name: {assemblyType.Name}");
 
-    for (int i = 0; i < field.GetLength(0); i++)
+    foreach (var assemblyTypeMethod in assemblyType.GetMethods())
     {
-        for (int j = 0; j < field.GetLength(1); j++)
+        System.Console.WriteLine($"Method: {assemblyTypeMethod}. Return type: {assemblyTypeMethod.ReturnType}");
+        foreach (var assemblyTypeParameters in assemblyTypeMethod.GetParameters())
         {
-            if(field[i, j] == turn)
-            {
-                //up
-                int count = 0;
-                for (int e = i - 1; e + 1 > 0; e--)
-                {
-                    if (field[e, j] == w)
-                    {
-                        count++;
-                        continue;
-                    }
-                    if (field[e, j] == turn || field[e, j] == '0' || (field[e, j] == '.' && e == i - 1)) break;
-                    else if (field[e, j] == '.' && count > 0)
-                    {
-                        field[e, j] = '0';
-                        break;
-                    }
-                }
-
-                //right
-                count = 0;
-                for (int e = j + 1; e <= field.GetLength(1) - 1; e++)
-                {
-                    if (field[i, e] == w)
-                    {
-                        count++;
-                        continue;
-                    }
-                    if (field[i, e] == turn || field[i, e] == '0' || (field[i, e] == '.' && e == j + 1)) break;
-                    else if (field[i, e] == '.' && count > 0)
-                    {
-                        field[i, e] = '0';
-                        break;
-                    }
-                }
-
-                //down
-                count = 0;
-                for (int e = i + 1; e <= field.GetLength(0) - 1; e++)
-                {
-                    if (field[e, j] == w)
-                    {
-                        count++;
-                        continue;
-                    }
-                    if (field[e, j] == turn || field[e, j] == '0' || (field[e, j] == '.' && e == i + 1)) break;
-                    else if (field[e, j] == '.' && count > 0)
-                    {
-                        field[e, j] = '0';
-                        break;
-                    }
-                }
-
-                //left
-                count = 0;
-                for (int e = j - 1; e + 1 > 0; e--)
-                {
-                    if (field[i, e] == w)
-                    {
-                        count++;
-                        continue;
-                    }
-                    if (field[i, e] == turn || field[i, e] == '0' || (field[i, e] == '.' && e == j - 1)) break;
-                    else if (field[i, e] == '.' && count > 0)
-                    {
-                        field[i, e] = '0';
-                        break;
-                    }
-                }
-
-
-                //diag up right
-                count = 0;
-                for (int t = i - 1, e = j + 1 ; e <= field.GetLength(1) - 1 && t + 1 > 0; e++,t--)
-                {
-                    if (field[t, e] == w)
-                    {
-                        count++;
-                        continue;
-                    }
-                    if (field[t, e] == turn || field[t, e] == '0' || (field[t, e] == '.' && t == i - 1  && e == j + 1)) break;
-                    else if (field[t, e] == '.' && count > 0)
-                    {
-                        field[t, e] = '0';
-                        break;
-                    }
-                }
-
-                //diag down right
-                count = 0;
-                for (int t = i + 1, e = j + 1; e <= field.GetLength(1) - 1 && t <= field.GetLength(0) - 1; e++, t++)
-                {
-                    if (field[t, e] == w)
-                    {
-                        count++;
-                        continue;
-                    }
-                    if (field[t, e] == turn || field[t, e] == '0' || (field[t, e] == '.' && t == i + 1 && e == j + 1)) break;
-                    else if (field[t, e] == '.' && count > 0)
-                    {
-                        field[t, e] = '0';
-                        break;
-                    }
-                }
-
-                //diag up left
-                count = 0;
-                for (int t = i - 1, e = j - 1; e + 1 > 0 && t + 1 > 0; e--, t--)
-                {
-                    if (field[t, e] == w)
-                    {
-                        count++;
-                        continue;
-                    }
-                    if (field[t, e] == turn || field[t, e] == '0' || (field[t, e] == '.' && t == i - 1 && e == j - 1)) break;
-                    else if (field[t, e] == '.' && count > 0)
-                    {
-                        field[t, e] = '0';
-                        break;
-                    }
-                }
-
-                //diag down left
-                count = 0;
-                for (int t = i + 1, e = j - 1; e + 1 > 0 && t <= field.GetLength(0) - 1; e--, t++)
-                {
-                    if (field[t, e] == w)
-                    {
-                        count++;
-                        continue;
-                    }
-                    if (field[t, e] == turn || field[t, e] == '0' || (field[t, e] == '.' && t == i + 1 && e == j - 1)) break;
-                    else if (field[t, e] == '.' && count > 0)
-                    {
-                        field[t, e] = '0';
-                        break;
-                    }
-                }
-            }
+            System.Console.WriteLine($"Argument needed to pass: {assemblyTypeParameters}");
         }
     }
-    return field;
+    System.Console.WriteLine();
 }
