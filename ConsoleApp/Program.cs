270c615a34eb2de2d59b28fb57e08ebcf8aadc4d
Danylo Strus <danylo.strus@gmail.com>
2022-08-05 13:10:32 +0300

    work

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using static System.Console;
+using ConsoleApp;
+
+
+using var cancellationTokenSource = new CancellationTokenSource();
+
+var cancellationToken = cancellationTokenSource.Token;
+
+Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
+{
+    cancellationTokenSource.Cancel();
+};
+
+using var httpClient = new HttpClient
+{
+    BaseAddress = new Uri("https://meowfacts.herokuapp.com/"),
+    Timeout = TimeSpan.FromSeconds(5)
+};
+
+var factClient = new FactClient(httpClient);
+
+var result = await factClient.GetRandomFact(cancellationToken);
+WriteLine("Fact generated!");
+
+WriteLine(result.Fact[0]);
