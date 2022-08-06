14d51e989db41954c7497f7df8a79d955550cb6f
Dwandry <Tihon.lavrov@gmail.com>
2022-08-07 02:10:18 +0300

    Update meeting done

---
+++ src/CalendarApp.Console/Presenters/UpdateMeetingPresenter.cs
@@ -0,0 +1,41 @@
+using CalendarApp.Console.Presenters.Interfaces;
+using CalendarApp.Contracts;
+using CalendarApp.Domain.Builders;
+using CalendarApp.Domain.Exceptions;
+using CalendarApp.Domain.Services;
+
+namespace CalendarApp.Console.Presenters;
+
+internal class UpdateMeetingPresenter : IPresenter
+{
+	private readonly MeetingBuilder _meetingBuilder;
+
+	public UpdateMeetingPresenter(MeetingBuilder meetingBuilder)
+	{
+		_meetingBuilder = meetingBuilder;
+	}
+
+    public IPresenter Action()
+    {
+        try
+		{
+			var meeting = _meetingBuilder.Build();
+
+			WriteLine("Meeting successfully updated!");
+		}
+		catch (CalendarAppDomainException exc)
+		{
+			WriteLine(exc.Message);
+		}
+
+		WriteLine("Press any key to continue...");
+		ReadKey();
+
+		return new MainMenuPresenter();
+    }
+
+    public void Show()
+    {
+        
+    }
+}
