fcfa2223fefe6a2bfb2bba6931b339741479012e
d.misik <misik.dmitriy@gmail.com>
2022-08-08 20:35:28 +0300

    select

--- sql/0004.select.sql
+++ sql/0004.select.sql
@@ -1,6 +1,35 @@
+-- C# operator && == SQL operator AND
+-- C# operator || == SQL operator OR
+-- C# operator == == SQL operator =
 SELECT first_name,
 	last_name,
 	age
 FROM tbl_persons
 WHERE LOWER(first_name) = 'dima'
+	AND age > 24
 ORDER BY age;
+
+SELECT *
+FROM tbl_persons
+WHERE address IS NULL;
+SELECT *
+FROM tbl_persons
+WHERE address IS NULL;
+
+SELECT first_name,
+	COUNT(1),
+	AVG(age),
+	MIN(age) AS min_age,
+	MAX(age) AS max_age
+FROM tbl_persons
+GROUP BY first_name
+HAVING COUNT(1) > 1;
+
+SELECT *
+FROM tbl_persons
+WHERE first_name IN (
+		SELECT first_name
+		FROM tbl_persons
+		GROUP BY first_name
+		HAVING COUNT(1) > 1
+	);
