971c0213c9dd779eea62741c8573ac24d3330ae1
PeEpEGa <106858735+PeEpEGa@users.noreply.github.com>
2022-08-10 12:31:30 +0300

    Homework-19-linq

--- LinqLesson/Program.cs
+++ LinqLesson/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,56 +13,138 @@ namespace LinqLesson
 		static void Main(string[] args)
 		{
 			var persons = JsonConvert.DeserializeObject<IEnumerable<Person>>(File.ReadAllText("data.json"));
+			var personsList = persons.ToList();
 
-			// 1. count males
-			var males = persons.Count(person => person.Gender == Gender.Male);
-			WriteLine($"Males: {males}");
+			//find out who is located farthest north using latitude/longitude data
+			var farthestNorth = persons.Max(n => n.Latitude);
+			System.Console.WriteLine("Person who is located farthest north is " + personsList.Find(n => n.Latitude == farthestNorth).Name);
 
-			// 2. count females
-			var females = persons.Count(person => person.Gender == Gender.Female);
-			WriteLine($"Females: {females}");
+			//find out who is located farthest south using latitude/longitude data
+			var farthestSouth = persons.Min(n => n.Latitude);
+			System.Console.WriteLine("Person who is located farthest south is " + personsList.Find(n => n.Latitude == farthestSouth).Name);
 
-			// 3. youngest person
-			var youngestAge = persons.Min(person => person.Age);
-			var youngestPerson = persons.MinBy(person => person.Age);
-			WriteLine($"Youngest person is {youngestPerson}");
+			//find out who is located farthest west using latitude/longitude data
+			var farthestWest = persons.Min(n => n.Longitude);
+			System.Console.WriteLine("Person who is located farthest west is " + personsList.Find(n => n.Longitude == farthestWest).Name);
 
-			// 4. oldest person
-			var oldestPerson = persons.MaxBy(x => x.Age);
-			WriteLine($"Oldest person is {oldestPerson}");
+			//find out who is located farthest east using latitude/longitude data
+			var farthestEast = persons.Max(n => n.Longitude);
+			System.Console.WriteLine("Person who is located farthest east is " + personsList.Find(n => n.Longitude == farthestEast).Name + "\n");
 
-			// 5. all youngest
-			var youngestPersons = persons.Where(x => x.Age == youngestAge);
-			WriteLine($"There are {youngestPersons.Count()} people");
+			//find max and min distance between 2 persons
+			var personsDistance = persons
+				.Join(persons,
+					person => true,
+					person => true,
+					(person1, person2) => new
+					{
+						Person1 = person1,
+						Person2 = person2
+					})
+				.Where(x => !Object.ReferenceEquals(x.Person1, x.Person2))
+				.Select(x =>
+				{
+					var person1 = x.Person1;
+					var person2 = x.Person2;
 
-			// 6. average age
-			var averageAge = persons.Average(x => x.Age);
-			WriteLine($"Average age is {averageAge}");
+					var pDistance = Distance(person1.Latitude,person2.Latitude,person1.Longitude,person2.Longitude);
 
-			// 7.1. count people with same eye color
-			var eyesColors = persons.Select(x => x.EyeColor);
-			var uniqueEyesColors = eyesColors.Distinct();
-			foreach (var eyeColor in uniqueEyesColors)
+					return new
+					{
+						Person1 = person1,
+						Person2 = person2,
+						PerDistance = pDistance
+					};
+				});
+				System.Console.WriteLine($"Min distance between 2 persons are {personsDistance.Min(n => n.PerDistance)}");
+				System.Console.WriteLine($"Max distance between 2 persons are {personsDistance.Max(n => n.PerDistance)}\n");
+
+			//find persons with most common about words
+			var personsWithMostCommonAboutWords = persons
+				.Join(persons,
+					person => true,
+					person => true,
+					(person1, person2) => new
+					{
+						Person1 = person1,
+						Person2 = person2
+					})
+				.Where(x => !Object.ReferenceEquals(x.Person1, x.Person2))
+				.Select(x =>
+				{
+					var person1 = x.Person1;
+					var person2 = x.Person2;
+
+					var about1 = person1.About.Split(' ');
+					var about2 = person2.About.Split(' ');
+
+					var intersection = about1.Intersect(about2).ToArray();
+
+					return new
+					{
+						Person1 = person1,
+						Person2 = person2,
+						CommonWordsInAbout = intersection
+					};
+				})
+				.MaxBy(x => x.CommonWordsInAbout.Length);
+			WriteLine($"Max number of common words between two persons are {personsWithMostCommonAboutWords.CommonWordsInAbout.Length}\n");
+
+			//find persons with same friends
+			//I changed data.json file, so there are 1 common friend
+			var personsWithCommonFriends = persons
+				.Join(persons,
+					person => true,
+					person => true,
+					(person1, person2) => new
+					{
+						Person1 = person1,
+						Person2 = person2
+					})
+				.Where(x => !Object.ReferenceEquals(x.Person1, x.Person2))
+				.Select(x =>
+				{
+					var person1 = x.Person1;
+					var person2 = x.Person2;
+
+					var friends1 = person1.Friends.Select(x => x.Name);
+					var friends2 =person2.Friends.Select(x => x.Name);
+					var commonFriends = friends1.Intersect(friends2);
+
+					return new
+					{
+						Person1 = person1,
+						Person2 = person2,
+						CommonFriends = commonFriends
+					};
+				})
+				.Where(x => x.CommonFriends.Count() >= 1);
+			foreach (var item in personsWithCommonFriends)
 			{
-				var countSameEyeColor = persons.Count(x => x.EyeColor == eyeColor);
-				WriteLine($"{eyeColor} has {countSameEyeColor} people");
+				System.Console.WriteLine($"Persons with same friends: {item.Person1.Name}");
 			}
-
-			// 7.2. 
-			var eyeColorsGroup = persons.GroupBy(x => x.EyeColor);
-			foreach (var item in eyeColorsGroup)
-			{
-				WriteLine($"{item.Key} has {item.Count()} people");
-			}
-
-			// 8. common tags
-			var allTagsSelected = persons.Select(x => x.Tags);
-			var allTags = persons.SelectMany(x => x.Tags);
-			var tagsGroups = allTags.GroupBy(x => x);
-			var mainTag = tagsGroups.MaxBy(x => x.Count()).Key;
-
-			var personsWithSameTag = persons.Where(x => x.Tags.Contains(mainTag)).Count();
-			WriteLine($"Persons with same tag '{mainTag}' are {personsWithSameTag}");
 		}
+		static double ToRadians(double angleIn10thOfADegree)
+    	{
+        	return (angleIn10thOfADegree * Math.PI) / 180;
+    	}
+    	static double Distance(double lat1, double lat2, double lon1, double lon2)
+    	{
+        	lon2 = ToRadians(lon2);
+        	lon1 = ToRadians(lon1);
+        	lat1 = ToRadians(lat1);
+        	lat2 = ToRadians(lat2);
+ 
+        	// Haversine formula
+        	double dlon = lon2 - lon1;
+        	double dlat = lat2 - lat1;
+        	double a = Math.Pow(Math.Sin(dlat / 2), 2) +
+                	Math.Cos(lat1) * Math.Cos(lat2) *
+                	Math.Pow(Math.Sin(dlon / 2),2);
+             
+        	double c = 2 * Math.Asin(Math.Sqrt(a));
+        	double r = 6371;
+        	return (c * r);
+    	}
 	}
 }
