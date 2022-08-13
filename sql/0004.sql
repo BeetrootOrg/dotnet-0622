a126c078e7165655a8cc164e718d7b3d1ebe284b
Nahlenko Oleksandr <noapioss@gmail.com>
2022-08-13 10:28:24 +0300

    primary keys

--- sql/0004.sql
+++ sql/0004.sql
@@ -1,6 +1,6 @@
 -- table to store bank accounts
 CREATE TABLE IF NOT EXISTS tbl_bank_accounts(
-    bank_account_id VARCHAR(16) NOT NULL UNIQUE,
+    bank_account_id VARCHAR(16) RPIMARY KEY,
 	fist_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
     currency_type VARCHAR(20) NOT NULL,
--- sql/0005.sql
+++ sql/0005.sql
@@ -1,6 +1,6 @@
 -- table to store bank transactions data
 CREATE TABLE IF NOT EXISTS tbl_bank_transactions(
-    transaction_id VARCHAR(20) NOT NULL UNIQUE,
+    transaction_id VARCHAR(20) RPIMARY KEY,
     from_account_id VARCHAR(16) NOT NULL,
 	to_account_id VARCHAR(16) NOT NULL,
     currency_type VARCHAR(20) NOT NULL,
