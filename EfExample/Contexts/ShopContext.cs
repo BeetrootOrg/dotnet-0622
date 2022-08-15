27368937dccc8e955553b5f79b4eb5fa24d98e73
d.misik <misik.dmitriy@gmail.com>
2022-08-15 19:46:25 +0300

    context

---
+++ EfExample/Contexts/ShopContext.cs
@@ -0,0 +1,24 @@
+using System;
+using EfExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfExample.Contexts;
+
+public class ShopContext : DbContext
+{
+	public DbSet<Customer> Customers { get; init; }
+	public DbSet<Employee> Employees { get; init; }
+	public DbSet<Position> Positions { get; init; }
+	public DbSet<Product> Products { get; init; }
+	public DbSet<Receipt> Receipts { get; init; }
+
+	public ShopContext() : base()
+	{
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=shop;")
+			.LogTo(Console.WriteLine);
+	}
+}
