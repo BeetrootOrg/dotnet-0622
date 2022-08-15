6b399ef2d76824e37ab8f83a378f7a555a44723b
d.misik <misik.dmitriy@gmail.com>
2022-08-15 19:45:26 +0300

    db context

--- EfExample/Program.cs
+++ EfExample/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using EfExample.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+await using var context = new ShopContext();
+
+var customers = await context.Customers.ToArrayAsync();
+
+foreach (var customer in customers)
+{
+	Console.WriteLine($"{customer.Id}: {customer.FirstName} {customer.LastName}");
+}
