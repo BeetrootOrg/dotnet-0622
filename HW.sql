ad38d02195abcd499ae5c6639af02e91a0327876
Danylo Strus <danylo.strus@gmail.com>
2022-08-15 11:47:23 +0300

    sql book

--- HW.sql
+++ HW.sql
@@ -1,49 +1,33 @@
-CREATE DATABASE phone_book;
 CREATE TABLE IF NOT EXISTS tbl_book (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50),
-	number INTEGER NOT NULL,
-    note VARCHAR(100)
+	book_id SERIAL PRIMARY KEY,
+	book_name VARCHAR(50) NOT NULL,
+	book_isbn VARCHAR(50) NOT NULL,
+	book_edition INTEGER(20) NOT NULL,
+	author_id INT NOT NULL REFERENCES tbl_authors(author_id),
+	publisher_id INT NOT NULL REFERENCES tbl_publishers(publisher_id)
 );
-
-CREATE DATABASE school_schedule;
-CREATE TABLE IF NOT EXISTS tbl_schedule (
-	id SERIAL PRIMARY KEY,
-	lesson_name VARCHAR(50),
-    lesson_start TIME NOT NULL,
-    lesson_end TIME NOT NULL,
-    lesson_date DATE NOT NULL,
-    teacher_id INTEGER NOT NULL,
-    class_id INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS tbl_authors (
+	author_id SERIAL PRIMARY KEY,
+	author_name VARCHAR(255) NOT NULL
 );
-
-CREATE DATABASE login_history;
-CREATE TABLE IF NOT EXISTS tbl_login_history (
-	id SERIAL PRIMARY KEY,
-	user_name VARCHAR(100) NOT NULL,
-	login_time TIME NOT NULL,
-    login_date DATE NOT NULL,
-    login_ip CIDR NOT NULL,
-    location VARCHAR(100) NOT NULL
+CREATE TABLE IF NOT EXISTS tbl_publishers (
+	publisher_id SERIAL PRIMARY KEY,
+	publisher_name VARCHAR(255) NOT NULL
 );
-
-CREATE DATABASE bank_accounts;
-CREATE TABLE IF NOT EXISTS tbl_accounts (
-	account_id SERIAL PRIMARY KEY,
-	account_name VARCHAR(50) NOT NULL,
-	date_opened DATE NOT NULL,
-	other_account_details VARCHAR(50),
-    account_type INTEGER NOT NULL,
-    costumer_id INTEGER NOT NULL,
-    amount MONEY
+CREATE TABLE IF NOT EXISTS tbl_loans (
+	loan_date DATETIME NOT NULL,
+	loans_is_active BOOLEAN NOT NULL,
+	custumer_id INT NOT NULL REFERENCES tbl_customers(customer_id),
+	book_id INT NOT NULL REFERENCES tbl_books(book_id),
+	librarian_id INT NOT NULL REFERENCES tbl_librarians(librarian_id)
 );
-
-CREATE DATABASE transactions_data;
-CREATE TABLE IF NOT EXISTS tbl_transactions (
-	transactions_id SERIAL PRIMARY KEY,
-	date_transactions DATE NOT NULL,
-	amount_of_transactions MONEY NOT NULL,
-	purchare_id INTEGER NOT NULL,
-    acoount_id INTEGER NOT NULL,
-    transaction_type VARCHAR(50)
+CREATE TABLE IF NOT EXISTS tbl_cutomers (
+	cutomers_id SERIAL PRIMARY KEY,
+	cutomers_name VARCHAR(255) NOT NULL,
+	cutomers_address VARCHAR(255) NOT NULL,
+	cutomers_phone VARCHAR(50) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS tbl_librarians (
+	librarian_id SERIAL PRIMARY KEY,
+	librarian_name VARCHAR(255) NOT NULL
 );
