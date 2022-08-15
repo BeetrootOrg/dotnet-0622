69b5e2fb9eef9259c3b5e5178c04d09a7f14ef70
d.misik <misik.dmitriy@gmail.com>
2022-08-15 20:37:49 +0300

    script to update database

--- commands.bat
+++ commands.bat
@@ -2,3 +2,4 @@ dotnet new tool-manifest
 dotnet tool install --local dotnet-ef
 dotnet add package Microsoft.EntityFrameworkCore.Design
 dotnet ef migrations add Initial
+dotnet ef database update --connection "User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=test;"
