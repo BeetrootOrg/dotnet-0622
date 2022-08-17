a2bb6648a324811cdec2fc3a47dd2c6f2d06ab49
Oleksandr <sanelka77@yahoo.com>
2022-08-18 02:28:33 +0300

    has added fields

--- sql/0002-storechoolschedule.sql
+++ sql/0002-storechoolschedule.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS tbl_storechoolschedule (
     id SERIAL PRIMARY KEY,
     lessonday DATE NOT NULL CHECK(lessonday > NOW() - INTERVAL '1 months'),
-    classname VARCHAR(4) NOT NULL CHECK(LENGTH(classname)>1),
+    classname VARCHAR(4) NOT NULL CHECK(LENGTH(classname) > 1),
     lessonname VARCHAR(40) NOT NULL,
-    numberlesson SMALLSERIAL NOT NULL CHECK(numberlesson < 11)
+    numberlesson SMALLSERIAL NOT NULL CHECK(numberlesson < 11),
+    fullnameteacher VARCHAR(1000) NOT NULL,
+    fullnameassistant VARCHAR(1000) NOT NULL
     );
--- sql/0003-storeusersloginhistory.sql
+++ sql/0003-storeusersloginhistory.sql
@@ -1,6 +1,11 @@
 CREATE TABLE IF NOT EXISTS tbl_storeusersloginhistory (
     timestampact TIMESTAMP PRIMARY KEY,
-    login VARCHAR(30) NOT NULL CHECK(LENGTH(login)>2),
+    login VARCHAR(30) NOT NULL CHECK(LENGTH(login) > 2),
     accessrights VARCHAR(30) NOT NULL DEFAULT 'guest',
-    inoutuser BOOL NOT NULL DEFAULT TRUE
+    inoutuser BOOL NOT NULL DEFAULT TRUE,
+    ipaddress VARCHAR(15) NOT NULL,
+    hostname VARCHAR(40) NOT NULL,
+    opereationsystem VARCHAR(20) NOT NULL,
+    countrycity VARCHAR(50) NOT NULL,
+    provider VARCHAR(50) NOT NULL
     );
