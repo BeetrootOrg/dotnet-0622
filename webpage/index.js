8255f62d3209dad6b93a7d1b253ee69426b84e13
d.misik <misik.dmitriy@gmail.com>
2022-08-20 10:46:09 +0300

    closure

--- webpage/index.js
+++ webpage/index.js
@@ -3,4 +3,14 @@ console.log('Hello, world');
 const clicker = document.getElementById('clicker');
 clicker.addEventListener('click', (ev) => {
 	console.log(ev);
-})
+});
+
+const clickerButton = document.querySelector('#counter button');
+const clickerText = document.querySelector('#counter p');
+
+let countClicks = 0;
+clickerButton.addEventListener('click', () => {
+	// analogue below
+	// clickerText.innerHTML = 'Clicks: ' + ++countClicks;
+	clickerText.innerHTML = `Clicks: ${++countClicks}`;
+});
