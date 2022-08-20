ed8962ba4cc07ad14f47fc9d8d5457601183b709
Veronika Telelym <ntelelym@gmail.com>
2022-08-20 22:51:42 +0300

    unit tests

--- tests/CalendarApp.UnitTests/MeetingsServiceTests.cs
+++ tests/CalendarApp.UnitTests/MeetingsServiceTests.cs
@@ -12,84 +12,138 @@ namespace CalendarApp.UnitTests;
 
 public class MeetingsServiceTests
 {
-	private readonly Mock<IMeetingsRepository> _meetingsRepository;
-	private readonly IMeetingsService _meetingsService;
+    private readonly Mock<IMeetingsRepository> _meetingsRepository;
+    private readonly IMeetingsService _meetingsService;
 
-	public MeetingsServiceTests()
-	{
-		_meetingsRepository = new Mock<IMeetingsRepository>();
-		_meetingsService = new MeetingsService(_meetingsRepository.Object);
-	}
+    public MeetingsServiceTests()
+    {
+        _meetingsRepository = new Mock<IMeetingsRepository>();
+        _meetingsService = new MeetingsService(_meetingsRepository.Object);
+    }
 
-	[Fact]
-	public void GetAllMeetingsShouldReturnThem()
-	{
-		// Arrange
-		var meetings = Factory.MeetingFaker.GenerateBetween(5, 15);
+    [Fact]
+    public void GetAllMeetingsShouldReturnThem()
+    {
+        // Arrange
+        var meetings = Factory.MeetingFaker.GenerateBetween(5, 15);
 
-		_meetingsRepository.Setup(x => x.GetAllMeetings())
-			.Returns(meetings);
+        _meetingsRepository.Setup(x => x.GetAllMeetings())
+            .Returns(meetings);
 
-		// Act
-		var result = _meetingsService.GetAllMeetings();
+        // Act
+        var result = _meetingsService.GetAllMeetings();
 
-		// Assert
-		result.ShouldBeSameAs(meetings);
-	}
+        // Assert
+        result.ShouldBeSameAs(meetings);
+    }
 
-	[Fact]
-	public void AddMeetingShouldAddItifNotOverlap()
-	{
-		// Arrange
-		var meetings = Factory.MeetingFaker.GenerateBetween(5, 15);
+    [Fact]
+    public void AddMeetingShouldAddItifNotOverlap()
+    {
+        // Arrange
+        var meetings = Factory.MeetingFaker.GenerateBetween(5, 15);
 
-		var meetingStart = Factory.CommonFaker.Date.Future().Add(TimeSpan.FromHours(2));
-		var newMeeting = new Meeting
-		{
-			Name = Factory.CommonFaker.Random.Word(),
-			Room = new Room
-			{
-				Name = Factory.CommonFaker.Random.Word(),
-			},
-			Timeframe = new Timeframe
-			{
-				Start = meetingStart,
-				End = meetingStart.Add(Factory.CommonFaker.Date.Timespan())
-			}
-		};
+        var meetingStart = Factory.CommonFaker.Date.Future().Add(TimeSpan.FromHours(2));
+        var newMeeting = new Meeting
+        {
+            Name = Factory.CommonFaker.Random.Word(),
+            Room = new Room
+            {
+                Name = Factory.CommonFaker.Random.Word(),
+            },
+            Timeframe = new Timeframe
+            {
+                Start = meetingStart,
+                End = meetingStart.Add(Factory.CommonFaker.Date.Timespan())
+            }
+        };
 
-		_meetingsRepository.Setup(x => x.GetAllMeetings())
-			.Returns(meetings);
+        _meetingsRepository.Setup(x => x.GetAllMeetings())
+            .Returns(meetings);
 
-		// Act
-		_meetingsService.AddMeeting(newMeeting);
+        // Act
+        _meetingsService.AddMeeting(newMeeting);
 
-		// Assert
-		_meetingsRepository.Verify(x => x.AddMeeting(newMeeting), Times.Once());
-	}
+        // Assert
+        _meetingsRepository.Verify(x => x.AddMeeting(newMeeting), Times.Once());
+    }
 
-	[Fact]
-	public void AddMeetingShouldRaiseExceptionIfOverlap()
-	{
-		// Arrange
-		var meetings = Factory.MeetingFaker.GenerateBetween(5, 15);
+    [Fact]
+    public void AddMeetingShouldRaiseExceptionIfOverlap()
+    {
+        // Arrange
+        var meetings = Factory.MeetingFaker.GenerateBetween(5, 15);
 
-		var meetingStart = Factory.CommonFaker.Date.Future().Add(TimeSpan.FromHours(2));
-		var newMeeting = new Meeting
-		{
-			Name = Factory.CommonFaker.Random.Word(),
-			Room = meetings[0].Room,
-			Timeframe = meetings[0].Timeframe
-		};
+        var meetingStart = Factory.CommonFaker.Date.Future().Add(TimeSpan.FromHours(2));
+        var newMeeting = new Meeting
+        {
+            Name = Factory.CommonFaker.Random.Word(),
+            Room = meetings[0].Room,
+            Timeframe = meetings[0].Timeframe
+        };
 
-		_meetingsRepository.Setup(x => x.GetAllMeetings())
-			.Returns(meetings);
+        _meetingsRepository.Setup(x => x.GetAllMeetings())
+            .Returns(meetings);
 
-		// Act
-		Action action = () => _meetingsService.AddMeeting(newMeeting);
+        // Act
+        Action action = () => _meetingsService.AddMeeting(newMeeting);
 
-		// Assert
-		action.ShouldThrow<CalendarAppDomainException>();
-		_meetingsRepository.Verify(x => x.AddMeeting(newMeeting), Times.Never());
-	}
+        // Assert
+        action.ShouldThrow<CalendarAppDomainException>();
+        _meetingsRepository.Verify(x => x.AddMeeting(newMeeting), Times.Never());
+    }
+
+    [Fact]
+    public void DeleteMeetingsShouldDeleteThem()
+    {
+        // Arrange
+        var meetings = Factory.MeetingFaker.Generate(1);
+
+        _meetingsRepository.Setup(x => x.GetAllMeetings())
+            .Returns(meetings);
+
+        // Act
+        _meetingsService.DeleteMeeting(meetings[0]);
+
+        // Assert
+        _meetingsRepository.Verify(x => x.DeleteMeeting(meetings[0]), Times.Once());
+    }
+
+    [Fact]
+    public void UpdateMeetingShouldUpdateDateAndTimeIfNotOverlap()
+    {
+        // Arrange
+        var meetings = Factory.MeetingFaker.GenerateBetween(5, 15);
+        var meetingToUpdate = meetings[0];
+        meetingToUpdate.Room = meetings[1].Room;
+        meetingToUpdate.Timeframe = meetings[1].Timeframe;
+
+        _meetingsRepository.Setup(x => x.GetAllMeetings())
+            .Returns(meetings);
+
+        Action action = () => _meetingsService.UpdateMeeting(meetingToUpdate, 0);
+
+        // Assert
+        action.ShouldThrow<CalendarAppDomainException>();
+        _meetingsRepository.Verify(x => x.UpdateMeeting(meetingToUpdate, 0), Times.Never());
+    }
+
+    public void UpdateMeetingShouldThrowExceptionIfOverlap()
+    {
+        // Arrange
+        var meetings = Factory.MeetingFaker.GenerateBetween(5, 15);
+        // var newMeeting = CreateRandomFakeMeeting();
+        var meetingToUpdate = meetings[0];
+        meetingToUpdate.Timeframe = new Timeframe
+        {
+            Start = DateTime.Now,
+            End = DateTime.Now.AddHours(2)
+        };
+
+        // Act 
+        _meetingsService.UpdateMeeting(meetingToUpdate, 0);
+
+        // Assert
+        _meetingsRepository.Verify(x => x.UpdateMeeting(meetingToUpdate, 0), Times.Once());
+    }
 }
