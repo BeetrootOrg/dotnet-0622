c4d3ebe7881fa4fd122b6dfe0826700fb6e2e020
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-08-20 19:14:44 +0300

    bank_account

--- sql/03.login_history.sql
+++ sql/03.login_history.sql
@@ -1,4 +1,4 @@
-CREATE TABLE  IF NOT EXISTS tbl_login_history (
+CREATE TABLE IF NOT EXISTS tbl_login_history (
     id SERIAL PRIMARY KEY NOT NULL,
     user_name VARCHAR(100) NOT NULL,
 	day_of_week TIMESTAMP NOT NULL,
