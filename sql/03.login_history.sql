fec29e562813e0c5a0d482ccf33bc500d20441ec
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-08-20 19:28:09 +0300

    transactions_data

--- sql/03.login_history.sql
+++ sql/03.login_history.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS tbl_login_history (
     id SERIAL PRIMARY KEY NOT NULL,
     user_name VARCHAR(100) NOT NULL,
-	day_of_week TIMESTAMP NOT NULL,
-	login_time TIME NOT NULL,
-    session_time TIME NOT NULL
+	date_session TIMESTAMP NOT NULL,
+    time_session TIME NOT NULL
 );
