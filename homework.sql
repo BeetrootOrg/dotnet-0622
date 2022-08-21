1dbf3dbe6678c8ada652d9d774f9ffd21d0f2a18
iVanyaVas <49608391+iVanyaVas@users.noreply.github.com>
2022-08-21 11:02:06 +0300

    init

--- homework.sql
+++ homework.sql
@@ -1,17 +1,40 @@
-SELECT * FROM tbl_persons WHERE gender = 'M';
+CREATE TABLE IF NOT EXISTS tbl_phonebook(
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(100) NOT NULL
+);
 
-SELECT * FROM tbl_persons WHERE age >= 18;
+CREATE TABLE IF NOT EXISTS tbl_school(
+    class_number SMALLINT PRIMARY KEY NOT NULL,
+    date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    subject VARCHAR(100) NOT NULL,
+    group_number SMALLINT NOT NULL
+);
 
-SELECT * FROM tbl_persons WHERE address IS NULL OR address = '';
+CREATE TABLE IF NOT EXISTS tbl_logingistory(
+    id SERIAL PRIMARY KEY,
+    login VARCHAR(20) NOT NULL,
+    login_date DATE,
+    logout_date DATE
+);
 
-UPDATE tbl_persons
-SET age = age+1;
+CREATE TABLE IF NOT EXISTS tbl_bankaccounts(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    bankaccount_number INTEGER NOT NULL,
+    bankaccount_money MONEY
+);
 
-DELETE FROM tbl_persons WHERE address IS NULL OR address ='';
-
-SELECT Count(1) 
-FROM tbl_persons;
-
-SELECT age, Count(1) 
-FROM tbl_persons 
-GROUP BY age;
+CREATE TABLE IF NOT EXISTS tbl_transaction(
+    id SERIAL PRIMARY KEY,
+    sender_id INTEGER NOT NULL,
+    recieve_id INTEGER NOT NULL,
+    send_date DATE NOT NULL,
+    recieve_date DATE NOT NULL,
+    isreceive BOOL NOT NULL DEFAULT FALSE 
+);
