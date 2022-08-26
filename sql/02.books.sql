65190e82c01e03ea585402ea0463baec7e150c19
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-08-26 21:09:52 +0300

    delete book_genre

--- sql/02.books.sql
+++ sql/02.books.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS tbl_books (
     id SERIAL PRIMARY KEY,
     book_title VARCHAR(255) NOT NULL,
-    book_genre VARCHAR(100) NOT NULL,
     book_publication SMALLINT,
     book_total INT NOT NULL,
     author_id INT NOT NULL REFERENCES tbl_authors(id),
