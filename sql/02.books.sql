44e367446dcf483079c7c5e575bfe073aca33031
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-08-26 10:52:03 +0300

    genres

--- sql/02.books.sql
+++ sql/02.books.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS tbl_books (
     id SERIAL PRIMARY KEY,
     book_title VARCHAR(255) NOT NULL,
     book_genre VARCHAR(100) NOT NULL,
-    book_publication SMALLINT NOT NULL,
-    book_language VARCHAR(50) NOT NULL,
-    author_id INT NOT NULL REFERENCES tbl_authors(id)
+    book_publication SMALLINT,
+    book_total INT NOT NULL,
+    author_id INT NOT NULL REFERENCES tbl_authors(id),
+    genre_id INT NOT NULL REFERENCES tbl_genres(id)
 )
--- sql/04.history.sql
+++ sql/04.history.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS tbl_history (
-    id SERIAL PRIMARY KEY,
     book_id INTEGER NOT NULL REFERENCES tbl_books(id),
     customer_id INTEGER NOT NULL REFERENCES tbl_customers(id),
     took_book DATE NOT NULL,
