b932a6b2ab8b470f9fb21a2fc37c6cb6f0219244
PeEpEGa <106858735+PeEpEGa@users.noreply.github.com>
2022-08-28 01:38:52 +0300

    Homework-27-sql-keys

--- sql/0000.sql
+++ sql/0000.sql
@@ -1,9 +1,33 @@
-CREATE TABLE IF NOT EXISTS tbl_phonebook
-(
-	first_name VARCHAR(150) NOT NULL,
-	last_name VARCHAR(150) NOT NULL,
-	country VARCHAR(100) NOT NULL,
-	city VARCHAR(100) NOT NULL,
-	address VARCHAR(250) NOT NULL,
-	phone_number VARCHAR(20) PRIMARY KEY NOT NULL
+CREATE TABLE IF NOT EXISTS tbl_readers(
+	id SERIAL PRIMARY KEY,
+	reader_first_name VARCHAR(150) NOT NULL,
+	reader_last_name VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tbl_authors(
+	id SERIAL PRIMARY KEY,
+	author_first_name VARCHAR(150) NOT NULL,
+	author_last_name VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tbl_genres(
+	id SERIAL PRIMARY KEY,
+	genre VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tbl_books(
+	id SERIAL PRIMARY KEY,
+	book_name VARCHAR(250) NOT NULL,
+	amount INT NOT NULL,
+	status BOOLEAN NOT NULL,
+	author_id INT REFERENCES tbl_authors(id),
+	genre_id INT NOT NULL REFERENCES tbl_genres(id),
+	year SMALLINT
+);
+
+CREATE TABLE IF NOT EXISTS tbl_rents(
+	book_id INT NOT NULL REFERENCES tbl_books(id),
+	customer_id INT NOT NULL REFERENCES tbl_readers(id),
+	hand_over TIMESTAMP NOT NULL,
+	returned TIMESTAMP
 );
