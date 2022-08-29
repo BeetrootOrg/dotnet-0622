27754b39b33c11b68fa262620805a16535deae82
d.misik <misik.dmitriy@gmail.com>
2022-08-29 20:52:29 +0300

    persons

--- Persons.Web/Program.cs
+++ Persons.Web/Program.cs
@@ -23,7 +23,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -36,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Persons}/{action=Index}/{id?}");
 
 app.Run();
