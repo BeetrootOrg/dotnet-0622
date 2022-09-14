701e9acedcab2f9027b74ae93614d76a91d124fa
d.misik <misik.dmitriy@gmail.com>
2022-09-14 19:28:23 +0300

    asp.net with static content

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseFileServer();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
